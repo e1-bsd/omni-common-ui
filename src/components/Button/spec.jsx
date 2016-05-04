32e8af875a31779800c33252dd0b1164c2ddba8b
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-05-04 17:42:29 +0800

    Tested that the Button calls the function passed in the onClick prop when clicked

--- src/components/Button/spec.jsx
+++ src/components/Button/spec.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
+import Sinon from 'sinon';
 import Button from './button';
 
 describe('Button', () => {
   it('renders its children', () => {
     const wrapper = shallow(<Button><div id="innerContent" /></Button>);
     expect(wrapper.contains(<div id="innerContent" />)).to.be.true;
+  });
+
+  it('calls onClick when clicked', () => {
+    const onClick = Sinon.spy();
+    const wrapper = shallow(<Button onClick={onClick} />);
+    wrapper.simulate('click', { preventDefault: () => {} });
+    expect(onClick.called).to.be.true;
   });
 });
