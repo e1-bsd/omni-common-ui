4a33108f28e9e4447a02c2e975d781b98550b134
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-05-05 10:22:38 +0800

    Nested Type and validateType tests inside Button

--- src/components/Button/type.spec.js
+++ src/components/Button/type.spec.js
@@ -1,26 +1,28 @@
 import { expect } from 'chai';
 import { Type, validateType } from './type';
 
-describe('Type', () => {
-  it('has default', () => {
-    expect(Type.default).to.exist;
+describe('Button', () => {
+  describe('Type', () => {
+    it('has default', () => {
+      expect(Type.default).to.exist;
+    });
+
+    it('has danger', () => {
+      expect(Type.danger).to.exist;
+    });
   });
 
-  it('has danger', () => {
-    expect(Type.danger).to.exist;
-  });
-});
+  describe('validateType', () => {
+    it('accepts default', () => {
+      expect(validateType(Type.default)).to.be.true;
+    });
 
-describe('validateType', () => {
-  it('accepts default', () => {
-    expect(validateType(Type.default)).to.be.true;
-  });
+    it('accepts danger', () => {
+      expect(validateType(Type.danger)).to.be.true;
+    });
 
-  it('accepts danger', () => {
-    expect(validateType(Type.danger)).to.be.true;
-  });
-
-  it('throws error if invalid type is passed', () => {
-    expect(() => validateType("faketype")).to.throw();
+    it('throws error if invalid type is passed', () => {
+      expect(() => validateType("faketype")).to.throw();
+    });
   });
 });
