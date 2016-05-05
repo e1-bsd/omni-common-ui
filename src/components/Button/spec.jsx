05a9a122be8ce55133d2fbd2fb3f2d49ea0f460b
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-05-05 10:26:41 +0800

    Reorganised tests for Button

--- src/components/Button/spec.jsx
+++ src/components/Button/spec.jsx
@@ -6,23 +6,25 @@ import Button from './';
 import { Type } from './type';
 
 describe('Button', () => {
+  it('contains Type object', () => {
+    expect(Button.Type).to.equal(Type);
+  });
+
   it('renders its children', () => {
     const wrapper = shallow(<Button><div id="innerContent" /></Button>);
     expect(wrapper.contains(<div id="innerContent" />)).to.be.true;
-  });
-
-  it('calls onClick when clicked', () => {
-    const onClick = Sinon.spy();
-    const wrapper = shallow(<Button onClick={onClick} />);
-    wrapper.simulate('click', { preventDefault: () => {} });
-    expect(onClick.called).to.be.true;
   });
 
   it('thows error if invalid type is passed', () => {
     expect(() => shallow(<Button type="faketype" />)).to.throw();
   });
 
-  it('contains Type object', () => {
-    expect(Button.Type).to.equal(Type);
+  describe('when clicked', () => {
+    it('calls onClick', () => {
+      const onClick = Sinon.spy();
+      const wrapper = shallow(<Button onClick={onClick} />);
+      wrapper.simulate('click', { preventDefault: () => {} });
+      expect(onClick.called).to.be.true;
+    });
   });
 });
