648ec01679c1f3eb543da6edd3627fceb87f8919
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-05-06 16:41:14 +0800

    CardShowcase uses the Grid component

--- sample/components/CardShowcase/index.jsx
+++ sample/components/CardShowcase/index.jsx
@@ -1,61 +1,58 @@
-import 'flexboxgrid';
-import styles from './style.postcss';
 import sampleImage from './sample-image.jpg';
 
 import React, { Component } from 'react';
-import { Card, Button } from 'omni-common-ui';
+import { Card, Button, Grid } from 'omni-common-ui';
 import Showcase from 'components/Showcase';
 import log from 'loglevel';
 
-const Item = (props) =>
-  <div className="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-    <div className={styles.Item}>{props.children}</div>
-  </div>;
+const itemClasses = 'col-xs-6 col-sm-4 col-md-3 col-lg-2';
 
 class CardShowcase extends Component {
   render() {
     return <Showcase title="Cards">
-      <div className="row">
-        <Item>
-          <Card>
-            <Card.Content>Normal</Card.Content>
-          </Card>
-        </Item>
-        <Item>
-          <Card><Card.Image src={sampleImage} /></Card>
-        </Item>
-        <Item>
-          <Card>
-            <Card.Image src={sampleImage} />
-            <Card.Content>Some text</Card.Content>
-          </Card>
-        </Item>
-        <Item>
-          <Card>
-            <Card.Content>Some text</Card.Content>
-            <Card.Image src={sampleImage} />
-          </Card>
-        </Item>
-        <Item>
-          <Card>
-            <Card.Image src={sampleImage} />
-            <Card.Content>
-              <Card>
-                <Card.Content>Some text</Card.Content>
-              </Card>
-            </Card.Content>
-          </Card>
-        </Item>
-        <Item>
-          <Card>
-            <Card.Image src={sampleImage} />
-            <Card.Content>
-              <div>Some text</div>
-              <Button onClick={() => log.info('Click!')}>A button</Button>
-            </Card.Content>
-          </Card>
-        </Item>
-      </div>
+      <Grid>
+        <Grid.Group>
+          <Grid.Item className={itemClasses}>
+            <Card>
+              <Card.Content>Normal</Card.Content>
+            </Card>
+          </Grid.Item>
+          <Grid.Item className={itemClasses}>
+            <Card><Card.Image src={sampleImage} /></Card>
+          </Grid.Item>
+          <Grid.Item className={itemClasses}>
+            <Card>
+              <Card.Image src={sampleImage} />
+              <Card.Content>Some text</Card.Content>
+            </Card>
+          </Grid.Item>
+          <Grid.Item className={itemClasses}>
+            <Card>
+              <Card.Content>Some text</Card.Content>
+              <Card.Image src={sampleImage} />
+            </Card>
+          </Grid.Item>
+          <Grid.Item className={itemClasses}>
+            <Card>
+              <Card.Image src={sampleImage} />
+              <Card.Content>
+                <Card>
+                  <Card.Content>Some text</Card.Content>
+                </Card>
+              </Card.Content>
+            </Card>
+          </Grid.Item>
+          <Grid.Item className={itemClasses}>
+            <Card>
+              <Card.Image src={sampleImage} />
+              <Card.Content>
+                <div>Some text</div>
+                <Button onClick={() => log.info('Click!')}>A button</Button>
+              </Card.Content>
+            </Card>
+          </Grid.Item>
+        </Grid.Group>
+      </Grid>
     </Showcase>;
   }
 }
