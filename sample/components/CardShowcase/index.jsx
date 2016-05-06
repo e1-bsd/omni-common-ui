a3b14bf4945e37eb3f3373adc7fbfb24a24b6f17
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-05-06 14:17:36 +0800

    Improved cards in sample site

--- sample/components/CardShowcase/index.jsx
+++ sample/components/CardShowcase/index.jsx
@@ -1,36 +1,41 @@
 import 'flexboxgrid';
-
+import styles from './style.postcss';
 import sampleImage from './sample-image.jpg';
 
 import React, { Component } from 'react';
 import { Card } from 'omni-common-ui';
 import Showcase from 'components/Showcase';
 
+const Item = (props) =>
+  <div className="col-xs-6 col-sm-4 col-md-3 col-lg-2">
+    <div className={styles.Item}>{props.children}</div>
+  </div>;
+
 class CardShowcase extends Component {
   render() {
     return <Showcase title="Cards">
       <div className="row">
-        <div className="col-xs-2">
+        <Item>
           <Card.Card>
             <Card.Content>Normal</Card.Content>
           </Card.Card>
-        </div>
-        <div className="col-xs-2">
+        </Item>
+        <Item>
           <Card.Card><Card.Image src={sampleImage} /></Card.Card>
-        </div>
-        <div className="col-xs-2">
+        </Item>
+        <Item>
           <Card.Card>
             <Card.Image src={sampleImage} />
             <Card.Content>Some text</Card.Content>
           </Card.Card>
-        </div>
-        <div className="col-xs-2">
+        </Item>
+        <Item>
           <Card.Card>
             <Card.Content>Some text</Card.Content>
             <Card.Image src={sampleImage} />
           </Card.Card>
-        </div>
-        <div className="col-xs-2">
+        </Item>
+        <Item>
           <Card.Card>
             <Card.Image src={sampleImage} />
             <Card.Content>
@@ -39,7 +44,7 @@ class CardShowcase extends Component {
               </Card.Card>
             </Card.Content>
           </Card.Card>
-        </div>
+        </Item>
       </div>
     </Showcase>;
   }
