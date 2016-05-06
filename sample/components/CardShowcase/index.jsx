0113be6d6140327d105371b346529f6dc4b5edee
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-05-06 15:36:35 +0800

    Added a showcase of a Card with a Button inside

--- sample/components/CardShowcase/index.jsx
+++ sample/components/CardShowcase/index.jsx
@@ -3,7 +3,7 @@ import styles from './style.postcss';
 import sampleImage from './sample-image.jpg';
 
 import React, { Component } from 'react';
-import { Card } from 'omni-common-ui';
+import { Card, Button } from 'omni-common-ui';
 import Showcase from 'components/Showcase';
 
 const Item = (props) =>
@@ -42,6 +42,15 @@ class CardShowcase extends Component {
               <Card.Card>
                 <Card.Content>Some text</Card.Content>
               </Card.Card>
+            </Card.Content>
+          </Card.Card>
+        </Item>
+        <Item>
+          <Card.Card>
+            <Card.Image src={sampleImage} />
+            <Card.Content>
+              <div>Some text</div>
+              <Button>A button</Button>
             </Card.Content>
           </Card.Card>
         </Item>
