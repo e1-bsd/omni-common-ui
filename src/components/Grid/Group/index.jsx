815d86ce306ca378b28d0254bb99c9c90f6e2d7e
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-05-06 17:37:17 +0800

    Group wraps its children with an <Item> if needed

--- src/components/Grid/Group/index.jsx
+++ src/components/Grid/Group/index.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import Item from '../Item';
+import is from 'is_js';
 
 const Group = (props) => {
   return <div className="row">
-    {props.children}
+    {buildChildren()}
   </div>;
+
+  function buildChildren() {
+    if (is.not.existy(props.children)) {
+      return;
+    }
+
+    if (is.not.function(props.children.map)) {
+      return wrapChildIfNeeded(props.children);
+    }
+
+    return props.children.map(wrapChildIfNeeded);
+  }
+
+  function wrapChildIfNeeded(child) {
+    if (child.type === Item) {
+      return child;
+    }
+
+    return <Item>{child}</Item>;
+  }
 };
 
 export default Group;
