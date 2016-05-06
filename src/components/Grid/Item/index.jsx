a8b1974f27f24b38de8858fbd66035284349cfc3
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-05-06 16:47:51 +0800

    Added custom styles to Grid.Item

--- src/components/Grid/Item/index.jsx
+++ src/components/Grid/Item/index.jsx
@@ -1,6 +1,10 @@
+import styles from './style.postcss';
 
 import React from 'react';
+import classnames from 'classnames';
+
 const Item = (props) => {
+  const classes = classnames(props.className || 'col-xs-12', styles.Item);
   return <div className={classes}>
     {props.children}
   </div>;
