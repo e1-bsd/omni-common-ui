f35af2ba05f1cc27b274d69b6fb6b83dbc5d7880
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-05-09 17:05:18 +0800

    The Dialog is a Card

--- src/components/Dialog/index.jsx
+++ src/components/Dialog/index.jsx
@@ -2,12 +2,17 @@ import styles from './style.postcss';
 
 import React from 'react';
 import Modal from 'react-modal';
+import Card from 'components/Card';
 
 const Dialog = (props) => {
   return <Modal className={styles.Dialog}
       overlayClassName={styles.Overlay}
       isOpen={true}>
-    <p>Modal Content</p>
+    <Card>
+      <Card.Content>
+        <p>Modal Content</p>
+      </Card.Content>
+    </Card>
   </Modal>;
 };
 
