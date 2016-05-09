e8c8f7f74e76e06d4fab7d1e4c413bfc100d5ab5
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-05-09 16:45:51 +0800

    Assigned custom classes to the modal

--- src/components/Dialog/index.jsx
+++ src/components/Dialog/index.jsx
@@ -1,10 +1,13 @@
+import styles from './style.postcss';
+
 import React from 'react';
 import Modal from 'react-modal';
 
 const Dialog = (props) => {
-  return <Modal isOpen={true}>
-    <h1>Modal Content</h1>
-    <p>Etc.</p>
+  return <Modal className={styles.Dialog}
+      overlayClassName={styles.Overlay}
+      isOpen={true}>
+    <p>Modal Content</p>
   </Modal>;
 };
 
