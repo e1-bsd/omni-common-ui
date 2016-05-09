d4e13a014f0106e47126aea6db07e63a9f3f3e7d
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-05-09 15:43:04 +0800

    Removed padding from <Grid />

--- src/components/Grid/Grid/index.jsx
+++ src/components/Grid/Grid/index.jsx
@@ -1,10 +1,14 @@
 import 'flexboxgrid';
+import styles from './style.postcss';
 
 import React from 'react';
+import classnames from 'classnames';
 
-const Grid = (props) =>
-  <div className="container-fluid">
+const Grid = (props) => {
+  const classes = classnames('container-fluid', styles.Grid);
+  return <div className={classes}>
     {props.children}
   </div>;
+};
 
 export default Grid;
