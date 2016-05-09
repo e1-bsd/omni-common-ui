db9888574fbdfa74f1fb49521f5c3405bcd36695
Le Zheng <le.zheng@ef.com>
2016-05-09 14:20:20 +0800

    add form show case

--- sample/components/App/index.jsx
+++ sample/components/App/index.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import ButtonShowcase from 'components/ButtonShowcase';
 import CardShowcase from 'components/CardShowcase';
+import FormShowcase from 'components/FormShowcase';
 
 class App extends Component {
   render() {
     return <div>
       <ButtonShowcase />
       <CardShowcase />
+      <FormShowcase />
     </div>;
   }
 }
---
+++ sample/components/FormShowcase/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Showcase from 'components/Showcase';
+
+class FormShowcase extends Component {
+  render() {
+    return <Showcase title="Form">
+      <div>Hello, This is form show case</div>
+    </Showcase>;
+  }
+}
+
+export default FormShowcase;
