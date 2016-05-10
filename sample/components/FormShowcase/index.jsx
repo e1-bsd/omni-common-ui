963291ed8da1cff8bddd013ebc987d5f2fbd7210
Le Zheng <le.zheng@ef.com>
2016-05-10 16:57:04 +0800

    and on change functionality to text input

--- sample/components/FormShowcase/index.jsx
+++ sample/components/FormShowcase/index.jsx
@@ -5,15 +5,16 @@ import SelectShowcase from './SelectShowcase';
 
 class FormShowcase extends Component {
 
-  handleFormChange() {
-    console.log('form change');
+  handleFormChange(data) {
+    console.log('form change', data);
   }
 
   render() {
     return <Showcase title="Form">
-      <Form onChange={this.handleFormChange}>
+      <Form onChange={(data) => this.handleFormChange(data)}>
         <Form.TextInput name="normal" label="Normal" />
-        <Form.TextInput name="disabled" label="Disabled" disabled="true" />
+        <Form.TextInput name="disabled" label="Disabled" disabled={true} />
+        <Form.TextInput name="required" label="Required Field" required />
         <SelectShowcase />
       </Form>
     </Showcase>;
--- src/components/Form/TextInput/index.jsx
+++ src/components/Form/TextInput/index.jsx
@@ -3,12 +3,19 @@ import styles from './style.postcss';
 import React from 'react';
 import { HOC as FormsyDecorator } from 'formsy-react';
 
-const TextInput = (props) => <div className={styles.TextInput}>
-  <label className={styles.TextInput_label} >{props.label}
-    <input type="text" name={props.name} disabled={props.disabled}  value={props.getValue()}
-           validations={props.validations}
-           validationError={props.validationError} />
-  </label>
-</div>;
+const TextInput = (props) => {
+    return <div className={styles.TextInput}>
+        <label className={styles.TextInput_label} >{props.label}
+            <input type="text" name={props.name} disabled={props.disabled}  value={props.getValue()}
+                   validations={props.validations}
+                   validationError={props.validationError}
+                   onChange={(e) => handleChange(e)}/>
+        </label>
+    </div>;
+
+    function handleChange(e) {
+        props.setValue(e.currentTarget.value);
+    }
+}
 
 export default FormsyDecorator(TextInput);
