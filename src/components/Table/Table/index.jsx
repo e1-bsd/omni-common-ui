26f9e8abf55201e9149ed8f6d1ad4d6b67797542
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-05-11 11:10:27 +0800

    Table receives a prop "extendable" and makes it available in the context

--- src/components/Table/Table/index.jsx
+++ src/components/Table/Table/index.jsx
@@ -1,31 +1,45 @@
 import styles from './style.postcss';
 
-import React from 'react';
+import React, { Component } from 'react';
 import Header from '../Header';
 import Body from '../Body';
 import is from 'is_js';
 
-const Table = (props) => {
-  const hasHeader = is.function(props.header);
+class Table extends Component {
+  constructor(props) {
+    super(props);
+    this.hasHeader = is.function(this.props.header);
+  }
 
-  return <table className={styles.Table}>
-    {renderHeader()}
-    <Body>
-      {props.children}
-    </Body>
-  </table>;
+  getChildContext() {
+    return { expandable: this.props.expandable };
+  }
 
-  function renderHeader() {
-    if (!hasHeader) {
+  render() {
+    return <table className={styles.Table}>
+      {this.renderHeader()}
+      <Body>
+        {this.props.children}
+      </Body>
+    </table>;
+  }
+
+  renderHeader() {
+    if (!this.hasHeader) {
       return;
     }
 
-    return <Header>{props.header()}</Header>;
+    return <Header>{this.props.header()}</Header>;
   }
-};
+}
 
 Table.propTypes = {
   header: React.PropTypes.func,
+  expandable: React.PropTypes.bool,
+};
+
+Table.childContextTypes = {
+  expandable: React.PropTypes.bool,
 };
 
 export default Table;
