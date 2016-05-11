58b93defd035dd7de7721f36ad183706d11684f6
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-05-11 15:48:21 +0800

    Fixed style warnigns

--- sample/components/TableShowcase/index.jsx
+++ sample/components/TableShowcase/index.jsx
@@ -4,9 +4,13 @@ import Showcase from 'components/Showcase';
 
 class TableShowcase extends Component {
   render() {
+    const data = [
+      { name: 'Row one', content: 'These are regular data rows' },
+      { name: 'Row two', content: 'They work like above' },
+    ];
+
     return <Showcase title="Tables">
-      <Table data={[{ name: 'Row one', content: 'These are regular data rows' },
-        { name: 'Row two', content: 'They work like above' },]}/>
+      <Table data={data}/>
     </Showcase>;
   }
 }
--- src/components/Table/Table/index.jsx
+++ src/components/Table/Table/index.jsx
@@ -1,6 +1,6 @@
 import styles from './style.postcss';
 
-import React, { Component } from 'react';
+import React from 'react';
 import Reactable from 'reactable';
 
 const Table = (props) => <Reactable.Table className={styles.Table} {...props} />;
