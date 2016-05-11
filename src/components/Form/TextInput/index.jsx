13fd1c42eedaf784ac0ee9f4041aa2ba496adad8
Le Zheng <le.zheng@ef.com>
2016-05-11 11:26:37 +0800

    add some css for the error element

--- src/components/Form/TextInput/index.jsx
+++ src/components/Form/TextInput/index.jsx
@@ -2,6 +2,7 @@ import styles from './style.postcss';
 
 import React, { Component } from 'react';
 import { HOC as FormsyDecorator } from 'formsy-react';
+import classnames from 'classnames';
 
 class TextInput extends Component {
 
@@ -9,11 +10,21 @@ class TextInput extends Component {
         this.props.setValue(e.currentTarget.value);
     }
 
+    getValidationClasses() {
+        if (this.props.showError()) {
+            return styles.TextInput_error;
+        }
+    }
+
     render() {
         const errorMessage = this.props.getErrorMessage();
+        const textInputElementClasses = classnames(styles.TextInput_element,
+            this.getValidationClasses());
         return <div className={styles.TextInput}>
-            <label className={styles.TextInput_label} >{this.props.label}
+            <label>
+                <span className={styles.TextInput_label}>{this.props.label}</span>
                 <input type="text"
+                    className={textInputElementClasses}
                     name={this.props.name}
                     disabled={this.props.disabled}
                     value={this.props.getValue()}
