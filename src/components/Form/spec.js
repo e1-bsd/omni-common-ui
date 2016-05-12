a7b1daa7e046a7ae1fa8677858a593debe16d5c7
Le Zheng <le.zheng@ef.com>
2016-05-12 15:44:54 +0800

    add form validation unit test

--- src/components/Form/spec.js
+++ src/components/Form/spec.js
@@ -1,4 +1,8 @@
+import styles from './TextInput/style.postcss';
+
+import React from 'react';
 import { expect } from 'chai';
+import { shallow, mount } from 'enzyme';
 import Form from './';
 import FormComponent from './Form';
 import TextInput from './TextInput';
@@ -15,5 +19,15 @@ describe('Form', () => {
 
   it('has a Select property', () => {
     expect(Select).to.equal(Form.Select);
+  });
+
+  it('show error when a input do not meet its email validation', ()=> {
+    const wrapper = mount(<Form>
+      <Form.TextInput name="email"
+         label="Email"
+         validations="isEmail"
+         validationError="This is not a valid email"
+         value='123'/></Form>);
+      expect(wrapper.find(`.${styles.__error}`)).to.have.length(1);
   });
 });
