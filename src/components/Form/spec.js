7749201ec2ac5634ac2a35dc3d267022dbd53c79
Le Zheng <le.zheng@ef.com>
2016-05-12 16:10:53 +0800

    add more cases

--- src/components/Form/spec.js
+++ src/components/Form/spec.js
@@ -2,7 +2,7 @@ import styles from './TextInput/style.postcss';
 
 import React from 'react';
 import { expect } from 'chai';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import Form from './';
 import FormComponent from './Form';
 import TextInput from './TextInput';
@@ -29,5 +29,54 @@ describe('Form', () => {
          validationError="This is not a valid email"
          value='123'/></Form>);
       expect(wrapper.find(`.${styles.__error}`)).to.have.length(1);
+      expect(wrapper.find(`.${styles.TextInput_inputContainer_validationError}`).text()).to.equal('This is not a valid email');
+  });
+
+  it('show no error when a input do meet its email validation', ()=> {
+    const wrapper = mount(<Form>
+      <Form.TextInput name="email"
+                      label="Email"
+                      validations="isEmail"
+                      validationError="This is not a valid email"
+                      value='test@ef.com'/></Form>);
+    expect(wrapper.find(`.${styles.__error}`)).to.have.length(0);
+  });
+
+  it('show error when a input do not meet its isNumeric validation', ()=> {
+    const wrapper = mount(<Form>
+      <Form.TextInput name="number"
+                      label="number"
+                      validations="isNumeric"
+                      validationError="This is not a valid number"
+                      value='abc'/></Form>);
+    expect(wrapper.find(`.${styles.__error}`)).to.have.length(1);
+    expect(wrapper.find(`.${styles.TextInput_inputContainer_validationError}`).text()).to.equal('This is not a valid number');
+  });
+
+  it('show error when a input do not meet its isNumeric validation', ()=> {
+    const wrapper = mount(<Form>
+      <Form.TextInput name="number"
+                      label="number"
+                      validations="isNumeric"
+                      validationError="This is not a valid email"
+                      value='123'/></Form>);
+    expect(wrapper.find(`.${styles.__error}`)).to.have.length(0);
+  });
+
+  it('show required styles when a input is required', ()=> {
+    const wrapper = mount(<Form>
+      <Form.TextInput name="required"
+                      label="required"
+                      required
+                      value=''/></Form>);
+    expect(wrapper.find(`.${styles.__required}`)).to.have.length(1);
+  });
+
+  it('show required styles when a input is required', ()=> {
+    const wrapper = mount(<Form>
+      <Form.TextInput name="required"
+                      label="required"
+                      value=''/></Form>);
+    expect(wrapper.find(`.${styles.__required}`)).to.have.length(0);
   });
 });
