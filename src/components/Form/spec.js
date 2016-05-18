c73fd01176d0a6111ccc52abc9fb2a111ceb5609
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-05-18 10:09:51 +0800

    Fixed code style issues in Form/spec.js

--- src/components/Form/spec.js
+++ src/components/Form/spec.js
@@ -21,34 +21,37 @@ describe('Form', () => {
     expect(Select).to.equal(Form.Select);
   });
 
-  it('show error when a input do not meet its email validation', ()=> {
+  it('show error when a input do not meet its email validation', () => {
     const wrapper = mount(<Form>
       <Form.TextInput name="email"
-         label="Email"
-         validations="isEmail"
-         validationError="This is not a valid email"
-         value='123'/></Form>);
-      expect(wrapper.find(`.${styles.__error}`)).to.have.length(1);
-      expect(wrapper.find(`.${styles.TextInput_inputContainer_validationError}`).text()).to.equal('This is not a valid email');
+          label="Email"
+          validations="isEmail"
+          validationError="This is not a valid email"
+          value="123" />
+    </Form>);
+    expect(wrapper.find(`.${styles.__error}`)).to.have.length(1);
+    expect(wrapper.find(`.${styles.TextInput_inputContainer_validationError}`).text()).to.equal('This is not a valid email');
   });
 
   it('show no error when a input do meet its email validation', ()=> {
     const wrapper = mount(<Form>
       <Form.TextInput name="email"
-                      label="Email"
-                      validations="isEmail"
-                      validationError="This is not a valid email"
-                      value='test@ef.com'/></Form>);
+          label="Email"
+          validations="isEmail"
+          validationError="This is not a valid email"
+          value="test@ef.com" />
+    </Form>);
     expect(wrapper.find(`.${styles.__error}`)).to.have.length(0);
   });
 
   it('show error when a input do not meet its isNumeric validation', ()=> {
     const wrapper = mount(<Form>
       <Form.TextInput name="number"
-                      label="number"
-                      validations="isNumeric"
-                      validationError="This is not a valid number"
-                      value='abc'/></Form>);
+          label="number"
+          validations="isNumeric"
+          validationError="This is not a valid number"
+          value="abc" />
+    </Form>);
     expect(wrapper.find(`.${styles.__error}`)).to.have.length(1);
     expect(wrapper.find(`.${styles.TextInput_inputContainer_validationError}`).text()).to.equal('This is not a valid number');
   });
@@ -56,29 +59,30 @@ describe('Form', () => {
   it('show error when a input do not meet its isNumeric validation', ()=> {
     const wrapper = mount(<Form>
       <Form.TextInput name="number"
-                      label="number"
-                      validations="isNumeric"
-                      validationError="This is not a valid email"
-                      value='123'/></Form>);
+          label="number"
+          validations="isNumeric"
+          validationError="This is not a valid email"
+          value="123" />
+    </Form>);
     expect(wrapper.find(`.${styles.__error}`)).to.have.length(0);
   });
 
   it('show required styles when a input is required', ()=> {
     const wrapper = mount(<Form>
       <Form.TextInput name="required"
-                      label="required"
-                      required
-                      value=''/></Form>);
+          label="required"
+          required
+          value="" />
+    </Form>);
     expect(wrapper.find(`.${styles.__required}`)).to.have.length(1);
   });
 
   it('show required styles when a input is required', ()=> {
     const wrapper = mount(<Form>
       <Form.TextInput name="required"
-                      label="required"
-                      value=''/></Form>);
+          label="required"
+          value="" />
+    </Form>);
     expect(wrapper.find(`.${styles.__required}`)).to.have.length(0);
   });
-
-
 });
