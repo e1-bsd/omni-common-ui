f29618dff768fe5ea3ee065d3a121b004d5c760f
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-05-18 11:41:01 +0800

    Simplified tests in Form/spec.jsx now that that behaviour is tested in Field/spec.jsx

--- src/components/Form/spec.jsx
+++ src/components/Form/spec.jsx
@@ -7,6 +7,7 @@ import Form from './';
 import FormComponent from './Form';
 import TextInput from './TextInput';
 import Select from './Select';
+import Field from './Field';
 
 describe('Form', () => {
   it('can be used directly as Form component', () => {
@@ -21,68 +22,22 @@ describe('Form', () => {
     expect(Select).to.equal(Form.Select);
   });
 
-  it('show error when a input do not meet its email validation', () => {
+  it('does not expose Field component', () => {
+    expect(Field).to.not.equal(Form.Field);
+  });
+
+  it('works along with Formsy', () => {
+    const errorMessage = 'This is not a valid email';
     const wrapper = mount(<Form>
       <Form.TextInput name="email"
           label="Email"
           validations="isEmail"
-          validationError="This is not a valid email"
+          validationError={errorMessage}
           value="123" />
     </Form>);
+    const errorClass = styles.Field_inputContainer_validationError;
+    const errorText = wrapper.find(`.${errorClass}`).text();
     expect(wrapper.find(`.${styles.__error}`)).to.have.length(1);
-    expect(wrapper.find(`.${styles.Field_inputContainer_validationError}`).text()).to.equal('This is not a valid email');
-  });
-
-  it('show no error when a input do meet its email validation', ()=> {
-    const wrapper = mount(<Form>
-      <Form.TextInput name="email"
-          label="Email"
-          validations="isEmail"
-          validationError="This is not a valid email"
-          value="test@ef.com" />
-    </Form>);
-    expect(wrapper.find(`.${styles.__error}`)).to.have.length(0);
-  });
-
-  it('show error when a input do not meet its isNumeric validation', ()=> {
-    const wrapper = mount(<Form>
-      <Form.TextInput name="number"
-          label="number"
-          validations="isNumeric"
-          validationError="This is not a valid number"
-          value="abc" />
-    </Form>);
-    expect(wrapper.find(`.${styles.__error}`)).to.have.length(1);
-    expect(wrapper.find(`.${styles.Field_inputContainer_validationError}`).text()).to.equal('This is not a valid number');
-  });
-
-  it('show error when a input do not meet its isNumeric validation', ()=> {
-    const wrapper = mount(<Form>
-      <Form.TextInput name="number"
-          label="number"
-          validations="isNumeric"
-          validationError="This is not a valid email"
-          value="123" />
-    </Form>);
-    expect(wrapper.find(`.${styles.__error}`)).to.have.length(0);
-  });
-
-  it('show required styles when a input is required', ()=> {
-    const wrapper = mount(<Form>
-      <Form.TextInput name="required"
-          label="required"
-          required
-          value="" />
-    </Form>);
-    expect(wrapper.find(`.${styles.__required}`)).to.have.length(1);
-  });
-
-  it('show required styles when a input is required', ()=> {
-    const wrapper = mount(<Form>
-      <Form.TextInput name="required"
-          label="required"
-          value="" />
-    </Form>);
-    expect(wrapper.find(`.${styles.__required}`)).to.have.length(0);
+    expect(errorText).to.equal(errorMessage);
   });
 });
