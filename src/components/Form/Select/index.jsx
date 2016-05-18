b134f3a8f043acfae27c08c6ee36c6d096f8cdaa
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-05-18 14:14:44 +0800

    Refactored Select and TextInput into functional components

--- src/components/Form/Select/index.jsx
+++ src/components/Form/Select/index.jsx
@@ -1,24 +1,22 @@
 import 'react-select/dist/react-select.css';
 import styles from './style.postcss';
 
-import React, { Component } from 'react';
+import React from 'react';
 import ReactSelect from 'react-select';
 import { HOC as FormsyDecorator } from 'formsy-react';
 import classnames from 'classnames';
 import Field from '../Field';
 
-class Select extends Component {
-  render() {
-    const classes = classnames(styles.Select_element,
-        { [styles.__required]: this.props.showRequired() },
-        { [styles.__error]: this.props.showError() });
-    return <Field label={this.props.label}
-        getErrorMessage={() => this.props.getErrorMessage()}
-        showError={() => this.props.showError()}
-        showRequired={() => this.props.showRequired()}>
-      <ReactSelect className={classes} {...this.props} />
-    </Field>;
-  }
-}
+const Select = (props) => {
+  const classes = classnames(styles.Select_element,
+      { [styles.__required]: props.showRequired() },
+      { [styles.__error]: props.showError() });
+  return <Field label={props.label}
+      getErrorMessage={() => props.getErrorMessage()}
+      showError={() => props.showError()}
+      showRequired={() => props.showRequired()}>
+    <ReactSelect className={classes} {...props} />
+  </Field>;
+};
 
 export default FormsyDecorator(Select);
--- src/components/Form/TextInput/index.jsx
+++ src/components/Form/TextInput/index.jsx
@@ -1,31 +1,30 @@
 import styles from './style.postcss';
 
-import React, { Component } from 'react';
+import React from 'react';
 import { HOC as FormsyDecorator } from 'formsy-react';
 import classnames from 'classnames';
 import Field from '../Field';
 
-class TextInput extends Component {
-  handleChange(e) {
-    this.props.setValue(e.currentTarget.value);
-  }
+const TextInput = (props) => {
+  const classes = classnames(styles.TextInput,
+      { [styles.__required]: props.showRequired() },
+      { [styles.__error]: props.showError() });
 
-  render() {
-    const classes = classnames(styles.TextInput,
-        { [styles.__required]: this.props.showRequired() },
-        { [styles.__error]: this.props.showError() });
-    return <Field label={this.props.label}
-        getErrorMessage={() => this.props.getErrorMessage()}
-        showError={() => this.props.showError()}
-        showRequired={() => this.props.showRequired()}>
-      <input type="text"
-          className={classes}
-          name={this.props.name}
-          disabled={this.props.disabled}
-          value={this.props.getValue()}
-          onChange={(e) => this.handleChange(e)} />
-    </Field>;
+  return <Field label={props.label}
+      getErrorMessage={() => props.getErrorMessage()}
+      showError={() => props.showError()}
+      showRequired={() => props.showRequired()}>
+    <input type="text"
+        className={classes}
+        name={props.name}
+        disabled={props.disabled}
+        value={props.getValue()}
+        onChange={(e) => handleChange(e)} />
+  </Field>;
+
+  function handleChange(e) {
+    props.setValue(e.currentTarget.value);
   }
-}
+};
 
 export default FormsyDecorator(TextInput);
