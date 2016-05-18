cd38cde2af1f63b8d624728fe9f776c493a46734
Le Zheng <le.zheng@ef.com>
2016-05-18 18:09:41 +0800

    add some basic css for top navigation component

--- src/components/TopNav/TopNav/index.jsx
+++ src/components/TopNav/TopNav/index.jsx
@@ -1,6 +1,8 @@
+import styles from './style.postcss';
+
 import React from 'react';
 
-const TopNav = (props) => <div { ...props }>
+const TopNav = (props) => <div className={styles.TopNav} { ...props }>
   Hi, This is Top Nav
 </div>;
 
