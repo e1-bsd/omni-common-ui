9bff4c6cf87b722ecf89f657b2a9d134ba0aa23b
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-05-19 11:19:00 +0800

    Got rid of warnings

--- src/components/Table/Expandable/index.jsx
+++ src/components/Table/Expandable/index.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import Table from '../Table';
 import Reactable from 'reactable';
+import log from 'loglevel';
 
-const Expandable = (props) => {
-  return <Table>
+const Expandable = (props) =>
+  <Table>
     {
       props.data.map((row) => {
-        console.log('row', row);
+        log.info('row', row);
         return <Reactable.Tr>
           {
-            Object.keys(row).map(function(column, index) {
+            Object.keys(row).map((column, index) => {
               const value = row[column];
-              return <Reactable.Td column={column} value={value} key={index} />
-            })
+              return <Reactable.Td column={column} value={value} key={index} />;
+            }).concat(<Reactable.Td column="expand" value="hey" />)
           }
         </Reactable.Tr>;
       })
     }
   </Table>;
-};
 
 Expandable.propTypes = {
   data: React.PropTypes.array.isRequired,
