80ace6af1d8aa255ab091041c1204860051048f6
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-05-19 17:13:02 +0800

    More extendable Table

--- src/components/Table/Table/index.jsx
+++ src/components/Table/Table/index.jsx
@@ -2,7 +2,12 @@ import styles from './style.postcss';
 
 import React from 'react';
 import Reactable from 'reactable';
+import classnames from 'classnames';
 
-const Table = (props) => <Reactable.Table className={styles.Table} {...props} />;
+const Table = (props) => <Reactable.Table columns={props.columns}
+    data={props.data}
+    className={classnames(styles.Table, props.className)}>
+  {props.children}
+</Reactable.Table>;
 
 export default Table;
