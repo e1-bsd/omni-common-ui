8e957db4ab486b821d88af655f5fc11e4cb5c959
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-05-20 11:18:46 +0800

    Using ExpandableContainer inside of Expandable

--- src/components/Table/Expandable/index.jsx
+++ src/components/Table/Expandable/index.jsx
@@ -1,26 +1,53 @@
-import React from 'react';
+import styles from './style.postcss';
+
+import React, { Component } from 'react';
 import Table from '../Table';
 import Reactable from 'reactable';
+import ExpandedContainer from './ExpandedContainer';
 
 const COLUMN_EXPAND_KEY = 'expand';
 const COLUMN_EXPAND_DEF = { key: COLUMN_EXPAND_KEY, label: '' };
 
-const Expandable = (props) => {
-  let { columns } = props;
-  columns = columns.concat(COLUMN_EXPAND_DEF);
+class Expandable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-  return <Table columns={columns}>{props.data.map(renderRow)}</Table>;
+  render() {
+    let { columns } = this.props;
+    columns = columns.concat(COLUMN_EXPAND_DEF);
 
-  function renderRow(row, index) {
+    return <Table className={styles.ExpandTable}
+        columns={columns}>
+      {this.props.data.map((row, index) => this.renderRow(row, index))}
+    </Table>;
+  }
+
+  renderRow(row, index) {
     return <Reactable.Tr key={index}>
-      {Object.keys(row).map((column) => renderCell(row, column))}
-      <Reactable.Td column={COLUMN_EXPAND_KEY}>Expand</Reactable.Td>
+      {Object.keys(row).map((column) => this.renderCell(row, column))}
+      <Reactable.Td column={COLUMN_EXPAND_KEY}>
+        <div>
+          <span>Expand</span>
+          <div style={{ height: this.state.expandedHeight }}></div>
+          {this.renderExpandedContent(index === 0)}
+        </div>
+      </Reactable.Td>
     </Reactable.Tr>;
   }
 
-  function renderCell(row, column) {
+  renderCell(row, column) {
     const value = row[column];
     return <Reactable.Td column={column} key={column}>{value}</Reactable.Td>;
+  }
+
+  renderExpandedContent(render) {
+    if (render !== true) {
+      return;
+    }
+
+    return <ExpandedContainer onHeightChanged={(height) => this.setState({ expandedHeight: height })} />;
   }
 };
 
