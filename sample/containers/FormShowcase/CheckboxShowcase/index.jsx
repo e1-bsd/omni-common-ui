cb4ddbbe5b5169fc87ebf1c8492633c4c4fd31e0
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-05-27 15:19:33 +0800

    Fixed more warnings from ESLint

--- sample/containers/FormShowcase/CheckboxShowcase/index.jsx
+++ sample/containers/FormShowcase/CheckboxShowcase/index.jsx
@@ -1,13 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Form } from 'omni-common-ui';
 
-class CheckboxShowcase extends Component {
-
-  render() {
-    return <div>
-      <Form.Checkbox name="checkBoxNormal" label="Normal Checkbox" items={['foo', 'bar']}/>
-    </div>;
-  }
-}
+const CheckboxShowcase = () => <div>
+  <Form.Checkbox name="checkBoxNormal"
+      label="Normal Checkbox"
+      items={['foo', 'bar']} />
+</div>;
 
 export default CheckboxShowcase;
--- sample/containers/FormShowcase/RadioShowcase/index.jsx
+++ sample/containers/FormShowcase/RadioShowcase/index.jsx
@@ -1,13 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Form } from 'omni-common-ui';
 
-class RadioShowcase extends Component {
-
-  render() {
-    return <div>
-      <Form.Radio name="radio" label="Radio" items={['foo', 'bar']}></Form.Radio>
-    </div>;
-  }
-}
+const RadioShowcase = () => <div>
+  <Form.Radio name="radio" label="Radio" items={['foo', 'bar']}></Form.Radio>
+</div>;
 
 export default RadioShowcase;
