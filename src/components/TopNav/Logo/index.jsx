bacac0ee4d8cd32e1b50a2af71627bcfb411ed5b
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-05-27 13:52:52 +0800

    Fixed warnings in TopNav.Logo

--- src/components/TopNav/Logo/index.jsx
+++ src/components/TopNav/Logo/index.jsx
@@ -4,49 +4,32 @@ import React, { Component } from 'react';
 import { mediaQuery } from 'domain/MediaQuery';
 
 class Logo extends Component {
-
   constructor(props) {
     super(props);
     this.xsQuery = mediaQuery('(min-width: 20em)');
     this.smQuery = mediaQuery('(min-width: 30em)');
     this.mdQuery = mediaQuery('(min-width: 40em)');
     this.state = {
-      xs: this.xsQuery.matches ? false : true,
-      sm: this.smQuery.matches ? false : true,
-      md: this.mdQuery.matches ? false : true,
+      xs: this.xsQuery.matches,
+      sm: this.smQuery.matches,
+      md: this.mdQuery.matches,
     };
   }
 
   componentDidMount() {
-    this.xsQuery.addListener((mql) => {
-      if (mql.matches) {
-        this.setState({ xs: false });
-      } else {
-        this.setState({ xs: true });
-      }
-    });
-    this.smQuery.addListener((mql) => {
-      if (mql.matches) {
-        this.setState({ sm: false });
-      } else {
-        this.setState({ sm: true });
-      }
-    });
-    this.mdQuery.addListener((mql) => {
-      if (mql.matches) {
-        this.setState({ md: false });
-      } else {
-        this.setState({ md: true });
-      }
-    });
+    this.xsQuery.addListener((mql) => this.setState({ xs: mql.matches }));
+    this.smQuery.addListener((mql) => this.setState({ sm: mql.matches }));
+    this.mdQuery.addListener((mql) => this.setState({ md: mql.matches }));
   }
 
   render() {
     if (!this.state.md) {
-      return <span className={styles.Logo} { ...this.props }><img src={this.props.src}/></span>;
-    } else {
-      return <span className={styles.Logo_sideBar}></span>;
+      return <span className={styles.Logo} { ...this.props }>
+        <img src={this.props.src}/>
+      </span>;
     }
+
+    return <span className={styles.Logo_sideBar}></span>;
   }
 
 }
