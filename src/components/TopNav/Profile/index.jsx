1f09c3054b09a3f5075fcc85744c4f8ea4352716
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-05-30 17:15:56 +0800

    Refactored CSS for TopNav a bit

--- src/components/TopNav/Profile/index.jsx
+++ src/components/TopNav/Profile/index.jsx
@@ -2,6 +2,8 @@ import styles from './style.postcss';
 
 import React from 'react';
 
-const Profile = (props) => <span className={styles.Profile} { ...props } />;
+const Profile = (props) => <div className={styles.Profile}>
+  <span className={styles.Profile_picture} { ...props } />
+</div>;
 
 export default Profile;
