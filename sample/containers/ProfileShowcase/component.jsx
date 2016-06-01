38ae35af64ac29742e4be3b612d78e4f59a5ca87
Le Zheng <le.zheng@ef.com>
2016-06-01 10:20:06 +0800

    use one expample which src is url

--- sample/containers/ProfileShowcase/component.jsx
+++ sample/containers/ProfileShowcase/component.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Profile } from 'omni-common-ui';
 import Showcase from 'components/Showcase';
-import yeahImage from './1.png';
 import stupidImage from './2.png';
 
 const ProfileShowcase = () => <Showcase title="Profile" titleLink="profile">
   <div>
-    <Profile src={yeahImage} />
+    <Profile src="http://7xk9wr.com1.z0.glb.clouddn.com/10.png" />
     <Profile src={stupidImage} disabled />
   </div>
 </Showcase>;
