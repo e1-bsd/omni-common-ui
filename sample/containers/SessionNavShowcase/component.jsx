be64842e0b0ebcfae78518502ef46486da294927
Le Zheng <le.zheng@ef.com>
2016-06-02 17:24:23 +0800

    add some functionality for sessionNav

--- sample/containers/SessionNavShowcase/component.jsx
+++ sample/containers/SessionNavShowcase/component.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Showcase from 'components/Showcase';
 import { SessionNav } from 'omni-common-ui';
 
-const SessionNavShowcase = () => <Showcase title="Session Navigation" titleLink="session-nav">
-  <div>
-    <SessionNav />
-  </div>
-</Showcase>;
+class SessionNavShowcase extends Component {
+
+  constructor(props) {
+    super(props);
+    const sessions = [{ name: 'Unit 1', date: 'Saturday, 4 June 2016' },
+      { name: 'Unit 2', date: 'Saturday, 11 June 2016' },
+      { name: 'Unit 3', date: 'Saturday, 18 June 2016' }];
+    this.state = { sessions, current: 1 };
+  }
+
+  handleBack() {
+    this.setState({ current: this.state.current - 1 });
+  }
+
+  handleForward() {
+    this.setState({ current: this.state.current + 1 });
+  }
+
+  render() {
+    const currentSession = this.state.sessions[this.state.current];
+    return <Showcase title="Session Navigation" titleLink="session-nav">
+      <div>
+        <SessionNav name={currentSession.name}
+            date={currentSession.date}
+            onBack={() => this.handleBack()}
+            onForward={() => this.handleForward()} />
+      </div>
+    </Showcase>;
+  }
+
+}
 
 export default SessionNavShowcase;
--- src/components/SessionNav/index.jsx
+++ src/components/SessionNav/index.jsx
@@ -1,4 +1,24 @@
-import React from 'react';
+import styles from './style.postcss';
 
-const SessionNav = () => <div>Session Nav, Yo</div>;
+import React from 'react';
+import back from './back.png';
+import forward from './forward.png';
+
+const SessionNav = (props) =>
+  <div className={styles.SessionNav}>
+    <img src={back} role="presentation" onClick={() => props.onBack()} />
+    <ul className={styles.SessionNav_content}>
+      <li>{props.name}</li>
+      <li>{props.date}</li>
+    </ul>
+    <img src={forward} role="presentation" onClick={() => props.onForward()} />
+  </div>;
+
+SessionNav.propTypes = {
+  name: React.PropTypes.string,
+  date: React.PropTypes.string,
+  onBack: React.PropTypes.func,
+  onForward: React.PropTypes.func,
+};
+
 export default SessionNav;
