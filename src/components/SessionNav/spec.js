a03faa853ba69afbb0f9a05c51e41aa19290a5dd
sunny.yu2 <sunny.yu2@EF.com>
2016-06-03 16:39:46 +0800

    add more unit test for SessionNav

--- src/components/SessionNav/spec.js
+++ src/components/SessionNav/spec.js
@@ -18,14 +18,44 @@ describe('SessionNav', () => {
     expect(wrapper.find(`.${styles.SessionNav_emptyBtn}`)).to.have.length(1);
   });
 
+  it('display forward&back button in the second item', () => {
+    const current = 1;
+    const wrapper = shallow(<SessionNav items={sessions} current={current} />);
+    expect(wrapper.find(`.${styles.SessionNav}`)).to.have.length(1);
+    expect(wrapper.find(`.${styles.SessionNav_forward}`)).to.have.length(1);
+    expect(wrapper.find(`.${styles.SessionNav_back}`)).to.have.length(1);
+    expect(wrapper.find(`.${styles.SessionNav_emptyBtn}`)).to.have.length(0);
+  });
+
+  it('hide the forward button in the last item', () => {
+    const current = 2;
+    const wrapper = shallow(<SessionNav items={sessions} current={current} />);
+    expect(wrapper.find(`.${styles.SessionNav}`)).to.have.length(1);
+    expect(wrapper.find(`.${styles.SessionNav_forward}`)).to.have.length(0);
+    expect(wrapper.find(`.${styles.SessionNav_back}`)).to.have.length(1);
+    expect(wrapper.find(`.${styles.SessionNav_emptyBtn}`)).to.have.length(1);
+  });
+
   it('back and forward btn callback is working', () => {
     const wrapper = mount(<SessionNav items={sessions}
-        current={0}
-        onForward={() => wrapper.setProps({ current: 1 })}
-        onBack={() => wrapper.setProps({ current: 2 })} />);
+        current={1}
+        onForward={() => wrapper.setProps({ current: 2 })}
+        onBack={() => wrapper.setProps({ current: 0 })} />);
     wrapper.find(`.${styles.SessionNav_forward}`).simulate('click');
-    expect(wrapper.prop('current')).to.eql(1);
-    wrapper.find(`.${styles.SessionNav_back}`).simulate('click');
     expect(wrapper.prop('current')).to.eql(2);
+    wrapper.find(`.${styles.SessionNav_back}`).simulate('click');
+    expect(wrapper.prop('current')).to.eql(0);
+  });
+});
+
+describe('SessionNav', () => {
+  const sessions = [{ name: 'Unit 1', date: 'Saturday, 4 June 2016' }];
+  it('hide the back button in the first item', () => {
+    const current = 0;
+    const wrapper = shallow(<SessionNav items={sessions} current={current} />);
+    expect(wrapper.find(`.${styles.SessionNav}`)).to.have.length(1);
+    expect(wrapper.find(`.${styles.SessionNav_forward}`)).to.have.length(0);
+    expect(wrapper.find(`.${styles.SessionNav_back}`)).to.have.length(0);
+    expect(wrapper.find(`.${styles.SessionNav_emptyBtn}`)).to.have.length(2);
   });
 });
