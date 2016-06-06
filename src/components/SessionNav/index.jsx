62fcc0ffe174a53ffc2aedbb4d8bf97103784f06
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-06-06 14:02:17 +0800

    Refactored SessionNav/index.jsx

--- src/components/SessionNav/index.jsx
+++ src/components/SessionNav/index.jsx
@@ -5,26 +5,33 @@ import React from 'react';
 const SessionNav = (props) => {
   const { current, onBack, onForward, items } = props;
   const currentSession = items[current];
+
   return <div className={styles.SessionNav}>
-    {renderBack(current, onBack)}
+    {renderBack()}
     <ul className={styles.SessionNav_content}>
       <li>{currentSession.name}</li>
       <li>{currentSession.date}</li>
     </ul>
-    {renderForward(current, onForward)}
+    {renderForward()}
   </div>;
 
   function renderBack() {
-    return current === 0 ?
-      <span className={styles.SessionNav_emptyBtn}>&nbsp;</span> :
-      <a className={styles.SessionNav_back} onClick={() => onBack()}></a>;
+    if (current === 0) {
+      return <span className={styles.SessionNav_emptyBtn}>&nbsp;</span>;
+    }
+
+    return <a className={styles.SessionNav_back} onClick={() => onBack()} />;
   }
+
   function renderForward() {
-    return current + 1 === items.length ?
-      <span className={styles.SessionNav_emptyBtn}>&nbsp;</span> :
-      <a className={styles.SessionNav_forward} onClick={() => onForward()}></a>;
+    if (current + 1 === items.length) {
+      return <span className={styles.SessionNav_emptyBtn}>&nbsp;</span>;
+    }
+
+    return <a className={styles.SessionNav_forward} onClick={() => onForward()} />;
   }
 };
+
 SessionNav.propTypes = {
   items: React.PropTypes.array,
   current: React.PropTypes.number,
