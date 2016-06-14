30c4b52a53f8d31b00e8ebcd2c01337ed36f62a3
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-06-14 14:40:05 +0800

    build.js passes ESLint checks

--- build.js
+++ build.js
@@ -1,20 +1,21 @@
+/* eslint strict: "off" */
 'use strict';
 
-let exec = require('child_process').exec;
-let path = require('path');
+const exec = require('child_process').exec;
+const path = require('path');
 
 let cmdLine = path.join('.', 'node_modules', '.bin', 'webpack');
 cmdLine += ' -p --bail --progress --colors';
 
-let env = !!process.argv[2] ? process.argv[2].toLowerCase() : 'production';
+const env = !!process.argv[2] ? process.argv[2].toLowerCase() : 'production';
 
 if (process.platform === 'win32') {
-  cmdLine = 'set NODE_ENV=' + env + '&& ' + cmdLine;
+  cmdLine = `set NODE_ENV=${env}&& ${cmdLine}`;
 } else {
-  cmdLine = 'NODE_ENV=' + env + ' ' + cmdLine;
+  cmdLine = `NODE_ENV=${env} ${cmdLine}`;
 }
 
-let command = exec(cmdLine, error => process.exit(error === null ? 0 : error.code));
+const command = exec(cmdLine, error => process.exit(error === null ? 0 : error.code));
 
 command.stdout.on('data', data => process.stdout.write(data));
 command.stderr.on('data', data => process.stderr.write(data));
