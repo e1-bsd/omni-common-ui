4329e9cfd82af85d2f5a2aff91d96e56798542cb
Luke Plaster <me@lukep.org>
2016-07-14 14:39:14 +0800

    Made student card content left-pad optional

--- sample/containers/StudentCardShowcase/component.jsx
+++ sample/containers/StudentCardShowcase/component.jsx
@@ -52,7 +52,7 @@ const StudentCardShowcase = () => <Showcase title="Student Cards" titleLink="stu
         </StudentCard>
       </Grid.Item>
       <Grid.Item xs={12} sm={6} md={4} lg={3}>
-        <StudentCard statusAccentColor="grey" withSeparatorLine>
+        <StudentCard withLeftPaddedContentArea statusAccentColor="grey" withSeparatorLine>
           <StudentCard.Profile name="吴君亮"
               gender={0} avatarUrl={testImage} />
           <StudentCard.Content>
--- src/components/StudentCard/Content/index.jsx
+++ src/components/StudentCard/Content/index.jsx
@@ -3,20 +3,20 @@ import styles from './style.postcss';
 import React from 'react';
 import classnames from 'classnames';
 
-const getClassNames = (isShowingStatusAccent) => {
-  if (isShowingStatusAccent) {
+const getClassNames = (isLeftPaddingRequiredInContentArea) => {
+  if (isLeftPaddingRequiredInContentArea) {
     return classnames(styles.Content, styles.__withLeftPadding);
   }
   return classnames(styles.Content);
 };
 
-const Content = (props, { isShowingStatusAccent }) => <div className={
-    getClassNames(isShowingStatusAccent)}>
+const Content = (props, { isLeftPaddingRequiredInContentArea }) => <div className={
+    getClassNames(isLeftPaddingRequiredInContentArea)}>
   {props.children}
 </div>;
 
 Content.contextTypes = {
-  isShowingStatusAccent: React.PropTypes.bool,
+  isLeftPaddingRequiredInContentArea: React.PropTypes.bool,
 };
 
 Content.propTypes = {
