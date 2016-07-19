3fe7fb6b6ccd51e731145f74eb7720a0b921ee8c
Luke Plaster <me@lukep.org>
2016-07-19 13:43:32 +0800

    Added active and autoWidth button props

--- sample/containers/ButtonShowcase/component.jsx
+++ sample/containers/ButtonShowcase/component.jsx
@@ -5,7 +5,13 @@ import log from 'loglevel';
 
 const ButtonShowcase = () => <Showcase title="Buttons" titleLink="buttons">
   <div>
-    <Button onClick={() => log.info('Normal button clicked')}>Normal</Button>
+    <Button onClick={() => log.info('Normal button clicked')}>
+      Normal
+    </Button>
+    <Button active
+        onClick={() => log.info('Active button clicked')}>
+      Active
+    </Button>
     <Button disabled
         onClick={() => log.info('Disabled button clicked')}>
       Disabled
@@ -15,6 +21,11 @@ const ButtonShowcase = () => <Showcase title="Buttons" titleLink="buttons">
     <Button type={Button.Type.primary}
         onClick={() => log.info('Primary button clicked')}>
       Primary
+    </Button>
+    <Button active
+        type={Button.Type.primary}
+        onClick={() => log.info('Active primary button clicked')}>
+      Active primary
     </Button>
     <Button disabled
         type={Button.Type.primary}
@@ -34,8 +45,45 @@ const ButtonShowcase = () => <Showcase title="Buttons" titleLink="buttons">
     </Button>
   </div>
   <div>
-    <Button onClick={() => log.info('Block button clicked')} block>
+    <Button onClick={() => log.info('Block button clicked')}
+        block>
       Block button
+    </Button>
+  </div>
+  <div>
+    <Button block type={Button.Type.primary}
+        onClick={() => log.info('Primary block button clicked')}>
+      Primary block button
+    </Button>
+  </div>
+  <div>
+    <Button active block
+        onClick={() => log.info('Active block button clicked')}>
+      Active block button
+    </Button>
+  </div>
+  <div>
+    <Button disabled block
+        onClick={() => log.info('Disabled block button clicked')}>
+      Disabled block button
+    </Button>
+  </div>
+  <div>
+    <Button autoWidth
+        onClick={() => log.info('Auto width button clicked')}>
+      Auto width button
+    </Button>
+    <Button autoWidth type={Button.Type.primary}
+        onClick={() => log.info('Primary auto width button clicked')}>
+      Primary auto width button
+    </Button>
+    <Button active autoWidth
+        onClick={() => log.info('Active auto width button clicked')}>
+      Active auto width button
+    </Button>
+    <Button disabled autoWidth
+        onClick={() => log.info('Disabled auto width button clicked')}>
+      Disabled auto width button
     </Button>
   </div>
 </Showcase>;
