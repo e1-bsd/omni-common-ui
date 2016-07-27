b24ef8c428566f10fc3f015b8b5c09639b190ae9
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-07-27 15:59:29 +0800

    Updated Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const gulp = require('gulp');
 const del = require('del');
 const postcss = require('gulp-postcss');
@@ -8,34 +9,33 @@ const sequence = require('gulp-sequence');
 
 gulp.task('clean', () => del(['lib']));
 
-gulp.task('build:css', () => gulp.src('src/**/*.postcss')
+gulp.task('copy:all', () => gulp.src('src/**/*')
+    .pipe(gulp.dest('lib')));
+
+gulp.task('build:css', () => gulp.src('lib/**/*.postcss')
     .pipe(sourcemaps.init())
     .pipe(postcss([postcssImport({
-      path: ['node_modules', 'src', 'src/assets/styles', './'],
+      path: ['node_modules', 'lib', 'lib/assets/styles', './'],
     })]))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('lib')));
 
-gulp.task('build:js', () => gulp.src(['src/**/*.js', 'src/**/*.jsx'])
+gulp.task('build:js', () => gulp.src(['lib/**/*.js', 'lib/**/*.jsx'])
     .pipe(sourcemaps.init())
     .pipe(babel({
       presets: ['react', 'es2015', 'stage-2'],
-      plugins: [['resolver', { resolveDirs: ['src'] }]],
+      plugins: [['resolver', { resolveDirs: [path.resolve('lib')] }]],
     }))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('lib')));
 
-gulp.task('build:rest', () => gulp.src([
-  'src/**/*.*',
-  '!src/**/*.jsx',
-  '!src/**/*.js',
-  '!src/**/*.postcss',
-])
-    .pipe(gulp.dest('lib')));
+gulp.task('remove:jsx', () => del(['lib/**/*.jsx']));
 
 gulp.task('build', sequence(
   'clean',
-  ['build:css', 'build:js', 'build:rest']
+  'copy:all',
+  ['build:css', 'build:js'],
+  'remove:jsx'
 ));
 
 gulp.task('default', ['build']);
