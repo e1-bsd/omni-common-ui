ea40eb70fce46b925d5e63f60f384a055713bc14
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-08-05 10:50:28 +0800

    Changed version "-develop" to "-dev"

--- ci.bump-version.js
+++ ci.bump-version.js
@@ -12,5 +12,5 @@ if (process.env.TRAVIS_BRANCH !== 'develop') {
 const execSync = require('child_process').execSync;
 const version = require('./package.json').version;
 let newVersion = version.match(/^\d+.\d+.\d+/)[0];
-newVersion = `${newVersion}-${process.env.TRAVIS_BRANCH}.${process.env.TRAVIS_BUILD_NUMBER}`;
+newVersion = `${newVersion}-dev.${process.env.TRAVIS_BUILD_NUMBER}`;
 execSync(`npm version ${newVersion}`);
