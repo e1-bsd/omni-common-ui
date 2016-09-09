aa58da90598362ef56b8e3ec7324e89311a430ca
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-09-09 13:59:16 +0800

    Added log

--- src/components/Header/component.jsx
+++ src/components/Header/component.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classnames from 'classnames';
 import style from './style.postcss';
 import UserInfo, { getImpersonate } from 'containers/UserInfo';
+import log from 'loglevel';
 
 class Header extends Component {
   constructor(props) {
@@ -16,12 +17,11 @@ class Header extends Component {
   }
 
   render() {
-    return <div className={classnames(
-        style.header,
-        this.state.impersonateData ? style.impersonate : null)}>
-      <div className={classnames(
-          style.header_logo,
-          this.state.impersonateData ? style.impersonate : null)} />
+    log.debug('Header - impersonateData', this.state.impersonateData);
+    return <div className={classnames(style.header,
+        { [style.impersonate]: this.state.impersonateData })}>
+      <div className={classnames(style.header_logo,
+          { [style.impersonate]: this.state.impersonateData })} />
       <UserInfo />
     </div>;
   }
