9da8921a793d41c7195d0600fcd667f9d87979e0
Rubén Illodo Brea <rubenillodobrea@gmail.com>
2016-10-04 10:45:08 +0800

    Common's connect() always makes push, replace, etc. available

--- src/domain/connect/index.js
+++ src/domain/connect/index.js
@@ -1,9 +1,11 @@
 import { connect as originalConnect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { routerActions } from 'react-router-redux';
 import { createBuildRoute } from 'domain/createBuildRoute';
 import is from 'is_js';
 
 export function connect(mapStateToProps, mapDispatchToProps, mergeProps, options) {
-  return originalConnect(wrapMapStateToProps, mapDispatchToProps, mergeProps, options);
+  return originalConnect(wrapMapStateToProps, wrapMapDispatchToProps, mergeProps, options);
 
   function wrapMapStateToProps(state, ownProps) {
     const buildRoute = createBuildRoute(ownProps);
@@ -11,6 +13,13 @@ export function connect(mapStateToProps, mapDispatchToProps, mergeProps, options
         mapStateToProps(state, ownProps) :
         undefined;
     return Object.assign({ buildRoute }, stateToProps);
+  }
+
+  function wrapMapDispatchToProps(dispatch, ownProps) {
+    const dispatchToProps = is.function(mapDispatchToProps) ?
+        mapDispatchToProps(dispatch, ownProps) :
+        undefined;
+    return Object.assign({}, bindActionCreators(routerActions, dispatch), dispatchToProps);
   }
 }
 
