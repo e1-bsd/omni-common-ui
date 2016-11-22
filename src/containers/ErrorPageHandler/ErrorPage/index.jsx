e8a075e52910b3b5b8d29309950b111b454bc31b
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-11-22 11:51:28 +0800

    OMO-1015 Customisable behaviour + state cleaning for the error page

--- src/containers/ErrorPageHandler/ErrorPage/index.jsx
+++ src/containers/ErrorPageHandler/ErrorPage/index.jsx
@@ -2,15 +2,59 @@ import styles from './style.postcss';
 import warningSrc from './warning.svg';
 import React from 'react';
 import Button from 'components/Button';
+import is from 'is_js';
 
-const ErrorPage = (props) => <div className={styles.ErrorPage}>
-  <img className={styles.ErrorPage_image} src={warningSrc} role="presentation" />
-  <div className={styles.ErrorPage_text}>{props.error.message}</div>
-  <Button type={Button.Type.primary}>Back to Dashboard</Button>
-</div>;
+
+const ErrorPage = ({ erroredApi, config, replace, afterButtonClicked }) => {
+  return <div className={styles.ErrorPage}>
+    <img className={styles.ErrorPage_image} src={warningSrc} role="presentation" />
+    <div className={styles.ErrorPage_text}>{renderMessage()}</div>
+    <Button type={Button.Type.primary}
+        onClick={() => onButtonClick()}>
+      {renderButtonText()}
+    </Button>
+  </div>;
+
+  function renderMessage() {
+    if (is.not.object(config) || is.not.function(config.message)) {
+      return erroredApi.error.message;
+    }
+
+    return config.message(erroredApi);
+  }
+
+  function onButtonClick() {
+    try {
+      if (is.not.object(config) || is.not.function(config.onClickButton)) {
+        return replace('/');
+      }
+
+      return config.onClickButton(erroredApi);
+    } finally {
+      afterButtonClicked();
+    }
+  }
+
+  function renderButtonText() {
+    if (is.not.object(config) || is.not.function(config.buttonText)) {
+      return 'Back';
+    }
+
+    return config.buttonText(erroredApi);
+  }
+};
 
 ErrorPage.propTypes = {
-  error: React.PropTypes.instanceOf(Error).isRequired,
+  replace: React.PropTypes.func.isRequired,
+  afterButtonClicked: React.PropTypes.func.isRequired,
+  erroredApi: React.PropTypes.shape({
+    error: React.PropTypes.instanceOf(Error).isRequired,
+  }).isRequired,
+  config: React.PropTypes.shape({
+    message: React.PropTypes.func,
+    buttonText: React.PropTypes.func,
+    onClickButton: React.PropTypes.func,
+  }),
 };
 
 export default ErrorPage;
--- src/containers/ErrorPageHandler/index.jsx
+++ src/containers/ErrorPageHandler/index.jsx
@@ -5,31 +5,59 @@ import ErrorPage from './ErrorPage';
 import connect from 'domain/connect';
 import ApiCall from 'containers/ApiCalls';
 import is from 'is_js';
+import { List } from 'immutable';
 
-const ErrorPageHandler = (props) => {
-  if (! (props.error instanceof Error)) {
-    return props.children;
+const ErrorPageHandler = ({ children, config, erroredApi, replace, clean }) => {
+  if (! ApiCall.State.isValue(erroredApi)) {
+    return children;
   }
 
-  return <div className={styles.ErrorPageHandler}><ErrorPage error={props.error} /></div>;
+  return <div className={styles.ErrorPageHandler}>
+    <ErrorPage erroredApi={erroredApi}
+        config={config}
+        replace={replace}
+        afterButtonClicked={() => clean(erroredApi.id)} />
+  </div>;
 };
 
 ErrorPageHandler.propTypes = {
   children: React.PropTypes.node,
-  error: React.PropTypes.instanceOf(Error),
+  replace: React.PropTypes.func.isRequired,
+  clean: React.PropTypes.func.isRequired,
+  erroredApi: React.PropTypes.shape({
+    error: React.PropTypes.instanceOf(Error).isRequired,
+  }),
+  config: React.PropTypes.shape({
+    message: React.PropTypes.func,
+    buttonText: React.PropTypes.func,
+    onClickButton: React.PropTypes.func,
+  }),
 };
 
-function mapStateToProps(state) {
-  return { error: getApiError(state) };
+function mapStateToProps(state, { routes }) {
+  return { erroredApi: getApiError(state), config: getConfig(routes) };
+}
+
+function mapDispatchToProps(dispatch) {
+  return { clean: (key) => dispatch(ApiCall.clean(key)) };
 }
 
 function getApiError(state) {
   const erroredApi = state.get('apiCalls').find((call) => ApiCall.State.hasFailed(call));
   if (is.object(erroredApi) && (erroredApi.error instanceof Error)) {
-    return erroredApi.error;
+    return erroredApi;
   }
 
   return undefined;
 }
 
-export default connect(mapStateToProps)(ErrorPageHandler);
+function getConfig(routes) {
+  const routeWithConfig = new List(routes).findLast((route) => is.object(route.errorPage));
+  if (is.undefined(routeWithConfig)) {
+    return undefined;
+  }
+
+  return routeWithConfig.errorPage;
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ErrorPageHandler);
