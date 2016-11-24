38299a018cf862a3dca490add146c087a621bef5
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-11-24 11:30:35 +0800

    OMO-1018 Sidebar config comes from routes

--- src/containers/Sidebar/component.jsx
+++ src/containers/Sidebar/component.jsx
@@ -2,21 +2,39 @@ import styles from './style.postcss';
 
 import React from 'react';
 import Link from './Link';
+import is from 'is_js';
+import { OrderedMap, List } from 'immutable';
 
-const Sidebar = ({ location: { pathname } }) => <div className={styles.sideNavContainer}>
-  <Link to="https://www.google.es" currentPath={pathname}>
-    <button className={styles.sideNavItem}>
-      <span className={styles.sideNavItem_displayName}>ODIN+</span>
-    </button>
-  </Link>
-  <Link to="/group/10616" currentPath={pathname}>
-    <button className={styles.sideNavItem}>
-      <span className={styles.sideNavItem_displayName}>wat</span>
-    </button>
-  </Link>
-</div>;
+const Sidebar = ({ routes, location: { pathname } }) => {
+  const items = getItems();
+  if (items.size <= 0) {
+    return null;
+  }
+
+  return <div className={styles.Sidebar}>
+    {
+      items.map(({ text, icon }, link) => <Link to={link} currentPath={pathname}>
+        <button className={styles.Sidebar_item}>
+          <img src={icon} alt={text} />
+          <span className={styles.Sidebar_item_text}>{text}</span>
+        </button>
+      </Link>)
+    }
+  </div>;
+
+  function getItems() {
+    return new List(routes).reduce((reduction, value) => {
+      if (is.not.object(value.sidebar)) {
+        return reduction;
+      }
+
+      return reduction.merge(value.sidebar);
+    }, new OrderedMap());
+  }
+};
 
 Sidebar.propTypes = {
+  routes: React.PropTypes.array.isRequired,
   location: React.PropTypes.shape({ pathname: React.PropTypes.string.isRequired }).isRequired,
 };
 
