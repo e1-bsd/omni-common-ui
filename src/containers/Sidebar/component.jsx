d6ca2832a1cde9b62e249c7ec0d538cd9b990b8b
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-11-24 14:10:39 +0800

    OMO-1018 Removed icons

--- src/containers/Sidebar/component.jsx
+++ src/containers/Sidebar/component.jsx
@@ -1,30 +1,13 @@
 import styles from './style.postcss';
 
-import React from 'react';
+import React, { Component } from 'react';
 import Link from './Link';
 import is from 'is_js';
 import { OrderedMap, List, Map } from 'immutable';
 
-const Sidebar = ({ routes, location: { pathname } }) => {
-  const items = getItems();
-  if (items.size <= 0) {
-    return null;
-  }
-
-  return <div className={styles.Sidebar}>
-    {
-      items.map((config, link) => <Link to={link} currentPath={pathname}>
-        <button className={styles.Sidebar_item}>
-          <img className={styles.Sidebar_item_icon}
-              src={config.get('icon')}
-              alt={config.get('text')} />
-          <span className={styles.Sidebar_item_text}>{config.get('text')}</span>
-        </button>
-      </Link>)
-    }
-  </div>;
-
-  function getItems() {
+class Sidebar extends Component {
+  static _getItems(props) {
+    const { routes } = props;
     return new List(routes).reduce((reduction, value) => {
       if (is.not.object(value.sidebar)) {
         return reduction;
@@ -35,7 +18,33 @@ const Sidebar = ({ routes, location: { pathname } }) => {
         .filter((item) => item instanceof Map && ! item.isEmpty())
         .sortBy((item) => item.order);
   }
-};
+
+  constructor(props) {
+    super(props);
+    this._items = Sidebar._getItems(props);
+  }
+
+  componentWillReceiveProps(props) {
+    this._items = Sidebar._getItems(props);
+  }
+
+  render() {
+    const { location: { pathname } } = this.props;
+    if (this._items.size <= 0) {
+      return null;
+    }
+
+    return <div className={styles.Sidebar}>
+      {
+        this._items.map((config, link) => <Link to={link} currentPath={pathname}>
+          <button className={styles.Sidebar_item}>
+            <span className={styles.Sidebar_item_text}>{config.get('text')}</span>
+          </button>
+        </Link>)
+      }
+    </div>;
+  }
+}
 
 Sidebar.propTypes = {
   routes: React.PropTypes.array.isRequired,
