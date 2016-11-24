e7f29d4905528d463b947da11fd4dd3fcfb2fe32
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-11-24 14:57:59 +0800

    OMO-1018 The sidebar can be expanded

--- src/containers/Sidebar/component.jsx
+++ src/containers/Sidebar/component.jsx
@@ -1,9 +1,11 @@
 import styles from './style.postcss';
+import closeSrc from './close.svg';
 
 import React, { Component } from 'react';
 import Link from './Link';
 import is from 'is_js';
 import { OrderedMap, List, Map } from 'immutable';
+import classnames from 'classnames';
 
 class Sidebar extends Component {
   static _getItems(props) {
@@ -22,27 +24,43 @@ class Sidebar extends Component {
   constructor(props) {
     super(props);
     this._items = Sidebar._getItems(props);
+    this.state = { expanded: false };
   }
 
   componentWillReceiveProps(props) {
     this._items = Sidebar._getItems(props);
   }
 
-  render() {
+  _renderExpanded() {
+    if (this.state.expanded !== true) {
+      return null;
+    }
+
     const { location: { pathname } } = this.props;
+    return <div>
+      <div className={styles.Sidebar_close}>
+        <button onClick={() => this.setState({ expanded: false })}
+            className={styles.Sidebar_close_button}>
+          <img src={closeSrc} alt="Close" className={styles.Sidebar_close_button_icon} />
+        </button>
+      </div>
+      {
+        this._items.map((config, link) => <Link to={link} currentPath={pathname}>
+          <button className={styles.Sidebar_item}>{config.get('text')}</button>
+        </Link>)
+      }
+    </div>;
+  }
+
+  render() {
     if (this._items.size <= 0) {
       return null;
     }
 
-    return <div className={styles.Sidebar}>
-      {
-        this._items.map((config, link) => <Link to={link} currentPath={pathname}>
-          <button className={styles.Sidebar_item}>
-            <span className={styles.Sidebar_item_text}>{config.get('text')}</span>
-          </button>
-        </Link>)
-      }
-    </div>;
+    const { expanded } = this.state;
+    const classes = classnames(styles.Sidebar, { [styles.__expanded]: expanded === true });
+    const onClickBar = expanded === true ? undefined : () => this.setState({ expanded: true });
+    return <div className={classes} onClick={onClickBar}>{this._renderExpanded()}</div>;
   }
 }
 
