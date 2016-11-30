2534a3000b20147ce91148e803ee5712979cf5d5
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-11-30 14:58:58 +0800

    OMO-1016 PermissionHandler is not meant to redirect anymore

--- src/containers/PermissionHandler/index.jsx
+++ src/containers/PermissionHandler/index.jsx
@@ -1,56 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 import connect from 'domain/connect';
 import is from 'is_js';
 
-export class PermissionHandler extends Component {
-  componentWillMount() {
-    this._redirectIfNeeded(this.props);
+export const PermissionHandler = (props) => {
+  const { permissionChecks, children } = props;
+  if (is.undefined(permissionChecks)) {
+    return children;
   }
 
-  componentWillReceiveProps(props) {
-    this._redirectIfNeeded(props);
+  const forbiddenRoute = permissionChecks.find(({ canAccess }) => ! canAccess(props));
+  if (is.undefined(forbiddenRoute)) {
+    return children;
   }
 
-  _redirectIfNeeded(props) {
-    if (is.not.object(props.shouldRedirect)) {
-      return;
-    }
-
-    props.shouldRedirect.find(({ checkPrivileges }) => checkPrivileges(props));
-  }
-
-  render() {
-    if (! this.props.children) {
-      return null;
-    }
-
-    return this.props.children;
-  }
-}
+  return <div>UNAUTHORISED!</div>;
+};
 
 PermissionHandler.propTypes = {
-  shouldRedirect: React.PropTypes.arrayOf(React.PropTypes.shape({
-    checkPrivileges: React.PropTypes.func.isRequired,
+  permissionChecks: React.PropTypes.arrayOf(React.PropTypes.shape({
+    canAccess: React.PropTypes.func.isRequired,
   })),
   children: React.PropTypes.node,
 };
 
 export function mapStateToProps(state, { routes }) {
-  const shouldRedirect = routes.filter((route) => {
-    if (is.not.existy(route.checkPrivileges)) {
+  const permissionChecks = routes.filter((route) => {
+    if (is.not.existy(route.canAccess)) {
       return false;
     }
 
     if (! PRODUCTION) {
-      if (is.not.function(route.checkPrivileges)) {
-        throw new Error('checkPrivileges in the route configuration should be a function');
+      if (is.not.function(route.canAccess)) {
+        throw new Error('canAccess in the route configuration should be a function');
       }
     }
 
     return true;
   });
 
-  return { shouldRedirect };
+  return { permissionChecks };
 }
 
 export default connect(mapStateToProps)(PermissionHandler);
