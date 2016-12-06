bbb51f9c3dc84a98ca89570c4d7dba64f0f655db
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-12-06 17:18:28 +0800

    Added Footer

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -3,6 +3,7 @@ import styles from './style.postcss';
 import React from 'react';
 import Header from 'containers/Header';
 import Sidebar from 'containers/Sidebar';
+import Footer from 'components/Footer';
 
 const App = (props) => <div className={styles.App}>
   <Header />
@@ -10,6 +11,7 @@ const App = (props) => <div className={styles.App}>
     <Sidebar {...props} />
     <div className={styles.App_content}>{props.children}</div>
   </div>
+  <Footer />
 </div>;
 
 App.propTypes = {
---
+++ src/components/Footer/component.jsx
@@ -0,0 +1,22 @@
+import styles from './style.postcss';
+import React from 'react';
+
+const Footer = () => <div className={styles.Footer}>
+  <div className={styles.Footer_logo} />
+  <div className={styles.Footer_content}>
+    <p className={styles.Footer_content_row}>
+      <a className={styles.Footer_content_link}
+          target="_blank" rel="noopener noreferrer"
+          href="http://www.ef.com/privacy-policy/">Privacy Policy</a>
+      <span className={styles.Footer_content_separator} />
+      <a className={styles.Footer_content_link}
+          target="_blank" rel="noopener noreferrer"
+          href="https://helpcenter.ef.com">Help Center</a>
+    </p>
+    <p className={styles.Footer_content_row}>
+      &copy; EF Education First 2016. All rights reserved.
+    </p>
+  </div>
+</div>;
+
+export default Footer;
