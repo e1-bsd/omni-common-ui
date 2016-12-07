ffaa0348326c0a117b98907ba14d9ffd3ad8d3bf
Luke Plaster <me@lukep.org>
2016-12-07 16:28:11 +0800

    Showcase: Made it standalone

--- sample/app.jsx
+++ sample/app.jsx
@@ -1,5 +1,16 @@
-import { setupApp } from 'omni-common-ui';
-import routes from './routes';
-import reducer from './rootReducer';
+import 'assets/styles/base/base.postcss';
 
-setupApp(routes, reducer);
+import React from 'react';
+import is from 'is_js';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import parseRoutes from 'domain/parseRoutes';
+
+import routes from './routes';
+
+const parsedRoutes = parseRoutes(is.array(routes) ? routes : [routes]);
+
+render(
+  <Router history={browserHistory} routes={parsedRoutes} />,
+  document.getElementById('root')
+);
