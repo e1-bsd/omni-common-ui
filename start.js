0dae1a287705542fce998332b79e3610384d163c
Luke Plaster <me@lukep.org>
2016-12-13 14:54:54 +0800

    Hosting dev on all interfaces

--- start.js
+++ start.js
@@ -9,4 +9,4 @@ const options = require('command-line-args')([
 
 process.env.CONFIG = options.config;
 
-execSync('node node_modules/webpack-dev-server/bin/webpack-dev-server.js --progress --hot --inline --port 8080', { stdio: [0, 1, 2] });
+execSync('node node_modules/webpack-dev-server/bin/webpack-dev-server.js --progress --hot --inline --host 0.0.0.0 --port 8080', { stdio: [0, 1, 2] });
