1beba7c394dfd868949706d8809cb2f0b73865cf
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-12-13 14:57:57 +0800

    Fixed bug in start.js

--- start.js
+++ start.js
@@ -7,6 +7,6 @@ const options = require('command-line-args')([
   { name: 'config', alias: 'c', type: String },
 ]);
 
-process.env.CONFIG = options.config;
+process.env.CONFIG = options.config || '';
 
 execSync('node node_modules/webpack-dev-server/bin/webpack-dev-server.js --progress --hot --inline --host 0.0.0.0 --port 8080', { stdio: [0, 1, 2] });
