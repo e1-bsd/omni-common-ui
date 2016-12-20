655ce28b7bab47c4526b163ba89fe34934ebea4f
Luke Plaster <me@lukep.org>
2016-12-20 12:06:19 +0800

    Dialog: Added an externally controlled loading overlay

--- src/components/Dialog/index.jsx
+++ src/components/Dialog/index.jsx
@@ -2,6 +2,8 @@ import styles from './style.postcss';
 
 import React from 'react';
 import Modal from 'react-modal';
+import classnames from 'classnames';
+
 import Card from 'components/Card';
 
 const Dialog = (props) => <Modal className={styles.Dialog}
@@ -13,11 +15,17 @@ const Dialog = (props) => <Modal className={styles.Dialog}
     <Card.Content>
       {props.children}
     </Card.Content>
+    <div className={classnames(styles.LoadingOverlay, {
+      [styles.__visible]: !! props.isLoading,
+    })}>
+      <div className={styles.LoadingOverlay_inner} />
+    </div>
   </Card>
 </Modal>;
 
 Dialog.propTypes = {
   isOpen: React.PropTypes.bool.isRequired,
+  isLoading: React.PropTypes.bool,
   onRequestClose: React.PropTypes.func.isRequired,
   children: React.PropTypes.node,
   className: React.PropTypes.string,
--- src/components/Dialog/spec.jsx
+++ src/components/Dialog/spec.jsx
@@ -1,6 +1,9 @@
+import styles from './style.postcss';
+
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { expect } from 'chai';
+
 import Dialog from './';
 
 describe('Dialog', () => {
@@ -14,5 +17,25 @@ describe('Dialog', () => {
     const wrapper = mount(<Dialog isOpen={false}><div id="innerContent" /></Dialog>);
     expect(document.getElementById('innerContent')).to.not.exist;
     wrapper.unmount();
+  });
+
+  it('renders the loading overlay when loading', () => {
+    const wrapper = shallow(<Dialog isOpen isLoading><div /></Dialog>);
+    expect(wrapper).to.have.descendants(
+      `.${styles.LoadingOverlay}`
+    );
+    expect(wrapper).to.have.descendants(
+      `.${styles.LoadingOverlay}.${styles.__visible}`
+    );
+  });
+
+  it('does not render the loading overlay when not loading', () => {
+    const wrapper = shallow(<Dialog isOpen><div /></Dialog>);
+    expect(wrapper).to.have.descendants(
+      `.${styles.LoadingOverlay}`
+    );
+    expect(wrapper).to.not.have.descendants(
+      `.${styles.LoadingOverlay}.${styles.__visible}`
+    );
   });
 });
