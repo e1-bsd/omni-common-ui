7e2eb7df97dbcbbc9f676b8bc2b8192f9b1eb33b
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2016-12-30 18:14:20 +0800

    Updated build.js

--- build.js
+++ build.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
-/* eslint strict: "off" */
 
-'use strict';
-
-const execSync = require('child_process').execSync;
+const log = require('loglevel');
+const colors = require('colors/safe');
+const spawn = require('child_process').spawn;
 const { Map } = require('immutable');
 const path = require('path');
 const fs = require('fs');
@@ -16,20 +15,36 @@ const configs = new Map(requireAll({
   map: (name, filePath) => path.basename(filePath, '.json'),
 }));
 
-let cmdLine = 'node node_modules/webpack/bin/webpack.js -p --bail --progress --colors';
+process.env.NODE_ENV = 'production';
+log.enableAll();
 
-if (process.platform === 'win32') {
-  cmdLine = `set NODE_ENV=production&& ${cmdLine}`;
-} else {
-  cmdLine = `NODE_ENV=production ${cmdLine}`;
-}
+const buildLog = fs.createWriteStream(path.resolve('build.log'), { flags: 'w+' });
+buildLog.on('open', () => {
+  log.info('📦  Generate config files');
+  const distDir = path.resolve('dist-configs');
+  fs.mkdir(distDir, () => {
+    configs.forEach((config, environment) => {
+      const file = `${environment}.js`;
+      const fileDir = path.join(distDir, file);
+      try {
+        fs.writeFileSync(fileDir, `var __CONFIG__ = Object.freeze(${JSON.stringify(config)})`);
+        log.info(colors.green(`   📄  ${file} generated`));
+      } catch (e) {
+        log.error(colors.red(`   📄  ${file} could not be generated`));
+        buildLog.write(e);
+        process.exit(1);
+      }
+    });
 
-execSync(cmdLine, { stdio: [0, 1, 2] });
+    log.info('📦  Build app');
+    const webpack = spawn('node', ['node_modules/webpack/bin/webpack.js', '-p', '--bail', '--progress', '--colors'], { env: process.env, stdio: [buildLog, buildLog, buildLog] });
+    webpack.on('close', (code) => {
+      if (code) {
+        log.error(colors.red('   💣  App build failed!'));
+        process.exit(code);
+      }
 
-const distDir = path.resolve('dist-configs');
-fs.mkdirSync(distDir);
-
-configs.forEach((config, environment) => {
-  const fileDir = path.join(distDir, `${environment}.js`);
-  fs.writeFileSync(fileDir, `var __CONFIG__ = Object.freeze(${JSON.stringify(config)})`);
+      log.info(colors.green('   📦  App built'));
+    });
+  });
 });
