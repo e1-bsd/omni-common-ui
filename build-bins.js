5cf424d3a0e05a2460d3289c0cbef4de18db620d
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-01-03 16:28:54 +0800

    Avoid crashing the build if there is no “config”

--- build-bins.js
+++ build-bins.js
@@ -4,10 +4,15 @@ const path = require('path');
 const fs = require('fs');
 const { Map } = require('immutable');
 const log = require('loglevel');
-
-const { config } = require(path.resolve('package.json')); // eslint-disable-line import/no-dynamic-require
+const is = require('is_js');
 
 log.enableAll();
+
+const { config } = require(path.resolve('package.json')); // eslint-disable-line import/no-dynamic-require
+if (is.not.object(config)) {
+  log.info('No binaries to build.');
+  process.exit(0);
+}
 
 const binDir = path.resolve('node_modules/.bin');
 
