e61b0df5a9bf2a37293da5f3ace57d25f169ea05
Rubén Illodo Brea <rubenillodobrea@gmail.com>
2017-01-10 11:45:13 +0800

    Log to a file instead of the console

--- scripts/gemini-test.js
+++ scripts/gemini-test.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 
 const path = require('path');
+const fs = require('fs');
 const spawn = require('child_process').spawn;
+const log = require('loglevel');
+const colors = require('colors/safe');
+
+log.enableAll();
 
 const processes = new Array(0);
 
@@ -9,28 +14,47 @@ process.on('exit', killAll);
 process.on('SIGINT', killAll);
 process.on('SIGTERM', killAll);
 
-start('node', ['node_modules/selenium-standalone/bin/selenium-standalone', 'start', '--version=3.0.1'], /(Selenium started)|(Selenium Server is up and running)/i)
-  .then(start('node', [path.join(__dirname, 'start-mock.js')], /Serving on port/i))
-  .then(start('yarn', ['start'], /webpack: bundle is now VALID/i))
-  .then(start('node', ['node_modules/gemini/bin/gemini', 'test', '--reporter', 'flat', '--reporter', 'html', '--config', path.join(__dirname, '../.gemini.conf.js')]))
+const logFile = fs.createWriteStream(path.resolve(`${path.basename(__filename)}.log`), { flags: 'w+' });
+start({
+  command: ['node', 'node_modules/selenium-standalone/bin/selenium-standalone', 'start', '--version=3.0.1'],
+  lookFor: /(Selenium started)|(Selenium Server is up and running)/i,
+  name: 'Selenium',
+})
+  .then(start({
+    command: ['node', path.join(__dirname, 'start-mock.js')],
+    lookFor: /Serving on port/i,
+    name: 'Mock server',
+  }))
+  .then(start({
+    command: ['yarn', 'start'],
+    lookFor: /webpack: bundle is now VALID/i,
+    name: 'Web server',
+  }))
+  .then(start({
+    command: ['node', 'node_modules/gemini/bin/gemini', 'test', '--reporter', 'flat', '--reporter', 'html', '--config', path.join(__dirname, '../.gemini.conf.js')],
+    name: 'Gemini',
+  }))
   .catch(() => {
     process.exit(1);
   });
 
-function start(command = 'node', args = [], lookFor) {
+function start({ command, lookFor, name }) {
   return new Promise((resolve) => {
     let done = false;
-    const child = spawn(command, args);
-    child.on('close', (code) => process.exit(code));
+    const child = spawn(command[0], command.splice(1));
+    child.on('close', (code) => {
+      log.info(colors.red(`💥  ${name} has failed`));
+      process.exit(code);
+    });
     processes.push(child);
 
     child.stdout.on('data', (data) => {
-      process.stdout.write(data);
+      logFile.write(data);
       isDone(data);
     });
 
     child.stderr.on('data', (data) => {
-      process.stderr.write(data);
+      logFile.write(data);
       isDone(data);
     });
 
@@ -39,6 +63,7 @@ function start(command = 'node', args = [], lookFor) {
 
       data.toString().split('\n').find((line) => {
         if (lookFor.test(line)) {
+          log.info(colors.green(`🏃  ${name} is up an running`));
           resolve();
           done = true;
           return true;
