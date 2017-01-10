be3098e6e6c395d17247fd4ae8c9d125de4a3818
Rubén Illodo Brea <rubenillodobrea@gmail.com>
2017-01-10 15:02:56 +0800

    Made sure the unit tests’ process is stopped

--- scripts/unit-test.js
+++ scripts/unit-test.js
@@ -5,5 +5,14 @@ const path = require('path');
 
 const args = process.argv.slice(2, process.argv.length);
 const karmaConf = path.resolve(__dirname, '../karma.conf.js');
-spawn('node', ['node_modules/karma/bin/karma', 'start', karmaConf, ...args], { stdio: 'inherit' })
+
+process.on('exit', kill);
+process.on('SIGINT', kill);
+process.on('SIGTERM', kill);
+
+const child = spawn('node', ['node_modules/karma/bin/karma', 'start', karmaConf, ...args], { stdio: 'inherit' })
   .on('close', (code) => process.exit(code || 0));
+
+function kill() {
+  child.kill('SIGINT');
+}
