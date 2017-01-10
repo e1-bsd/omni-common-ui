e3d15736b9a4df385ced647321eca2f9a600bfc8
Rubén Illodo Brea <rubenillodobrea@gmail.com>
2017-01-10 15:02:16 +0800

    Make sure the mock server’s process is stopped

--- scripts/start-mock.js
+++ scripts/start-mock.js
@@ -2,5 +2,13 @@
 
 const spawn = require('child_process').spawn;
 
-spawn('node', ['node_modules/supervisor/lib/cli-wrapper.js', '-n', 'error', '-w', 'mocks', '-i', 'mocks/data,mocks/node_modules', '--', 'node_modules/mockizen/bin/mockizen', 'mocks/scenarios.json'], { stdio: 'inherit' })
+process.on('exit', kill);
+process.on('SIGINT', kill);
+process.on('SIGTERM', kill);
+
+const child = spawn('node', ['node_modules/supervisor/lib/cli-wrapper.js', '-n', 'error', '-w', 'mocks', '-i', 'mocks/data,mocks/node_modules', '--', 'node_modules/mockizen/bin/mockizen', 'mocks/scenarios.json'], { stdio: 'inherit' })
   .on('close', (code) => process.exit(code));
+
+function kill() {
+  child.kill('SIGINT');
+}
