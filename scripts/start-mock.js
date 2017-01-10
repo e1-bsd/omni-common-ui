2256b6166ff2ac17ef3b0b89be41e0c29b7e8e4a
Rubén Illodo Brea <rubenillodobrea@gmail.com>
2017-01-10 09:15:58 +0800

    Fixed start-mock.js

--- scripts/start-mock.js
+++ scripts/start-mock.js
@@ -2,5 +2,5 @@
 
 const spawn = require('child_process').spawn;
 
-spawn('node', ['node_modules/supervisor/lib/cli-wrapper.js', '-n', 'error', '-w', 'mocks', '-i', 'mocks/data,mocks/node_modules', '--', 'node_modules/mockizen/bin/mockizen mocks/scenarios.json'], { stdio: 'inherit' })
+spawn('node', ['node_modules/supervisor/lib/cli-wrapper.js', '-n', 'error', '-w', 'mocks', '-i', 'mocks/data,mocks/node_modules', '--', 'node_modules/mockizen/bin/mockizen', 'mocks/scenarios.json'], { stdio: 'inherit' })
   .on('close', (code) => process.exit(code));
