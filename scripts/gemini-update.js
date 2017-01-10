df1676cef0fe44e54ba0c06ed28fdeae4f8f3c57
Rubén Illodo Brea <rubenillodobrea@gmail.com>
2017-01-10 15:21:42 +0800

    Made sure the child process is killed in gemini-update.js

--- scripts/gemini-update.js
+++ scripts/gemini-update.js
@@ -2,5 +2,13 @@
 
 const spawn = require('child_process').spawn;
 
-spawn('node', ['node_modules/gemini/bin/gemini', 'update'], { stdio: 'inherit' })
+process.on('exit', kill);
+process.on('SIGINT', kill);
+process.on('SIGTERM', kill);
+
+const child = spawn('node', ['node_modules/gemini/bin/gemini', 'update'], { stdio: 'inherit' })
     .on('close', (code) => process.exit(code));
+
+function kill() {
+  child.kill('SIGINT');
+}
