f48e65f6c58355eced03e9af7fdd09b8dd0c2365
Rubén Illodo Brea <rubenillodobrea@gmail.com>
2017-03-08 15:12:39 +0800

    OMO-1425 Updated footer styles

--- src/components/Footer/component.jsx
+++ src/components/Footer/component.jsx
@@ -4,15 +4,18 @@ import React from 'react';
 const Footer = () => <div className={styles.Footer}>
   <div className={styles.Footer_logo} />
   <div className={styles.Footer_content}>
-    <p className={styles.Footer_content_row}>
+    <div className={styles.Footer_content_row}>
       <a className={styles.Footer_content_link}
           target="_blank" rel="noopener noreferrer"
           href="https://helpcenter.ef.com">Help Center</a>
-    </p>
-    <p className={styles.Footer_content_row}>
-      <span>&copy; EF Education First 2016.&nbsp;</span>
-      <span>All rights reserved.</span>
-    </p>
+    </div>
+    <div className={styles.Footer_content_row}>
+      <div className={styles.Footer_copyright}>
+        <span className={styles.Footer_copyright_firstLine}>EF Education First 2016.</span>
+        <br className={styles.Footer_break} />
+        All rights reserved.
+      </div>
+    </div>
   </div>
 </div>;
 
