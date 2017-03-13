699318c59a692c22f8f7df5a00d5db4244be6e02
Rubén Illodo Brea <rubenillodobrea@gmail.com>
2017-03-13 14:03:15 +0800

    Fixed Enzyme problem

--- webpack.config.js
+++ webpack.config.js
@@ -222,7 +222,6 @@ module.exports = {
     }
   ),
   externals: {
-    cheerio: 'window',
     'react/lib/ExecutionEnvironment': true,
     'react/lib/ReactContext': true,
     'react/addons': true,
