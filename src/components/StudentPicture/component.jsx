e6e117887c4c94d27c846918bcbab010ba3f4762
Rubén Illodo Brea <rubenillodobrea@gmail.com>
2017-03-13 11:35:42 +0800

    OMO-1417 <StudentPicture /> uses <Avatar />

--- src/components/StudentPicture/component.jsx
+++ src/components/StudentPicture/component.jsx
@@ -1,52 +1,16 @@
-import styles from './style.postcss';
 import defaultAvatarMaleImg from './default-avatar-male.svg';
 import defaultAvatarFemaleImg from './default-avatar-female.svg';
 import defaultAvatarImg from './default-avatar.svg';
 
-import React, { Component } from 'react';
-import Gender from 'domain/Gender';
-import classnames from 'classnames';
+import React from 'react';
+import Avatar from 'components/Avatar';
 
-class StudentPicture extends Component {
-  componentWillMount() {
-    this.setImageSrcInState(this.props);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.setImageSrcInState(nextProps);
-  }
-
-  getDefaultAvatar() {
-    switch (this.props.gender) {
-      case Gender.MALE:
-        return defaultAvatarMaleImg;
-      case Gender.FEMALE:
-        return defaultAvatarFemaleImg;
-      default:
-        return defaultAvatarImg;
-    }
-  }
-
-  setImageSrcInState(props, cb = () => {}) {
-    const { src = this.getDefaultAvatar() } = props;
-    if (this.state && this.state.originalSrc === src) {
-      return cb();
-    }
-    this.setState({
-      originalSrc: src,
-    }, cb);
-  }
-
-  render() {
-    const { originalSrc } = this.state;
-    const inlineStyles = {
-      backgroundImage: `url("${originalSrc}"), url("${this.getDefaultAvatar()}")`,
-    };
-    return <div style={inlineStyles}
-        className={classnames(styles.StudentPicture, this.props.className)}
-        data-src={originalSrc} />;
-  }
-}
+const StudentPicture = ({ src, className, gender }) => <Avatar url={src}
+    default={defaultAvatarImg}
+    defaultMale={defaultAvatarMaleImg}
+    defaultFemale={defaultAvatarFemaleImg}
+    className={className}
+    gender={gender} />;
 
 StudentPicture.propTypes = {
   gender: React.PropTypes.string,
