560368002d0e6c9595233be6ca0ae953086be4e2
Luke Plaster <me@lukep.org>
2017-03-15 14:51:33 +0800

    webpack: Removed happypack
    This was done to address issues with enumerating network devices on Ubuntu for Windows,
    which requires for node to be patched at runtime.
    
    See:
    https://github.com/CherryDT/wsl_node_network_interfaces_shim
    https://github.com/Microsoft/BashOnWindows/issues/468

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@
 
 'use strict';
 
-const os = require('os');
 const path = require('path');
 const webpack = require('webpack');
 const combineLoaders = require('webpack-combine-loaders');
@@ -11,7 +10,6 @@ const git = require('git-rev-sync');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const Visualizer = require('webpack-visualizer-plugin');
-const HappyPack = require('happypack');
 
 const packageInfo = require(path.resolve('package.json'));
 const version = packageInfo.version;
@@ -31,9 +29,6 @@ const excludedInCoverage = /(node_modules(\/|\\)((?!(omni-common-ui)).)|spec.jsx
 const regExpFonts = new RegExp(`fonts\\${path.sep}.+\\.(woff2?|ttf|eot|otf|svg)$`);
 const regExpInlineSvgs = new RegExp(`(\\.inline\\.svg$)|(components\\${path.sep}Icon\\${path.sep}.+\\.svg$)`);
 const regExpFavicons = new RegExp(`assets\\${path.sep}favicons\\${path.sep}.+$`);
-
-HappyPack.SERIALIZABLE_OPTIONS = HappyPack.SERIALIZABLE_OPTIONS.concat(['postcss']);
-const happyPackThreadPool = new HappyPack.ThreadPool({ size: os.cpus().length });
 
 const BABEL_CACHE_ENABLED = true;
 
@@ -82,7 +77,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: excluded,
-        loader: isDev ? 'happypack/loader?id=jsx' : jsxLoader,
+        loader: jsxLoader,
       },
       {
         test: /\.css$/,
@@ -90,7 +85,7 @@ module.exports = {
       },
       {
         test: /\.postcss$/,
-        loader: isDev ? 'happypack/loader?id=postcss' : postcssLoader,
+        loader: postcssLoader,
       },
       {
         test: regExpFonts,
@@ -154,27 +149,6 @@ module.exports = {
         [
           new webpack.ProvidePlugin({
             __CONFIG__: path.resolve(`config/${process.env.CONFIG || nodeEnv}.json`),
-          }),
-        ] :
-        [])
-      .concat(isDev ?
-        [
-          new HappyPack({
-            id: 'jsx',
-            cache: ! BABEL_CACHE_ENABLED,
-            threadPool: happyPackThreadPool,
-            loaders: [jsxLoader],
-            cacheContext: {
-              env: process.env.NODE_ENV,
-            },
-          }),
-          new HappyPack({
-            id: 'postcss',
-            threadPool: happyPackThreadPool,
-            loaders: [postcssLoader],
-            cacheContext: {
-              env: process.env.NODE_ENV,
-            },
           }),
         ] :
         [])
