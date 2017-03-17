973a107ba334cbfe69f0f017b4d1ac8e1fd03f46
Le Zheng <le.zheng@ef.com>
2017-03-17 17:01:37 +0800

    [selection tablle] basic Unit Test

--- src/components/SelectionTable/spec.jsx
+++ src/components/SelectionTable/spec.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
 import { expect } from 'chai';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import PageCard from 'components/PageCard';
 import SelectionTable from './';
 
 describe('<SelectionTable />', () => {
   it('renders the root Level and title', () => {
-    const selectionTable = <SelectionTable title="Classroom">
+    const selectionTableTitle = 'Classroom';
+    const selectionTable = <SelectionTable title={selectionTableTitle}>
       <SelectionTable.Level label="CN" />
       <SelectionTable.Level label="RU" />
     </SelectionTable>;
-    const wrapper = shallow(selectionTable);
+    const wrapper = mount(selectionTable);
     expect(wrapper.find(SelectionTable.Level)).to.have.length(2);
-    expect(wrapper.find(PageCard.Heading)).to.have.length(1);
+    expect(wrapper.find(PageCard.Heading)).to.have.prop('text', selectionTableTitle);
+  });
+
+  it('render sub-Level once a level is clicked', () => {
+    const selectionTable = <SelectionTable>
+      <SelectionTable.Level label="CN">
+        <SelectionTable.Level label="CN-1" />
+        <SelectionTable.Level label="CN-2" />
+        <SelectionTable.Level label="CN-3" />
+      </SelectionTable.Level>
+      <SelectionTable.Level label="RU">
+        <SelectionTable.Level label="CN-1" />
+      </SelectionTable.Level>
+    </SelectionTable>;
+
+    const wrapper = mount(selectionTable);
+    expect(wrapper.find(SelectionTable.Level)).to.have.length(2);
+    wrapper.find(SelectionTable.Level).first().simulate('click');
+    expect(wrapper.find(SelectionTable.Level)).to.have.length(3);
+    expect(wrapper.find(SelectionTable.Level).first()).to.have.prop('label', 'CN-1');
   });
 
 });
