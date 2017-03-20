adab63386be08518a25c80b94fec9c4685889ade
Le Zheng <le.zheng@ef.com>
2017-03-20 15:21:12 +0800

    [tooltips] add a property named overlayClassNaame

--- src/components/Tooltip/index.jsx
+++ src/components/Tooltip/index.jsx
@@ -1,9 +1,11 @@
 import 'rc-tooltip/assets/bootstrap.css';
+
 import styles from './style.postcss';
 
 import React from 'react';
 import RcTooltip from 'rc-tooltip';
 import is from 'is_js';
+import classnames from 'classnames';
 
 const Tooltip = (props) => {
   if (is.not.string(props.text) || is.empty(props.text)) {
@@ -11,11 +13,14 @@ const Tooltip = (props) => {
   }
 
   const placement = is.existy(props.placement) ? props.placement : 'top';
+  const overlayClassName = is.existy(props.overlayClassName) ?
+    classnames(styles.Tooltip_bubble, props.overlayClassName) :
+    styles.Tooltip_bubble;
 
   return <RcTooltip placement={placement}
       trigger={['hover']}
       destroyPopupOnHide
-      overlay={<div className={styles.Tooltip_bubble}>{props.text}</div>}>
+      overlay={<div className={overlayClassName}>{props.text}</div>}>
     <div className={props.className}>{props.children}</div>
   </RcTooltip>;
 };
@@ -25,6 +30,7 @@ Tooltip.propTypes = {
   text: React.PropTypes.string,
   className: React.PropTypes.string,
   placement: React.PropTypes.string,
+  overlayClassName: React.PropTypes.string,
 };
 
 export default Tooltip;
