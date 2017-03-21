c2ecd0c222d182a019fde099aaf2525b8cb9b251
Rubén Illodo Brea <rubenillodobrea@gmail.com>
2017-03-21 11:29:58 +0800

    Added the redirection back to the app after successful login

--- src/containers/SingleSignOn/SingleSignOnRedirectCallback.jsx
+++ src/containers/SingleSignOn/SingleSignOnRedirectCallback.jsx
@@ -1,9 +1,14 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { replace } from 'react-router-redux';
 import userManager from './userManager';
 
 class SingleSignOnRedirectCallback extends Component {
   componentDidMount() {
-    userManager.signinRedirectCallback();
+    userManager.signinRedirectCallback() // TODO What if it fails?
+        .then(() => {
+          this.props.dispatch(replace(sessionStorage.lastUrlPath || ''));
+        });
   }
 
   render() {
@@ -11,4 +16,8 @@ class SingleSignOnRedirectCallback extends Component {
   }
 }
 
-export default SingleSignOnRedirectCallback;
+SingleSignOnRedirectCallback.propTypes = {
+  dispatch: React.PropTypes.func.isRequired,
+};
+
+export default connect()(SingleSignOnRedirectCallback);
