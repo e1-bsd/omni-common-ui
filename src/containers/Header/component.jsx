77d55397c58c251205cffedf3ad1f90f19e903ec
Rubén Illodo Brea <rubenillodobrea@gmail.com>
2017-03-24 11:20:24 +0800

    OMO-1549 Added new icons

--- src/containers/Header/component.jsx
+++ src/containers/Header/component.jsx
@@ -6,12 +6,14 @@ import UserInfo from './UserInfo';
 import log from 'domain/log';
 import { connect } from 'domain/connect';
 import { bindActionCreators } from 'redux';
+import Icon from 'components/Icon';
 
 const Header = (props) => {
   log.debug('Header - impersonateData', props.impersonate);
   const classes = classnames(styles.Header, { [styles.__impersonating]: props.impersonate });
   return <div className={classes}>
-    <div className={classnames(styles.Header_logo)} />
+    <div className={styles.Header_burger}><Icon id="burger" /></div>
+    <div className={styles.Header_logo} />
     <div className={styles.Header_wrap}>
       <UserInfo impersonate={props.impersonate}
           setImpersonate={props.setImpersonate}
