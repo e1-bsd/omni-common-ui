d84926e53b833f5a4d96c99c538fe16666ee839e
Rubén Illodo Brea <me@rubenillodo.com>
2017-04-06 15:23:48 +0800

    Revert "OMO-1450 Added USER_UNLOADED action"
    This reverts commit 95cb56d1cd543afec9f34aeb7060902f10bfdcb7.

--- src/containers/SingleSignOn/SingleSignOnHandler.jsx
+++ src/containers/SingleSignOn/SingleSignOnHandler.jsx
@@ -19,13 +19,11 @@ class SingleSignOnHandlerImpl extends Component {
   constructor(props) {
     super(props);
     this._onUserLoaded = this._onUserLoaded.bind(this);
-    this._onUserUnloaded = this._onUserUnloaded.bind(this);
   }
 
   componentWillMount() {
     this._setLastUrlPath();
     userManager.events.addUserLoaded(this._onUserLoaded);
-    userManager.events.addUserUnloaded(this._onUserUnloaded);
   }
 
   componentDidMount() {
@@ -38,17 +36,11 @@ class SingleSignOnHandlerImpl extends Component {
 
   componentWillUnmount() {
     userManager.events.removeUserLoaded(this._onUserLoaded);
-    userManager.events.removeUserUnloaded(this._onUserUnloaded);
   }
 
   _onUserLoaded(user) {
     log.debug('SingleSignOnHandler - _onUserLoaded');
     this.props.userLoaded(user);
-  }
-
-  _onUserUnloaded() {
-    log.debug('SingleSignOnHandler - _onUserUnloaded');
-    this.props.userUnloaded();
   }
 
   _checkUserAndPrivileges(props) {
@@ -110,7 +102,6 @@ SingleSignOnHandlerImpl.propTypes = {
   user: React.PropTypes.shape(),
   fetchPrivilegesIfNeeded: React.PropTypes.func.isRequired,
   userLoaded: React.PropTypes.func.isRequired,
-  userUnloaded: React.PropTypes.func.isRequired,
 };
 
 function mapStateToProps(state) {
--- src/containers/SingleSignOn/reducer.js
+++ src/containers/SingleSignOn/reducer.js
@@ -1,12 +1,10 @@
 import { Map } from 'immutable';
-import { USER_LOADED, USER_UNLOADED } from './actions';
+import { USER_LOADED } from './actions';
 
 export default function oidc(state = new Map(), action) {
   switch (action.type) {
     case USER_LOADED:
       return state.set('user', action.user);
-    case USER_UNLOADED:
-      return state.remove('user');
     default:
       return state;
   }
