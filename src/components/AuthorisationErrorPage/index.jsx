d309991dc0133a0afba22fe39cf85cee2a107bb9
Rubén Illodo Brea <me@rubenillodo.com>
2017-04-11 11:16:30 +0800

    OMO-1416 AuthorisationErrorPage uses Icon

--- src/components/AuthorisationErrorPage/index.jsx
+++ src/components/AuthorisationErrorPage/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ErrorPage from 'components/ErrorPage';
-import lockSrc from './lock.svg';
 
 const AuthorisationErrorPage = (props) => {
   const parsedProps = Object.assign({}, props);
   parsedProps.config = Object.assign(
     {
-      icon: () => lockSrc,
+      icon: () => 'lock',
       message: () => 'You have no permission to access this page.',
     },
     props.config);
