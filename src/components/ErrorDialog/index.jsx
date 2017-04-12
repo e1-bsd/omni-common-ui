dfe52d38ddcab40c3cdb27c489e8be3555aabe10
Rubén Illodo Brea <me@rubenillodo.com>
2017-04-12 10:26:00 +0800

    Reduced indentation in ErrorDialog

--- src/components/ErrorDialog/index.jsx
+++ src/components/ErrorDialog/index.jsx
@@ -4,27 +4,20 @@ import React from 'react';
 import Dialog from 'components/Dialog';
 import Button from 'components/Button';
 
-const ErrorDialog = ({
-      onConfirmClick,
-      isOpen,
-    }) => <Dialog withCloseButton
-        isOpen={isOpen}
-        onRequestClose={(source) =>
-          (source === 'button' ? onConfirmClick() : null)}>
-      <div className={styles.ErrorDialog}>
-        <div className={styles.ErrorDialog_warningIcon} />
-        <div className={styles.ErrorDialog_content}>
-          <span>An error occurred!</span>
-        </div>
-        <Button.Container className={styles.ErrorDialog_buttons}
-            align="center">
-          <Button type={Button.Type.primary}
-              onClick={() => onConfirmClick()}>
-            OK
-          </Button>
-        </Button.Container>
-      </div>
-    </Dialog>;
+const ErrorDialog = ({ onConfirmClick, isOpen }) => <Dialog withCloseButton
+    isOpen={isOpen}
+    onRequestClose={(source) => (source === 'button' ? onConfirmClick() : null)}>
+  <div className={styles.ErrorDialog}>
+    <div className={styles.ErrorDialog_warningIcon} />
+    <div className={styles.ErrorDialog_content}>
+      <span>An error occurred!</span>
+    </div>
+    <Button.Container className={styles.ErrorDialog_buttons}
+        align="center">
+      <Button type={Button.Type.primary} onClick={() => onConfirmClick()}>OK</Button>
+    </Button.Container>
+  </div>
+</Dialog>;
 
 ErrorDialog.propTypes = {
   isOpen: React.PropTypes.bool,
