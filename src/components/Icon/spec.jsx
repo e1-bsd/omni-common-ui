3062bcd8701613fc324afd0da5167455d5ec8541
Rubén Illodo Brea <me@rubenillodo.com>
2017-04-13 11:28:37 +0800

    OMO-1416 <Icon /> can be clickable and have a title attribute

--- src/components/Icon/spec.jsx
+++ src/components/Icon/spec.jsx
@@ -3,6 +3,7 @@ import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import Icon from './';
 import icons from './icons';
+import Sinon from 'sinon';
 
 describe('<Icon />', () => {
   it('renders the content of the SVG file into the DOM', () => {
@@ -15,5 +16,17 @@ describe('<Icon />', () => {
     const iconId = 'magnifying-glass';
     const wrapper = shallow(<Icon id={iconId} className="custom-class" />);
     expect(wrapper).to.have.descendants('.custom-class');
+  });
+
+  it('allows setting an onClick event', () => {
+    const onClick = Sinon.spy();
+    const wrapper = shallow(<Icon id="burger" onClick={onClick} />);
+    wrapper.simulate('click');
+    expect(onClick.called).to.be.true;
+  });
+
+  it('allows setting a title attribute to the icon', () => {
+    const wrapper = shallow(<Icon id="burger" title="Some title" />);
+    expect(wrapper.find('[title="Some title"]')).to.have.length(1);
   });
 });
--- src/components/InlineSvg/index.jsx
+++ src/components/InlineSvg/index.jsx
@@ -2,13 +2,17 @@ import styles from './style.postcss';
 import React from 'react';
 import classnames from 'classnames';
 
-const InlineSvg = ({ children, className }) =>
+const InlineSvg = ({ children, className, title, onClick }) =>
   <i className={classnames(className, styles.InlineSvg)}
-      dangerouslySetInnerHTML={{ __html: children }} />;
+      dangerouslySetInnerHTML={{ __html: children }}
+      title={title}
+      onClick={onClick} />;
 
 InlineSvg.propTypes = {
   children: React.PropTypes.string,
   className: React.PropTypes.string,
+  title: React.PropTypes.string,
+  onClick: React.PropTypes.func,
 };
 
 export default InlineSvg;
