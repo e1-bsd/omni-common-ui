5bf08b6ca52530e39a5889b32c3dc730af45ef44
Le Zheng <le.zheng@ef.com>
2017-05-04 17:50:59 +0800

    add a className property to Card.Content

--- src/components/Card/Content/index.jsx
+++ src/components/Card/Content/index.jsx
@@ -3,13 +3,16 @@ import styles from './style.postcss';
 import React from 'react';
 import classnames from 'classnames';
 
-const Content = (props) => <div className={
-    classnames(styles.Content, { [styles.__bottomless]: props.withoutBottomPadding }
-  )}>
-  {props.children}
-</div>;
+const Content = (props) => {
+  const className = classnames(props.className, styles.Content,
+    { [styles.__bottomless]: props.withoutBottomPadding });
+  return <div className={className}>
+    {props.children}
+  </div>;
+};
 
 Content.propTypes = {
+  className: React.PropTypes.string,
   withoutBottomPadding: React.PropTypes.bool,
   children: React.PropTypes.node,
 };
