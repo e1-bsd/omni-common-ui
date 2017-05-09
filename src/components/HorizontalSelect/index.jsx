0b867d27672364eb28e06b17835442e9dc326af7
Luke Plaster <me@lukep.org>
2017-05-09 18:04:06 +0800

    HorizontalSelect: Padding to mimic group nav tabs, side overlays, left click detection

--- src/components/HorizontalSelect/index.jsx
+++ src/components/HorizontalSelect/index.jsx
@@ -5,6 +5,11 @@ import { Link } from 'react-router';
 import HorizontalScroll from '../HorizontalScroll';
 import classnames from 'classnames';
 
+const wasLeftMouseButtonPressed = (e = window.event) => {
+  const button = e.which || e.button;
+  return button === 1;
+};
+
 class HorizontalSelect extends PureComponent {
   constructor(props) {
     super();
@@ -16,15 +21,17 @@ class HorizontalSelect extends PureComponent {
 
     // this collection of goodies will prevent `onclick` firing after a drag
     this._onMouseDown = (e) => {
+      if (! wasLeftMouseButtonPressed(e.nativeEvent)) return;
       this._startMouseX = e.screenX;
     };
     this._onMouseUp = (option, e) => {
+      if (! wasLeftMouseButtonPressed(e.nativeEvent)) return;
       if (this._startMouseX !== e.screenX) return;
       this._onOptionSelect(option.value);
       e.target.click();
     };
     this._onClick = (e) => {
-      if (e.screenX === 0) return true;  // manually fired above
+      if (e.screenX === 0) return;  // manually fired above
       e.preventDefault();
     };
     this._onOptionSelect = (value) => {
@@ -37,29 +44,34 @@ class HorizontalSelect extends PureComponent {
 
   render() {
     const { options, getLinkHrefForValue } = this.props;
-    return <HorizontalScroll className={styles.HorizontalSelect}
-        scrollToElement={this.scrollToElement}>
-      <ul className={styles.HorizontalSelect_options_wrapper}>
-        {
-          options.map((option) => {
-            const className = classnames(styles.HorizontalSelect_option, {
-              [styles.HorizontalSelect_option_active]: option.value === this.state.value,
-            });
-            option._onMouseUp = this._onMouseUp.bind(null, option);  // eslint-disable-line
-            return <li key={option.value}
-                className={className}>
-              <Link to={getLinkHrefForValue && getLinkHrefForValue(option.value)}
-                  draggable={false}
-                  onMouseDown={this._onMouseDown}
-                  onMouseUp={option._onMouseUp}
-                  onClick={this._onClick}>
-                {option.html}
-              </Link>
-            </li>;
-          })
-        }
-      </ul>
-    </HorizontalScroll>;
+    return <div className={styles.HorizontalSelect_wrap}>
+      <HorizontalScroll className={styles.HorizontalSelect}
+          innerClassName={styles.HorizontalSelect_scroller}
+          scrollToElement={this.scrollToElement}>
+        <div className={styles.HorizontalSelect_options_wrap}>
+          <ul className={styles.HorizontalSelect_options}>
+            {
+              options.map((option) => {
+                const className = classnames(styles.HorizontalSelect_option, {
+                  [styles.HorizontalSelect_option_active]: option.value === this.state.value,
+                });
+                option._onMouseUp = this._onMouseUp.bind(null, option);  // eslint-disable-line
+                return <li key={option.value}
+                    className={className}>
+                  <Link to={getLinkHrefForValue && getLinkHrefForValue(option.value)}
+                      draggable={false}
+                      onMouseDown={this._onMouseDown}
+                      onMouseUp={option._onMouseUp}
+                      onClick={this._onClick}>
+                    {option.html}
+                  </Link>
+                </li>;
+              })
+            }
+          </ul>
+        </div>
+      </HorizontalScroll>
+    </div>;
   }
 }
 
