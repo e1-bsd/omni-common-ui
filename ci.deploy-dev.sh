ffff540f486f895cc0cfaf0c525bce619cd80f83
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-05-19 10:41:45 +0800

    test

--- ci.deploy-dev.sh
+++ ci.deploy-dev.sh
@@ -6,3 +6,4 @@ git merge --no-commit --no-ff $TRAVIS_COMMIT && \
 git add --force lib && \
 git commit -m "Merged $TRAVIS_BRANCH ($TRAVIS_COMMIT) into master" && \
 git push origin master:master
+#test
