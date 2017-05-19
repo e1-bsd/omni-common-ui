f22d0b525e3199e77808e1f0760aa536ad422495
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-05-19 10:13:38 +0800

    Accessing the repo through SSH

--- ci.deploy-dev.sh
+++ ci.deploy-dev.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-openssl aes-256-cbc -K $encrypted_3a8c1431a01a_key -iv $encrypted_3a8c1431a01a_iv -in deploy.enc -out deploy -d && \
-chmod 600 deploy && \
-eval `ssh-agent -s` && \
-ssh-add deploy && \
-rm deploy && \
-
 git stash && \
 git checkout -qf master && \
 git merge --no-commit --no-ff $TRAVIS_COMMIT && \
