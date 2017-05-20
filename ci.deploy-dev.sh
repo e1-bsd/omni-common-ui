6779dde2330af61756655f1997c07cf780646fd8
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-05-20 11:35:09 +0800

    Setting package version after the merge

--- ci.deploy-dev.sh
+++ ci.deploy-dev.sh
@@ -3,9 +3,11 @@
 git stash && \
 git fetch --all --prune --verbose && \
 git checkout -qf master && \
-TAG="$(git describe --abbrev=0 --tags)" && \
-npm version "$TAG-dev.$TRAVIS_BUILD_NUMBER" && \
 git merge --no-commit --no-ff $TRAVIS_COMMIT && \
 git add --force lib && \
-git commit -m "Merged $TRAVIS_BRANCH ($TRAVIS_COMMIT) into master" && \
+git commit -m "no message" && \
+TAG="$(git describe --abbrev=0 --tags)" && \
+npm version "$TAG-dev.$TRAVIS_BUILD_NUMBER" && \
+git add package.json && \
+git commit --amend -m "Merged $TRAVIS_BRANCH ($TRAVIS_COMMIT) into master" && \
 git push origin master:master
