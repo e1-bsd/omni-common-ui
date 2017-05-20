27d738c6e93557db44c04b46cdd4d98e59607b38
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-05-20 11:18:17 +0800

    Add autogenerated versions to the merges to master

--- ci.deploy-dev.sh
+++ ci.deploy-dev.sh
@@ -3,6 +3,9 @@
 git stash && \
 git fetch --all --prune --verbose && \
 git checkout -qf master && \
+TAG="$(git describe --abbrev=0 --tags)" && \
+npm config set git-tag-version false && \
+npm version "$TAG-dev.$TRAVIS_BUILD_NUMBER" && \
 git merge --no-commit --no-ff $TRAVIS_COMMIT && \
 git add --force lib && \
 git commit -m "Merged $TRAVIS_BRANCH ($TRAVIS_COMMIT) into master" && \
