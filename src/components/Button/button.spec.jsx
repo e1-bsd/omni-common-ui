92dd5ff073698e3e52b3569cf4de88aca49883e0
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-06-05 15:15:17 +0800

    Fixing tests

--- src/components/Button/button.spec.jsx
+++ src/components/Button/button.spec.jsx
@@ -7,102 +7,100 @@ import Sinon from 'sinon';
 import Button from './';
 import { Type } from './type';
 
-describe('Button', () => {
-  it('contains Type object', () => {
-    expect(Button.Type).toBe(Type);
+test('contains Type object', () => {
+  expect(Button.Type).toBe(Type);
+});
+
+test('renders its children', () => {
+  const wrapper = shallow(<Button><div id="innerContent" /></Button>);
+  expect(wrapper.contains(<div id="innerContent" />)).toBe(true);
+});
+
+test('renders a Link when linkTo is provided', () => {
+  const wrapper = shallow(<Button linkTo="/" />);
+  expect(wrapper.find(Link)).toHaveLength(1);
+});
+
+test('will open link in a new Tab if newTab is provided', () => {
+  const wrapper = shallow(<Button linkHref="/" newTab />);
+  expect(wrapper.prop('target')).toBe('_blank');
+});
+
+test('thows error if invalid type is passed', () => {
+  expect(() => shallow(<Button type="faketype" />)).toThrowError();
+});
+
+test('applies proper styles if Type.primary is passed', () => {
+  const wrapper = shallow(<Button type={Type.primary} />);
+  expect(wrapper.find(`.${styles.__primary}`)).toHaveLength(1);
+});
+
+test('applies proper styles if Type.default is passed', () => {
+  const wrapper = shallow(<Button type={Type.default} />);
+  expect(wrapper.find(`.${styles.__default}`)).toHaveLength(1);
+});
+
+test('uses Type.default if no type is provided', () => {
+  const wrapper = shallow(<Button />);
+  expect(wrapper.find(`.${styles.__default}`).length).toBe(1);
+});
+
+test('uses block styles if block property is provided', () => {
+  const wrapper = shallow(<Button block />);
+  expect(wrapper.find(`.${styles.__block}`).length).toBe(1);
+});
+
+test('uses block styles on Link and itself if block propery is provided', () => {
+  const wrapper = shallow(<Button linkTo="/" block />);
+  expect(wrapper.find(`.${styles.__block}`).length).toBe(2);
+});
+
+test('applies custom attrs when provided', () => {
+  const wrapper = shallow(<Button attrs={{ type: 'submit' }} />);
+  expect(wrapper.find('button[type="submit"]').length).toBe(1);
+});
+
+describe('when clicked', () => {
+  test('calls onClick', () => {
+    const onClick = Sinon.spy();
+    const wrapper = shallow(<Button onClick={onClick} />);
+    wrapper.simulate('click');
+    expect(onClick.called).toBe(true);
   });
 
-  it('renders its children', () => {
-    const wrapper = shallow(<Button><div id="innerContent" /></Button>);
-    expect(wrapper.contains(<div id="innerContent" />)).toBe(true);
+  test('sets the .__active class after 100ms', (done) => {
+    const wrapper = mount(<Button onClick={() => {}} />);
+    wrapper.simulate('click');
+    setTimeout(() => {
+      expect(wrapper).to.have.descendants(`.${styles.__active}`);
+      done();
+    }, 150);
   });
 
-  it('renders a Link when linkTo is provided', () => {
-    const wrapper = shallow(<Button linkTo="/" />);
-    expect(wrapper.find(Link)).toHaveLength(1);
+  test('removes the .__active class when onClick promise is resolved', (done) => {
+    const promise = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 10);
+    });
+    const wrapper = mount(<Button onClick={() => promise}
+        onClickActiveClassAddDelay={0} />);
+    wrapper.simulate('click');
+    setTimeout(() => {
+      expect(wrapper.find(`.${styles.__active}`).length).toBe(0);
+      done();
+    }, 50);
   });
 
-  it('will open link in a new Tab if newTab is provided', () => {
-    const wrapper = shallow(<Button linkHref="/" newTab />);
-    expect(wrapper.prop('target')).toBe('_blank');
-  });
-
-  it('thows error if invalid type is passed', () => {
-    expect(() => shallow(<Button type="faketype" />)).toThrowError();
-  });
-
-  it('applies proper styles if Type.primary is passed', () => {
-    const wrapper = shallow(<Button type={Type.primary} />);
-    expect(wrapper.find(`.${styles.__primary}`)).toHaveLength(1);
-  });
-
-  it('applies proper styles if Type.default is passed', () => {
-    const wrapper = shallow(<Button type={Type.default} />);
-    expect(wrapper.find(`.${styles.__default}`)).toHaveLength(1);
-  });
-
-  it('uses Type.default if no type is provided', () => {
+  test('does not fail if onClick is not provided', () => {
     const wrapper = shallow(<Button />);
-    expect(wrapper.find(`.${styles.__default}`).length).toBe(1);
+    expect(() => wrapper.simulate('click')).not.toThrowError();
   });
 
-  it('uses block styles if block property is provided', () => {
-    const wrapper = shallow(<Button block />);
-    expect(wrapper.find(`.${styles.__block}`).length).toBe(1);
-  });
-
-  it('uses block styles on Link and itself if block propery is provided', () => {
-    const wrapper = shallow(<Button linkTo="/" block />);
-    expect(wrapper.find(`.${styles.__block}`).length).toBe(2);
-  });
-
-  it('applies custom attrs when provided', () => {
-    const wrapper = shallow(<Button attrs={{ type: 'submit' }} />);
-    expect(wrapper.find('button[type="submit"]').length).toBe(1);
-  });
-
-  describe('when clicked', () => {
-    it('calls onClick', () => {
-      const onClick = Sinon.spy();
-      const wrapper = shallow(<Button onClick={onClick} />);
-      wrapper.simulate('click');
-      expect(onClick.called).toBe(true);
-    });
-
-    it('sets the .__active class after 100ms', (done) => {
-      const wrapper = mount(<Button onClick={() => {}} />);
-      wrapper.simulate('click');
-      setTimeout(() => {
-        expect(wrapper).to.have.descendants(`.${styles.__active}`);
-        done();
-      }, 150);
-    });
-
-    it('removes the .__active class when onClick promise is resolved', (done) => {
-      const promise = new Promise((resolve) => {
-        setTimeout(() => {
-          resolve();
-        }, 10);
-      });
-      const wrapper = mount(<Button onClick={() => promise}
-          onClickActiveClassAddDelay={0} />);
-      wrapper.simulate('click');
-      setTimeout(() => {
-        expect(wrapper.find(`.${styles.__active}`).length).toBe(0);
-        done();
-      }, 50);
-    });
-
-    it('does not fail if onClick is not provided', () => {
-      const wrapper = shallow(<Button />);
-      expect(() => wrapper.simulate('click')).not.toThrowError();
-    });
-
-    it('does nothing if it is disabled', () => {
-      const onClick = Sinon.spy();
-      const wrapper = shallow(<Button onClick={onClick} disabled />);
-      wrapper.simulate('click');
-      expect(onClick.called).toBe(false);
-    });
+  test('does nothing if it is disabled', () => {
+    const onClick = Sinon.spy();
+    const wrapper = shallow(<Button onClick={onClick} disabled />);
+    wrapper.simulate('click');
+    expect(onClick.called).toBe(false);
   });
 });
--- src/domain/PrivilegeChecker/spec.js
+++ src/domain/PrivilegeChecker/spec.js
@@ -1,53 +1,50 @@
-import _Config from 'domain/Config';
 import { Map, List } from 'immutable';
+import PrivilegeChecker from './';
 
-// eslint-disable-next-line import/no-webpack-loader-syntax, global-require
-const requireChecker = (Config) => require('inject-loader?domain/Config!./')({
-  'domain/Config': _Config.merge(Config),
-}).default;
+jest.mock('domain/Config', () => {
+  /* eslint-disable global-require, no-shadow */
+  const { Map } = require('immutable');
+  return new Map({ featureLogin: true });
+});
 
-describe('PrivilegeChecker', () => {
-  let PrivilegeChecker;
-  let state;
+let state;
 
+beforeEach(() => {
+  state = new Map({ privileges: { items: new List(['pr1', 'pr2', 'PREFIX_pr3']) } });
+});
+
+it('returns false if the privilege is not a string', () => {
+  expect(PrivilegeChecker.hasPrivilege(state, 1)).toBe(false);
+});
+
+it('throws if something else rather than a Map is provided as a state', () => {
+  expect(() => PrivilegeChecker.hasPrivilege({}, '')).toThrow();
+});
+
+it('returns false if the state is malformed', () => {
+  expect(PrivilegeChecker.hasPrivilege(Map(), 'pr1')).toBe(false);
+  expect(PrivilegeChecker.hasPrivilege(Map({ privileges: null }), 'pr1')).toBe(false);
+  expect(PrivilegeChecker.hasPrivilege(Map({ privileges: Map() }), 'pr1')).toBe(false);
+});
+
+it('returns true if the privilege is found in the privilege list', () => {
+  expect(PrivilegeChecker.hasPrivilege(state, 'pr1')).toBe(true);
+});
+
+it('returns true even if the privilege is provided with a different case', () => {
+  expect(PrivilegeChecker.hasPrivilege(state, 'PR1')).toBe(true);
+});
+
+it('returns true if there is a privilege in the list that ends with the provided string', () => {
+  expect(PrivilegeChecker.hasPrivilege(state, 'pr3')).toBe(true);
+});
+
+describe('when featureLogin is not true', () => {
   beforeEach(() => {
-    state = new Map({ privileges: { items: new List(['pr1', 'pr2', 'PREFIX_pr3']) } });
-    PrivilegeChecker = requireChecker({ featureLogin: true });
+    ConfigPkg.default = new Map({ featureLogin: false });
   });
 
-  it('returns false if the privilege is not a string', () => {
-    expect(PrivilegeChecker.hasPrivilege(state, 1)).toBe(false);
-  });
-
-  it('throws if something else rather than a Map is provided as a state', () => {
-    expect(() => PrivilegeChecker.hasPrivilege({}, '')).to.throw;
-  });
-
-  it('returns false if the state is malformed', () => {
-    expect(PrivilegeChecker.hasPrivilege(Map(), 'pr1')).toBe(false);
-    expect(PrivilegeChecker.hasPrivilege(Map({ privileges: null }), 'pr1')).toBe(false);
-    expect(PrivilegeChecker.hasPrivilege(Map({ privileges: Map() }), 'pr1')).toBe(false);
-  });
-
-  it('returns true if the privilege is found in the privilege list', () => {
-    expect(PrivilegeChecker.hasPrivilege(state, 'pr1')).toBe(true);
-  });
-
-  it('returns true even if the privilege is provided with a different case', () => {
-    expect(PrivilegeChecker.hasPrivilege(state, 'PR1')).toBe(true);
-  });
-
-  it('returns true if there is a privilege in the list that ends with the provided string', () => {
-    expect(PrivilegeChecker.hasPrivilege(state, 'pr3')).toBe(true);
-  });
-
-  describe('when featureLogin is not true', () => {
-    beforeEach(() => {
-      PrivilegeChecker = requireChecker({ featureLogin: false });
-    });
-
-    it('returns true', () => {
-      expect(PrivilegeChecker.hasPrivilege()).toBe(true);
-    });
+  it('returns true', () => {
+    expect(PrivilegeChecker.hasPrivilege()).toBe(true);
   });
 });
