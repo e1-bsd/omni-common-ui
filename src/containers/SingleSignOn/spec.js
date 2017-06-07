afb0a17cf3016d9079a0fd1199eb089bb4a9b75f
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-06-07 15:17:50 +0800

    Fixes to SingleSignOn/spec.js

--- src/containers/SingleSignOn/spec.js
+++ src/containers/SingleSignOn/spec.js
@@ -2,23 +2,24 @@
 
 import { Map } from 'immutable';
 import * as ConfigPkg from 'domain/Config';
-import * as OidcPkg from 'redux-oidc';
+import createOidcMiddleware from 'redux-oidc';
 
-jest.mock('./userManager', () => 'userManager');
-jest.mock('./SingleSignOnHandler', () => 'SingleSignOnHandler');
+jest.mock('./userManager');
+jest.mock('./SingleSignOnHandler');
+jest.mock('redux-oidc', () => jest.fn());
 
 beforeEach(() => {
-  OidcPkg.default = jest.fn();
+  jest.resetModules();
 });
 
 test('exports the oidc middleware if featureLogin is true', () => {
   ConfigPkg.default = new Map({ featureLogin: true });
   require('./');
-  expect(OidcPkg.default).toHaveBeenCalledTimes(1);
+  expect(createOidcMiddleware).toHaveBeenCalledTimes(1);
 });
 
 test('exports a fake oidc middleware if featureLogin is not true', () => {
   ConfigPkg.default = new Map({ featureLogin: false });
   require('./');
-  expect(OidcPkg.default).not.toHaveBeenCalled();
+  expect(createOidcMiddleware).not.toHaveBeenCalled();
 });
