df96208a73f665ce22549368310a4ade81fcfe4e
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-06-08 18:21:15 +0800

    Fixed tests for StudentCard/StudentCard/spec

--- src/components/StudentCard/StudentCard/spec.jsx
+++ src/components/StudentCard/StudentCard/spec.jsx
@@ -7,50 +7,44 @@ import StudentCard from 'components/StudentCard';
 import Card from 'components/Card';
 import classnames from 'classnames';
 
-describe('StudentCard', () => {
-  test('renders its children', () => {
-    const wrapper = shallow(<StudentCard><div id="innerContent" /></StudentCard>);
-    expect(wrapper.contains(<div id="innerContent" />)).toBe(true);
-  });
+jest.mock('domain/Config');
 
-  test('renders a Card descendent', () => {
-    const wrapper = shallow(<StudentCard />);
-    expect(wrapper).to.have.descendants(Card);
-  });
+test('renders its children', () => {
+  const wrapper = shallow(<StudentCard><div id="innerContent" /></StudentCard>);
+  expect(wrapper.contains(<div id="innerContent" />)).toBe(true);
+});
 
-  test('adds the borderless prop to Card when borderless prop is provided', () => {
-    const wrapper = shallow(<StudentCard borderless />);
-    expect(wrapper.find(Card).prop('borderless')).toBe(true);
-  });
+test('renders a Card descendent', () => {
+  const wrapper = shallow(<StudentCard />);
+  expect(wrapper.find(Card)).toHaveLength(1);
+});
 
-  test('sets the given status accent color class when provided', () => {
-    const wrapper = shallow(<StudentCard statusAccentColor="grey" />);
-    expect(
-      wrapper.find('div')
-    ).to.have.prop('className', classnames(styles.StudentCard, styles.__grey));
-  });
+test('adds the borderless prop to Card when borderless prop is provided', () => {
+  const wrapper = shallow(<StudentCard borderless />);
+  expect(wrapper.find(Card).prop('borderless')).toBe(true);
+});
 
-  test('renders a StudentCard.Profile when provided', () => {
-    const wrapper = shallow(<StudentCard><StudentCard.Profile /></StudentCard>);
-    expect(wrapper).toContain(<StudentCard.Profile />);
-  });
+test('sets the given status accent color class when provided', () => {
+  const wrapper = shallow(<StudentCard statusAccentColor="grey" />);
+  expect(wrapper.find('div').prop('className')).toBe(classnames(styles.StudentCard, styles.__grey));
+});
 
-  test('renders a StudentCard.Content when provided', () => {
-    const wrapper = shallow(<StudentCard><StudentCard.Content /></StudentCard>);
-    expect(wrapper).toContain(<StudentCard.Content />);
-  });
+test('renders a StudentCard.Profile when provided', () => {
+  const wrapper = shallow(<StudentCard><StudentCard.Profile /></StudentCard>);
+  expect(wrapper.contains(<StudentCard.Profile />)).toBe(true);
+});
 
-  test('renders a StudentCard.Profile without separator class by default', () => {
-    const wrapper = mount(<StudentCard><StudentCard.Profile /></StudentCard>);
-    expect(
-      wrapper.find(Card.Content).children()
-    ).to.not.have.className(profileStyles.__separated);
-  });
+test('renders a StudentCard.Content when provided', () => {
+  const wrapper = shallow(<StudentCard><StudentCard.Content /></StudentCard>);
+  expect(wrapper.contains(<StudentCard.Content />)).toBe(true);
+});
 
-  test('renders a StudentCard.Profile with separator class (withSeparatorLine provided)', () => {
-    const wrapper = mount(<StudentCard withSeparatorLine><StudentCard.Profile /></StudentCard>);
-    expect(
-      wrapper.find(Card.Content).children()
-    ).to.have.className(profileStyles.__separated);
-  });
+test('renders a StudentCard.Profile without separator class by default', () => {
+  const wrapper = mount(<StudentCard><StudentCard.Profile /></StudentCard>);
+  expect(wrapper.find(Card.Content).children().hasClass(profileStyles.__separated)).toBe(false);
+});
+
+test('renders a StudentCard.Profile with separator class (withSeparatorLine provided)', () => {
+  const wrapper = mount(<StudentCard withSeparatorLine><StudentCard.Profile /></StudentCard>);
+  expect(wrapper.find(Card.Content).children().hasClass(profileStyles.__separated)).toBe(true);
 });
