f887456d8a4ebb4a4c973698d7e4449588597496
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-06-08 10:10:23 +0800

    Fixed test for Card

--- src/components/Card/Card/spec.jsx
+++ src/components/Card/Card/spec.jsx
@@ -4,14 +4,12 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Card from './';
 
-describe('Card', () => {
-  test('renders its children', () => {
-    const wrapper = shallow(<Card><div id="innerContent" /></Card>);
-    expect(wrapper.contains(<div id="innerContent" />)).toBe(true);
-  });
+test('renders its children', () => {
+  const wrapper = shallow(<Card><div id="innerContent" /></Card>);
+  expect(wrapper.contains(<div id="innerContent" />)).toBe(true);
+});
 
-  test('is given a __borderless class when borderless prop is provided', () => {
-    const wrapper = shallow(<Card borderless />);
-    expect(wrapper).to.have.className(styles.__borderless);
-  });
+test('is given a __borderless class when borderless prop is provided', () => {
+  const wrapper = shallow(<Card borderless />);
+  expect(wrapper.hasClass(styles.__borderless)).toBe(true);
 });
