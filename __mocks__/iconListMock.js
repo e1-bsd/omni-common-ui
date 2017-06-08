612c10d2c446c026dafedf8da5ff1c8dc29cbf1c
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-06-08 11:32:45 +0800

    Placed mock for icon list in its proper place

--- __mocks__/iconListMock.js
+++ __mocks__/iconListMock.js
@@ -1,12 +1,1 @@
-jest.mock('/components/Icon/icons', () => {
-  /* eslint-disable global-require */
-  const path = require('path');
-  const fs = require('fs');
-  const { Map } = require('immutable');
-
-  const files = fs.readdirSync(path.resolve('src/components/Icon'))
-    .filter((file) => /\.svg$/.test(file))
-    .map((file) => path.basename(file, '.svg'));
-
-  return new Map(files.map((file) => [file, file]));
-});
+jest.mock('/components/Icon/icons');
