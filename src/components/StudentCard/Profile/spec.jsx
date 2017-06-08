04ba66bd7f349fff5497f80a3b7f9297e842676f
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-06-08 18:16:25 +0800

    Fixed tests for StudentCard/Profile/spec

--- src/components/StudentCard/Profile/spec.jsx
+++ src/components/StudentCard/Profile/spec.jsx
@@ -7,33 +7,30 @@ import { shallow } from 'enzyme';
 import StudentPicture from 'components/StudentPicture';
 import Profile from './';
 
-describe('StudentCard.Profile', () => {
-  test('renders a StudentPicture with the given attributes', () => {
-    const wrapper = shallow(<Profile avatarUrl="url" gender={1} />);
-    expect(wrapper).toContain(<StudentPicture src="url"
-        gender={1}
-        className={styles.StudentCard_profile_image} />);
-  });
-  test('renders a bigger StudentPicture with the withBiggerAvatar prop', () => {
-    const wrapper = shallow(<Profile withBiggerAvatar avatarUrl="url" gender={1} />);
-    expect(wrapper).toContain(<StudentPicture src="url"
-        gender={1}
-        className={classnames(styles.StudentCard_profile_image, styles.__bigger)} />);
-  });
-  test('renders profile info with the given name', () => {
-    const wrapper = shallow(<Profile name="John" surname="Doe" localName="王呆呆" />);
-    expect(
-      wrapper.find(`.${styles.StudentCard_profile_name}`)
-    ).to.have.text(
-      'John Doe'
-    );
-  });
-  test('renders profile info with the given surname', () => {
-    const wrapper = shallow(<Profile name="John" surname="Doe" localName="王呆呆" />);
-    expect(
-      wrapper.find(`.${styles.StudentCard_profile_localName}`)
-    ).to.have.text(
-      '王呆呆'
-    );
-  });
+jest.mock('domain/Config');
+
+test('renders a StudentPicture with the given attributes', () => {
+  const wrapper = shallow(<Profile avatarUrl="url" gender={1} />);
+  const containsPicture = wrapper.contains(<StudentPicture src="url"
+      gender={1}
+      className={styles.StudentCard_profile_image} />);
+  expect(containsPicture).toBe(true);
+});
+
+test('renders a bigger StudentPicture with the withBiggerAvatar prop', () => {
+  const wrapper = shallow(<Profile withBiggerAvatar avatarUrl="url" gender={1} />);
+  const containsPicture = wrapper.contains(<StudentPicture src="url"
+      gender={1}
+      className={classnames(styles.StudentCard_profile_image, styles.__bigger)} />);
+  expect(containsPicture).toBe(true);
+});
+
+test('renders profile info with the given name', () => {
+  const wrapper = shallow(<Profile name="John" surname="Doe" localName="王呆呆" />);
+  expect(wrapper.find(`.${styles.StudentCard_profile_name}`).text()).toBe('John Doe');
+});
+
+test('renders profile info with the given surname', () => {
+  const wrapper = shallow(<Profile name="John" surname="Doe" localName="王呆呆" />);
+  expect(wrapper.find(`.${styles.StudentCard_profile_localName}`).text()).toBe('王呆呆');
 });
