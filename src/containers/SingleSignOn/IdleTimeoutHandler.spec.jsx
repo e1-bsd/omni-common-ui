7fbda18e7d287048b408d1b7065b2ccd8b7a74ab
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-06-08 13:17:01 +0800

    Using the new Config mock in SingleSignOn/spec

--- src/containers/SingleSignOn/IdleTimeoutHandler.spec.jsx
+++ src/containers/SingleSignOn/IdleTimeoutHandler.spec.jsx
@@ -2,74 +2,69 @@ import React from 'react';
 import Sinon from 'sinon';
 import { mount } from 'enzyme';
 import userManager from './userManager';
-import _Config from 'domain/Config';
+import Config from 'domain/Config';
+import IdleTimeoutHandler from './IdleTimeoutHandler';
 
-describe('<IdleTimeoutHandler />', () => {
-  let IdleTimeoutHandler;
-  let signoutRedirect;
+jest.mock('domain/Config');
+jest.mock('./userManager', () => ({ forceSignoutRedirect: jest.fn() }));
 
-  // eslint-disable-next-line import/no-webpack-loader-syntax, global-require
-  const requireComponent = (Config) => require('inject-loader?domain/Config&./userManager!./IdleTimeoutHandler')({
-    'domain/Config': _Config.merge(Config),
-    './userManager': userManager,
-  }).default;
+let signoutRedirect;
 
-  const mountComponent = () => mount(<IdleTimeoutHandler><div id="inner" /></IdleTimeoutHandler>);
+const mountComponent = () => mount(<IdleTimeoutHandler><div id="inner" /></IdleTimeoutHandler>);
 
+beforeEach(() => {
+  signoutRedirect = Sinon.stub(userManager, 'signoutRedirect');
+  signoutRedirect.returns();
+  Config.merge({ autoSignOutTimeout: false });
+});
+
+afterEach(() => {
+  signoutRedirect.restore();
+});
+
+describe('when autoSignOutTimeout is false', () => {
+  test('does not call userManager.signoutRedirect()', function test(done) {
+    this.timeout(5000);
+    mountComponent();
+    setTimeout(() => {
+      expect(signoutRedirect.called).toBe(false);
+      done();
+    }, 2000);
+  });
+
+  test('renders its children', () => {
+    const wrapper = mountComponent();
+    expect(wrapper).to.have.descendants('#inner');
+  });
+});
+
+describe('when autoSignOutTimeout is a number', () => {
   beforeEach(() => {
-    signoutRedirect = Sinon.stub(userManager, 'signoutRedirect');
-    signoutRedirect.returns();
-    IdleTimeoutHandler = requireComponent({ autoSignOutTimeout: false });
+    Config.merge({ autoSignOutTimeout: 2 });
   });
 
-  afterEach(() => {
-    signoutRedirect.restore();
+  test('calls userManager.signoutRedirect() after the seconds set in autoSignOutTimeout', function test(done) {
+    this.timeout(5000);
+    mountComponent();
+    setTimeout(() => {
+      expect(signoutRedirect.called).toBe(true);
+      done();
+    }, 2000);
   });
 
-  describe('when autoSignOutTimeout is false', () => {
-    test('does not call userManager.signoutRedirect()', function test(done) {
-      this.timeout(5000);
-      mountComponent();
-      setTimeout(() => {
-        expect(signoutRedirect.called).toBe(false);
-        done();
-      }, 2000);
-    });
-
-    test('renders its children', () => {
-      const wrapper = mountComponent();
-      expect(wrapper).to.have.descendants('#inner');
-    });
+  test('does not call userManager.signoutRedirect() after the seconds set in autoSignOutTimeout ' +
+      'if there are some user interactions happening', function test(done) {
+    this.timeout(5000);
+    mountComponent();
+    setTimeout(() => window.document.dispatchEvent(new Event('click')), 1000);
+    setTimeout(() => {
+      expect(signoutRedirect.called).toBe(false);
+      done();
+    }, 2000);
   });
 
-  describe('when autoSignOutTimeout is a number', () => {
-    beforeEach(() => {
-      IdleTimeoutHandler = requireComponent({ autoSignOutTimeout: 2 });
-    });
-
-    test('calls userManager.signoutRedirect() after the seconds set in autoSignOutTimeout', function test(done) {
-      this.timeout(5000);
-      mountComponent();
-      setTimeout(() => {
-        expect(signoutRedirect.called).toBe(true);
-        done();
-      }, 2000);
-    });
-
-    test('does not call userManager.signoutRedirect() after the seconds set in autoSignOutTimeout ' +
-        'if there are some user interactions happening', function test(done) {
-      this.timeout(5000);
-      mountComponent();
-      setTimeout(() => window.document.dispatchEvent(new Event('click')), 1000);
-      setTimeout(() => {
-        expect(signoutRedirect.called).toBe(false);
-        done();
-      }, 2000);
-    });
-
-    test('renders its children', () => {
-      const wrapper = mountComponent();
-      expect(wrapper).to.have.descendants('#inner');
-    });
+  test('renders its children', () => {
+    const wrapper = mountComponent();
+    expect(wrapper).to.have.descendants('#inner');
   });
 });
