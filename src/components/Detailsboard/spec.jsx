d82ef77e5d8ee9aeb97b2e8e34c047f860b9e492
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-06-08 10:11:28 +0800

    Fixed test for Detailsboard

--- src/components/Detailsboard/spec.jsx
+++ src/components/Detailsboard/spec.jsx
@@ -2,12 +2,9 @@ import styles from './style.postcss';
 
 import React from 'react';
 import { shallow } from 'enzyme';
-import { expect } from 'chai';
 import { DetailsBoard } from './';
 
-describe('DetailsBoard', () => {
-  test('renders itself with title and value', () => {
-    const wrapper = shallow(<DetailsBoard title="test" value="test" />);
-    expect(wrapper).to.have.className(styles.Detailsboard_board);
-  });
+test('renders itself with title and value', () => {
+  const wrapper = shallow(<DetailsBoard title="test" value="test" />);
+  expect(wrapper.hasClass(styles.Detailsboard_board)).toBe(true);
 });
