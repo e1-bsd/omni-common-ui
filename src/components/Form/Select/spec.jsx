508510a6395dd9b3e81f376024343fd1e5fd2b29
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-06-08 10:32:05 +0800

    Simplified Form/Select tests a bit

--- src/components/Form/Select/spec.jsx
+++ src/components/Form/Select/spec.jsx
@@ -5,43 +5,39 @@ import { mount } from 'enzyme';
 import Form from '../';
 import Field from '../Field';
 
-describe('Form', () => {
-  describe('Select', () => {
-    const options = [
-      { value: 'one', label: 'One' },
-      { value: 'two', label: 'Two', clearableValue: false },
-    ];
+const options = [
+  { value: 'one', label: 'One' },
+  { value: 'two', label: 'Two', clearableValue: false },
+];
 
-    test('works along with Formsy', () => {
-      const wrapper = mount(<Form>
-        <Form.Select name="required"
-            label="required"
-            value=""
-            options={options}
-            required />
-      </Form>);
-      expect(wrapper.find(`.${styles.__required}`)).toHaveLength(1);
-    });
+test('works along with Formsy', () => {
+  const wrapper = mount(<Form>
+    <Form.Select name="required"
+        label="required"
+        value=""
+        options={options}
+        required />
+  </Form>);
+  expect(wrapper.find(`.${styles.__required}`)).toHaveLength(1);
+});
 
-    describe('label prop existence', () => {
-      test('has a Field when there is a label prop present', () => {
-        const wrapper = mount(<Form>
-          <Form.Select name="labelled"
-              label="labelled"
-              value=""
-              options={options} />
-        </Form>);
-        expect(wrapper).to.have.descendants(Field);
-      });
+describe('label prop existence', () => {
+  test('has a Field when there is a label prop present', () => {
+    const wrapper = mount(<Form>
+      <Form.Select name="labelled"
+          label="labelled"
+          value=""
+          options={options} />
+    </Form>);
+    expect(wrapper).to.have.descendants(Field);
+  });
 
-      test('does not have a Field when there is no label prop present', () => {
-        const wrapper = mount(<Form>
-          <Form.Select name="labelless"
-              value=""
-              options={options} />
-        </Form>);
-        expect(wrapper).to.not.have.descendants(Field);
-      });
-    });
+  test('does not have a Field when there is no label prop present', () => {
+    const wrapper = mount(<Form>
+      <Form.Select name="labelless"
+          value=""
+          options={options} />
+    </Form>);
+    expect(wrapper).to.not.have.descendants(Field);
   });
 });
