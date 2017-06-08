07e931e6c7b0ec152dd8326f022651258e44b7a1
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-06-08 10:15:58 +0800

    Fixed test for Grid

--- src/components/Grid/Grid/spec.jsx
+++ src/components/Grid/Grid/spec.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Grid from './';
 
-describe('Grid', () => {
-  test('renders its children', () => {
-    const wrapper = shallow(<Grid><div id="innerContent" /></Grid>);
-    expect(wrapper.contains(<div id="innerContent" />)).toBe(true);
-  });
+jest.mock('domain/MediaQuery', () => jest.fn(() => ({
+  addListener: jest.fn(),
+  removeListener: jest.fn(),
+})));
+
+test('renders its children', () => {
+  const wrapper = shallow(<Grid><div id="innerContent" /></Grid>);
+  expect(wrapper.contains(<div id="innerContent" />)).toBe(true);
 });
