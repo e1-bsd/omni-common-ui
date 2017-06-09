048c78d02774c5acb37cd21451b2dafa9155fac7
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-06-09 16:01:09 +0800

    Fixed tests for Breadcrumbs/spec

--- src/components/Breadcrumbs/spec.jsx
+++ src/components/Breadcrumbs/spec.jsx
@@ -4,37 +4,32 @@ import { Link } from 'react-router';
 import Breadcrumbs from './';
 import styles from './style.postcss';
 
-describe('<Breadcrumbs />', () => {
-  describe('check basic render functionality', () => {
-    const groupLink = {
-      label: 'group1',
-      href: '/group/1',
-      clickable: true,
-    };
+const groupLink = {
+  label: 'group1',
+  href: '/group/1',
+  clickable: true,
+};
 
-    const markAttendanceLink = {
-      label: 'mark attendance',
-      clickable: false,
-    };
+const markAttendanceLink = {
+  label: 'mark attendance',
+  clickable: false,
+};
 
-    let wrapper;
+let wrapper;
 
-    beforeEach(() => {
-      wrapper = shallow(<Breadcrumbs items={[groupLink, markAttendanceLink]} />);
-    });
+beforeEach(() => {
+  wrapper = shallow(<Breadcrumbs items={[groupLink, markAttendanceLink]} />);
+});
 
-    test('links has been rendered properly', () => {
-      expect(wrapper.find(`.${styles.Breadcrumbs_crumb}`)).toHaveLength(2);
-    });
+test('links has been rendered properly', () => {
+  expect(wrapper.find(`.${styles.Breadcrumbs_crumb}`)).toHaveLength(2);
+});
 
-    test('back button has been rendered properly', () => {
-      expect(wrapper).to.have.descendants(`.${styles.Breadcrumbs_crumb_back}`);
-    });
+test('back button has been rendered properly', () => {
+  expect(wrapper.find(`.${styles.Breadcrumbs_crumb_back}`)).toHaveLength(1);
+});
 
-    test('only render the first item as a link', () => {
-      expect(wrapper.find(`.${styles.Breadcrumbs_crumb}`).last().find('span'))
-        .to.have.text('mark attendance');
-      expect(wrapper.find(`.${styles.Breadcrumbs_crumb}`).first().contains(<Link />));
-    });
-  });
+test('only render the first item as a link', () => {
+  expect(wrapper.find(`.${styles.Breadcrumbs_crumb}`).last().find('span').text()).toBe('mark attendance');
+  expect(wrapper.find(`.${styles.Breadcrumbs_crumb}`).first().find(Link)).toHaveLength(1);
 });
