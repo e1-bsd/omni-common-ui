bf1f796f593c9d6b7809636591e8832f91865e3f
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-06-09 16:14:05 +0800

    Fixed tests for HorizontalSelect/spec

--- src/components/HorizontalSelect/spec.jsx
+++ src/components/HorizontalSelect/spec.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
-import Sinon from 'sinon';
 import styles from './style.postcss';
 import ReactRouter from 'react-router';
 import HorizontalSelect from './';
-import PropTypes from 'prop-types';
 
-beforeEach(() => {
-  ReactRouter.Link = ({ onClick }) => <div onClick={onClick} />;
-  ReactRouter.Link.propTypes = { onClick: PropTypes.func };
+jest.mock('react-router', () => {
+  const PropTypes = require('prop-types');
+  const Link = ({ onClick }) => <div onClick={onClick} />;
+  Link.propTypes = { onClick: PropTypes.func };
+  return { Link };
 });
 
 test('renders option html as a node', () => {
   const helloNodeHtml = <div>hello</div>;
-  const options = [{
-    html: helloNodeHtml,
-    value: 'hello',
-  }];
-  expect(shallow(<HorizontalSelect options={options} />)).toContain(helloNodeHtml);
+  const options = [{ html: helloNodeHtml, value: 'hello' }];
+  expect(shallow(<HorizontalSelect options={options} />).contains(helloNodeHtml)).toBe(true);
 });
 
 test('onSelect is called once being clicked', () => {
@@ -28,11 +25,11 @@ test('onSelect is called once being clicked', () => {
     html: <div>2</div>,
     value: 2,
   }];
-  const testOnSelect = Sinon.spy();
+  const testOnSelect = jest.fn();
   const wrapper = mount(<HorizontalSelect options={options}
       onSelect={testOnSelect} />);
   wrapper.find(ReactRouter.Link).last().simulate('click');
-  expect(testOnSelect.args[0]).toEqual([2]);
+  expect(testOnSelect).toHaveBeenCalledWith(2);
 });
 
 test('set active styles to the selected option', () => {
