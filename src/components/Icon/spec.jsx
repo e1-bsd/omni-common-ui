a1a05595ad1761dfd44031b492f3dcfb63531fbd
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-06-09 15:55:06 +0800

    Fixed tests for Icon/spec

--- src/components/Icon/spec.jsx
+++ src/components/Icon/spec.jsx
@@ -2,30 +2,27 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Icon from './';
 import icons from './icons';
-import Sinon from 'sinon';
 
-describe('<Icon />', () => {
-  test('renders the content of the SVG file into the DOM', () => {
-    const iconId = 'magnifying-glass';
-    const wrapper = shallow(<Icon id={iconId} />);
-    expect(wrapper).toContain(icons.get(iconId));
-  });
+test('renders the content of the SVG file into the DOM', () => {
+  const iconId = 'magnifying-glass';
+  const wrapper = shallow(<Icon id={iconId} />);
+  expect(wrapper.contains(icons.get(iconId))).toBe(true);
+});
 
-  test('allows passing className down to the inline SVG component', () => {
-    const iconId = 'magnifying-glass';
-    const wrapper = shallow(<Icon id={iconId} className="custom-class" />);
-    expect(wrapper).to.have.descendants('.custom-class');
-  });
+test('allows passing className down to the inline SVG component', () => {
+  const iconId = 'magnifying-glass';
+  const wrapper = shallow(<Icon id={iconId} className="custom-class" />);
+  expect(wrapper.hasClass('custom-class')).toBe(true);
+});
 
-  test('allows setting an onClick event', () => {
-    const onClick = Sinon.spy();
-    const wrapper = shallow(<Icon id="burger" onClick={onClick} />);
-    wrapper.simulate('click');
-    expect(onClick.called).toBe(true);
-  });
+test('allows setting an onClick event', () => {
+  const onClick = jest.fn();
+  const wrapper = shallow(<Icon id="burger" onClick={onClick} />);
+  wrapper.simulate('click');
+  expect(onClick).toHaveBeenCalled();
+});
 
-  test('allows setting a title attribute to the icon', () => {
-    const wrapper = shallow(<Icon id="burger" title="Some title" />);
-    expect(wrapper.find('[title="Some title"]')).toHaveLength(1);
-  });
+test('allows setting a title attribute to the icon', () => {
+  const wrapper = shallow(<Icon id="burger" title="Some title" />);
+  expect(wrapper.find('[title="Some title"]')).toHaveLength(1);
 });
