62198aaae54d1cc2c0d9958e160ca29340e0b7bc
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-06-09 16:08:22 +0800

    Fixed tests for TextInput/spec

--- src/components/Form/TextInput/spec.jsx
+++ src/components/Form/TextInput/spec.jsx
@@ -4,17 +4,13 @@ import React from 'react';
 import { mount } from 'enzyme';
 import Form from '../';
 
-describe('Form', () => {
-  describe('TextInput', () => {
-    test('works along with Formsy', () => {
-      const wrapper = mount(<Form>
-        <Form.TextInput name="email"
-            label="Email"
-            validations="isEmail"
-            validationError="This is not a valid email"
-            value="123" />
-      </Form>);
-      expect(wrapper.find(`.${styles.__error}`)).toHaveLength(1);
-    });
-  });
+test('works along with Formsy', () => {
+  const wrapper = mount(<Form>
+    <Form.TextInput name="email"
+        label="Email"
+        validations="isEmail"
+        validationError="This is not a valid email"
+        value="123" />
+  </Form>);
+  expect(wrapper.find(`.${styles.__error}`).length).toBeGreaterThanOrEqual(1);
 });
