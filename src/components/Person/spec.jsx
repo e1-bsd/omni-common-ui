8b21ab20a20264c8f438ac17edbe703eed93a36d
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-06-09 16:02:25 +0800

    Fixed tests for Person/spec

--- src/components/Person/spec.jsx
+++ src/components/Person/spec.jsx
@@ -6,37 +6,35 @@ import StudentPicture from 'components/StudentPicture';
 import AdultPicture from 'components/AdultPicture';
 import Person from './';
 
-describe('<Person />', () => {
-  test('renders StudentPicture if provided', () => {
-    const wrapper = shallow(<Person><StudentPicture /></Person>);
-    expect(wrapper.find(StudentPicture)).toHaveLength(1);
-  });
+test('renders StudentPicture if provided', () => {
+  const wrapper = shallow(<Person><StudentPicture /></Person>);
+  expect(wrapper.find(StudentPicture)).toHaveLength(1);
+});
 
-  test('renders AdultPicture if provided', () => {
-    const wrapper = shallow(<Person><AdultPicture /></Person>);
-    expect(wrapper.find(AdultPicture)).toHaveLength(1);
-  });
+test('renders AdultPicture if provided', () => {
+  const wrapper = shallow(<Person><AdultPicture /></Person>);
+  expect(wrapper.find(AdultPicture)).toHaveLength(1);
+});
 
-  test('renders only the first picture provided', () => {
-    const wrapper = shallow(<Person>
-      <AdultPicture />
-      <AdultPicture />
-      <StudentPicture />
-    </Person>);
-    expect(wrapper.find(AdultPicture)).toHaveLength(1);
-    expect(wrapper.find(StudentPicture)).toHaveLength(0);
-  });
+test('renders only the first picture provided', () => {
+  const wrapper = shallow(<Person>
+    <AdultPicture />
+    <AdultPicture />
+    <StudentPicture />
+  </Person>);
+  expect(wrapper.find(AdultPicture)).toHaveLength(1);
+  expect(wrapper.find(StudentPicture)).toHaveLength(0);
+});
 
-  test('renders all the other children as "lines"', () => {
-    const wrapper = shallow(<Person>
-      <AdultPicture />
-      <div id="line-1" />
-      <h1 id="line-2">Line 2</h1>
-      <div id="line-3" />
-    </Person>);
-    const lines = wrapper.find(`.${styles.Person_lines}`);
-    expect(lines).to.have.descendants('#line-1');
-    expect(lines).to.have.descendants('#line-2');
-    expect(lines).to.have.descendants('#line-3');
-  });
+test('renders all the other children as "lines"', () => {
+  const wrapper = shallow(<Person>
+    <AdultPicture />
+    <div id="line-1" />
+    <h1 id="line-2">Line 2</h1>
+    <div id="line-3" />
+  </Person>);
+  const lines = wrapper.find(`.${styles.Person_lines}`);
+  expect(lines.find('#line-1')).toHaveLength(1);
+  expect(lines.find('#line-2')).toHaveLength(1);
+  expect(lines.find('#line-3')).toHaveLength(1);
 });
