ea4a1db12be1b9185ee4629a65dc200f26224dd3
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-06-09 16:21:50 +0800

    Fixed tests for Avatar/spec

--- src/components/Avatar/spec.jsx
+++ src/components/Avatar/spec.jsx
@@ -1,100 +1,94 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { expect } from 'chai';
 import Avatar from './';
 import generateUserInitialsAvatarSvgUri from './generateUserInitialsAvatarSvg';
 
-describe('<Avatar />', () => {
-  const bloggsInitialsAvatarUri = generateUserInitialsAvatarSvgUri('Joe', 'Bloggs');
-  const unknownInitialsAvatarUri = generateUserInitialsAvatarSvgUri('?', '?');
-  const redInitialsAvatarUri = generateUserInitialsAvatarSvgUri('Kelly', '?', '#D8213A', 'white');
+const bloggsInitialsAvatarUri = generateUserInitialsAvatarSvgUri('Joe', 'Bloggs');
+const unknownInitialsAvatarUri = generateUserInitialsAvatarSvgUri('?', '?');
+const redInitialsAvatarUri = generateUserInitialsAvatarSvgUri('Kelly', '?', '#D8213A', 'white');
 
-  let props;
+let props;
 
+beforeEach(() => {
+  props = {
+    src: 'fake-src',
+    default: 'fake-default',
+    defaultMale: 'fake-default-male',
+    defaultFemale: 'fake-default-female',
+  };
+});
+
+test('allows to add custom classes', () => {
+  const wrapper = shallow(<Avatar className="aClass" />);
+  expect(wrapper.find('.aClass')).toHaveLength(1);
+});
+
+test('shows provided src', () => {
+  const wrapper = shallow(<Avatar src="fake-src" />);
+  expect(wrapper.prop('style').backgroundImage).toBe('url("fake-src")');
+});
+
+test('assigns two background images, in case the src is broken', () => {
+  const wrapper = shallow(<Avatar {...props} />);
+  expect(wrapper.prop('style').backgroundImage).toBe('url("fake-src"), url("fake-default")');
+});
+
+describe('when src is not provided', () => {
   beforeEach(() => {
-    props = {
-      src: 'fake-src',
-      default: 'fake-default',
-      defaultMale: 'fake-default-male',
-      defaultFemale: 'fake-default-female',
-    };
+    props.src = undefined;
   });
 
-  test('allows to add custom classes', () => {
-    const wrapper = shallow(<Avatar className="aClass" />);
-    expect(wrapper).to.have.descendants('.aClass');
+  test('shows the default image for males', () => {
+    const wrapper = shallow(<Avatar {...props} gender="male" />);
+    expect(wrapper.prop('style').backgroundImage).toBe('url("fake-default-male")');
   });
 
-  test('shows provided src', () => {
-    const wrapper = shallow(<Avatar src="fake-src" />);
-    expect(wrapper).to.have.style('background-image', 'url("fake-src")');
+  test('shows the default image for females', () => {
+    const wrapper = shallow(<Avatar {...props} gender="female" />);
+    expect(wrapper.prop('style').backgroundImage).toBe('url("fake-default-female")');
   });
 
-  test('assigns two background images, in case the src is broken', () => {
+  test('shows the default image if no gender is provided', () => {
     const wrapper = shallow(<Avatar {...props} />);
-    expect(wrapper).to.have.style('background-image', 'url("fake-src"), url("fake-default")');
+    expect(wrapper.prop('style').backgroundImage).toBe('url("fake-default")');
   });
 
-  describe('when src is not provided', () => {
-    beforeEach(() => {
-      props.src = undefined;
+  describe('shows an SVG-based avatar containing the users initials (when enabled via prop)', () => {
+    test('happy path', () => {
+      const wrapper = shallow(<Avatar {...props}
+          userFirstName="Joe"
+          userLastName="Bloggs"
+          displayUserInitialsAsDefaultAvatar />);
+      expect(wrapper.prop('style').backgroundImage).toBe(`url("${bloggsInitialsAvatarUri}")`);
     });
 
-    test('shows the default image for males', () => {
-      const wrapper = shallow(<Avatar {...props} gender="male" />);
-      expect(wrapper).to.have.style('background-image', 'url("fake-default-male")');
+    test('happy path - colour spec correctness check', () => {
+      const wrapper = shallow(<Avatar {...props}
+          userFirstName="Kelly"
+          displayUserInitialsAsDefaultAvatar />);
+      expect(wrapper.prop('style').backgroundImage).toBe(`url("${redInitialsAvatarUri}")`);
     });
 
-    test('shows the default image for females', () => {
-      const wrapper = shallow(<Avatar {...props} gender="female" />);
-      expect(wrapper).to.have.style('background-image', 'url("fake-default-female")');
+    test('containing "??" when user name is blank', () => {
+      const wrapper = shallow(<Avatar {...props}
+          userFirstName=""
+          userLastName=""
+          displayUserInitialsAsDefaultAvatar />);
+      expect(wrapper.prop('style').backgroundImage).toBe(`url("${unknownInitialsAvatarUri}")`);
     });
 
-    test('shows the default image if no gender is provided', () => {
-      const wrapper = shallow(<Avatar {...props} />);
-      expect(wrapper).to.have.style('background-image', 'url("fake-default")');
+    test('containing "??" when user name is absent', () => {
+      const wrapper = shallow(<Avatar {...props}
+          displayUserInitialsAsDefaultAvatar />);
+      expect(wrapper.prop('style').backgroundImage).toBe(`url("${unknownInitialsAvatarUri}")`);
     });
 
-    describe(
-      'shows an SVG-based avatar containing the users initials (when enabled via prop)',
-      () => {
-        test('happy path', () => {
-          const wrapper = shallow(<Avatar {...props}
-              userFirstName="Joe"
-              userLastName="Bloggs"
-              displayUserInitialsAsDefaultAvatar />);
-          expect(wrapper).to.have.attr('style', `background-image:url("${bloggsInitialsAvatarUri}");`);
-        });
-
-        test('happy path - colour spec correctness check', () => {
-          const wrapper = shallow(<Avatar {...props}
-              userFirstName="Kelly"
-              displayUserInitialsAsDefaultAvatar />);
-          expect(wrapper).to.have.attr('style', `background-image:url("${redInitialsAvatarUri}");`);
-        });
-
-        test('containing "??" when user name is blank', () => {
-          const wrapper = shallow(<Avatar {...props}
-              userFirstName=""
-              userLastName=""
-              displayUserInitialsAsDefaultAvatar />);
-          expect(wrapper).to.have.attr('style', `background-image:url("${unknownInitialsAvatarUri}");`);
-        });
-
-        test('containing "??" when user name is absent', () => {
-          const wrapper = shallow(<Avatar {...props}
-              displayUserInitialsAsDefaultAvatar />);
-          expect(wrapper).to.have.attr('style', `background-image:url("${unknownInitialsAvatarUri}");`);
-        });
-
-        test('containing "??" when user name is a non-string value', () => {
-          const wrapper = shallow(<Avatar {...props}
-              userFirstName={null}
-              userLastName={0}
-              displayUserInitialsAsDefaultAvatar />);
-          expect(wrapper).to.have.attr('style', `background-image:url("${unknownInitialsAvatarUri}");`);
-        });
-      }
-    );
+    test('containing "??" when user name is a non-string value', () => {
+      const wrapper = shallow(<Avatar {...props}
+          userFirstName={null}
+          userLastName={0}
+          displayUserInitialsAsDefaultAvatar />);
+      expect(wrapper.prop('style').backgroundImage).toBe(`url("${unknownInitialsAvatarUri}")`);
+    });
   });
 });
