1abaeb7b8c2e31d516bae684b3dfc33408c1d5ab
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-06-09 12:12:08 +0800

    Fixed tests for Card/Content/spec

--- src/components/Card/Content/spec.jsx
+++ src/components/Card/Content/spec.jsx
@@ -4,17 +4,17 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Content } from './';
 
-describe('Card.Content', () => {
-  test('renders its children', () => {
-    const wrapper = shallow(<Content><div id="innerContent" /></Content>);
-    expect(wrapper.contains(<div id="innerContent" />)).toBe(true);
-  });
-  test('sets the bottomless padding style when the property is supplied', () => {
-    const wrapper = shallow(<Content withoutBottomPadding />);
-    expect(wrapper).to.have.className(styles.__bottomless);
-  });
-  test('does not set the bottomless padding style when the property is omitted', () => {
-    const wrapper = shallow(<Content />);
-    expect(wrapper).to.not.have.className(styles.__bottomless);
-  });
+test('renders its children', () => {
+  const wrapper = shallow(<Content><div id="innerContent" /></Content>);
+  expect(wrapper.contains(<div id="innerContent" />)).toBe(true);
+});
+
+test('sets the bottomless padding style when the property is supplied', () => {
+  const wrapper = shallow(<Content withoutBottomPadding />);
+  expect(wrapper.hasClass(styles.__bottomless)).toBe(true);
+});
+
+test('does not set the bottomless padding style when the property is omitted', () => {
+  const wrapper = shallow(<Content />);
+  expect(wrapper.hasClass(styles.__bottomless)).toBe(false);
 });
