1afc65b27285c5bc3f8fd93a7711ca096db619b6
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-06-09 12:17:00 +0800

    Fixed tests for PageCard/spec

--- src/components/PageCard/spec.jsx
+++ src/components/PageCard/spec.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import PageCard from './';
 
-describe('PageCard', () => {
-  test('renders its children', () => {
-    const wrapper = shallow(<PageCard><div id="innerContent" /></PageCard>);
-    expect(wrapper.contains(<div id="innerContent" />)).toBe(true);
-  });
-  test('allows to add custom classes', () => {
-    const wrapper = shallow(<PageCard className="aClass" />);
-    expect(wrapper).to.have.descendants('.aClass');
-  });
+test('renders its children', () => {
+  const wrapper = shallow(<PageCard><div id="innerContent" /></PageCard>);
+  expect(wrapper.contains(<div id="innerContent" />)).toBe(true);
+});
+
+test('allows to add custom classes', () => {
+  const wrapper = shallow(<PageCard className="aClass" />);
+  expect(wrapper.find('.aClass')).toHaveLength(1);
 });
