1936ce0cc85c486b15451318abcc6e1531ca6565
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-06-09 16:06:53 +0800

    Fixed tests for Dialog/spec

--- src/components/Dialog/spec.jsx
+++ src/components/Dialog/spec.jsx
@@ -2,62 +2,51 @@ import styles from './style.postcss';
 
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import Sinon from 'sinon';
 
 import { Dialog } from './';
 
-describe('Dialog', () => {
-  test('renders its children if open', () => {
-    const wrapper = mount(<Dialog isOpen><div id="innerContent" /></Dialog>);
-    expect(document.getElementById('innerContent')).toBeDefined();
-    wrapper.unmount();
+test('renders its children if open', () => {
+  const wrapper = mount(<Dialog isOpen><div id="innerContent" /></Dialog>);
+  expect(document.getElementById('innerContent')).toBeDefined();
+  wrapper.unmount();
+});
+
+test('does not render its children if not open', () => {
+  const wrapper = mount(<Dialog isOpen={false}><div id="innerContent" /></Dialog>);
+  expect(document.getElementById('innerContent')).toBeFalsy();
+  wrapper.unmount();
+});
+
+describe('loading overlay', () => {
+  test('shows the loading overlay when loading', () => {
+    const wrapper = shallow(<Dialog isOpen isLoading><div /></Dialog>);
+    expect(wrapper.find(`.${styles.LoadingOverlay}`)).toHaveLength(1);
+    expect(wrapper.find(`.${styles.LoadingOverlay}.${styles.__visible}`)).toHaveLength(1);
   });
 
-  test('does not render its children if not open', () => {
-    const wrapper = mount(<Dialog isOpen={false}><div id="innerContent" /></Dialog>);
-    expect(document.getElementById('innerContent')).toBeFalsy();
-    wrapper.unmount();
+  test('hides the loading overlay when not loading', () => {
+    const wrapper = shallow(<Dialog isOpen><div /></Dialog>);
+    expect(wrapper.find(`.${styles.LoadingOverlay}`)).toHaveLength(1);
+    expect(wrapper.find(`.${styles.LoadingOverlay}.${styles.__visible}`)).toHaveLength(0);
+  });
+});
+
+describe('close button', () => {
+  test('contains a close button icon when enabled', () => {
+    const wrapper = shallow(<Dialog isOpen withCloseButton />);
+    expect(wrapper.find(`.${styles.Dialog_closeIcon}`)).toHaveLength(1);
   });
 
-  describe('loading overlay', () => {
-    test('shows the loading overlay when loading', () => {
-      const wrapper = shallow(<Dialog isOpen isLoading><div /></Dialog>);
-      expect(wrapper).to.have.descendants(
-        `.${styles.LoadingOverlay}`
-      );
-      expect(wrapper).to.have.descendants(
-        `.${styles.LoadingOverlay}.${styles.__visible}`
-      );
-    });
-
-    test('hides the loading overlay when not loading', () => {
-      const wrapper = shallow(<Dialog isOpen><div /></Dialog>);
-      expect(wrapper).to.have.descendants(
-        `.${styles.LoadingOverlay}`
-      );
-      expect(wrapper).to.not.have.descendants(
-        `.${styles.LoadingOverlay}.${styles.__visible}`
-      );
-    });
+  test('does not contain a close button icon when not enabled', () => {
+    const wrapper = shallow(<Dialog isOpen />);
+    expect(wrapper.find(`.${styles.Dialog_closeIcon}`)).toHaveLength(0);
   });
 
-  describe('close button', () => {
-    test('contains a close button icon when enabled', () => {
-      const wrapper = shallow(<Dialog isOpen withCloseButton />);
-      expect(wrapper).to.have.descendants(`.${styles.Dialog_closeIcon}`);
-    });
-
-    test('does not contain a close button icon when not enabled', () => {
-      const wrapper = shallow(<Dialog isOpen />);
-      expect(wrapper).to.not.have.descendants(`.${styles.Dialog_closeIcon}`);
-    });
-
-    test('calls `onRequestClose` when the close button is clicked', () => {
-      const onRequestClose = Sinon.spy();
-      const wrapper = shallow(<Dialog isOpen withCloseButton onRequestClose={onRequestClose} />);
-      const button = wrapper.find(`.${styles.Dialog_closeIcon}`);
-      button.simulate('click');
-      expect(onRequestClose.calledWith('button')).toBe(true);
-    });
+  test('calls `onRequestClose` when the close button is clicked', () => {
+    const onRequestClose = jest.fn();
+    const wrapper = shallow(<Dialog isOpen withCloseButton onRequestClose={onRequestClose} />);
+    const button = wrapper.find(`.${styles.Dialog_closeIcon}`);
+    button.simulate('click');
+    expect(onRequestClose).toHaveBeenCalledWith('button', undefined);
   });
 });
