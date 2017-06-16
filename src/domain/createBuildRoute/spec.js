ccb4f4c1f51d0f26685dc34061aad7eb78f934a4
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-06-16 16:51:17 +0800

    Test cases use toBe() instead of toMatch() !

--- src/domain/createBuildRoute/spec.js
+++ src/domain/createBuildRoute/spec.js
@@ -12,62 +12,62 @@ describe('buildRoute', () => {
   test('joins a simple route', () => {
     const ownProps = { location: { pathname: 'oldpathname' } };
     const buildRoute = createBuildRoute(ownProps);
-    expect(buildRoute('newroute')).toMatch('/oldpathname/newroute');
+    expect(buildRoute('newroute')).toBe('/oldpathname/newroute');
   });
 
   test('interprets ../ in the route', () => {
     const ownProps = { location: { pathname: 'old/path/name' } };
     const buildRoute = createBuildRoute(ownProps);
-    expect(buildRoute('..')).toMatch('/old/path');
-    expect(buildRoute('../newroute')).toMatch('/old/path/newroute');
-    expect(buildRoute('../../newroute')).toMatch('/old/newroute');
-    expect(buildRoute('../useless/../route')).toMatch('/old/path/route');
+    expect(buildRoute('..')).toBe('/old/path');
+    expect(buildRoute('../newroute')).toBe('/old/path/newroute');
+    expect(buildRoute('../../newroute')).toBe('/old/newroute');
+    expect(buildRoute('../useless/../route')).toBe('/old/path/route');
   });
 
   test('interprets routes starting with /', () => {
     const ownProps = { location: { pathname: 'old/path/name' } };
     const buildRoute = createBuildRoute(ownProps);
-    expect(buildRoute('/newroute')).toMatch('/newroute');
+    expect(buildRoute('/newroute')).toBe('/newroute');
   });
 
   describe('when provided with an object with route parameters', () => {
     test('joins a simple route', () => {
       const ownProps = { routes: [{ path: 'old' }, { path: 'path' }] };
       const buildRoute = createBuildRoute(ownProps);
-      expect(buildRoute('newroute', { param: 'param1' })).toMatch('/old/path/newroute');
+      expect(buildRoute('newroute', { param: 'param1' })).toBe('/old/path/newroute');
     });
 
     test('interprets ../ in the route', () => {
       const ownProps = { routes: [{ path: 'old' }, { path: 'path' }, { path: 'name' }] };
       const buildRoute = createBuildRoute(ownProps);
-      expect(buildRoute('../newroute', { param: 'param1' })).toMatch('/old/path/newroute');
-      expect(buildRoute('../../newroute', { param: 'param1' })).toMatch('/old/newroute');
-      expect(buildRoute('../useless/../route', { param: 'param1' })).toMatch('/old/path/route');
+      expect(buildRoute('../newroute', { param: 'param1' })).toBe('/old/path/newroute');
+      expect(buildRoute('../../newroute', { param: 'param1' })).toBe('/old/newroute');
+      expect(buildRoute('../useless/../route', { param: 'param1' })).toBe('/old/path/route');
     });
 
     test('interprets routes starting with /', () => {
       const ownProps = { routes: [{ path: 'old' }, { path: 'path' }] };
       const buildRoute = createBuildRoute(ownProps);
-      expect(buildRoute('/newroute', { param: 'param1' })).toMatch('/newroute');
+      expect(buildRoute('/newroute', { param: 'param1' })).toBe('/newroute');
     });
 
     describe('simple param syntax', () => {
       test('keeps the old route parameters if no new value is given', () => {
         const ownProps = { routes: [{ path: 'group/:groupId' }], params: { groupId: '1' } };
         const buildRoute = createBuildRoute(ownProps);
-        expect(buildRoute('sessions', { param: 'param1' })).toMatch('/group/1/sessions');
+        expect(buildRoute('sessions', { param: 'param1' })).toBe('/group/1/sessions');
       });
 
       test('updates the old route parameters if a new value is given', () => {
         const ownProps = { routes: [{ path: 'group/:groupId' }], params: { groupId: '1' } };
         const buildRoute = createBuildRoute(ownProps);
-        expect(buildRoute('sessions', { groupId: '2' })).toMatch('/group/2/sessions');
+        expect(buildRoute('sessions', { groupId: '2' })).toBe('/group/2/sessions');
       });
 
       test('allows use without a route parameter', () => {
         const ownProps = { routes: [{ path: 'group/:groupId' }], params: { groupId: '1' } };
         const buildRoute = createBuildRoute(ownProps);
-        expect(buildRoute({ groupId: '2' })).toMatch('/group/2');
+        expect(buildRoute({ groupId: '2' })).toBe('/group/2');
       });
 
       test('does not explode when there is a param omitted', () => {
@@ -75,7 +75,7 @@ describe('buildRoute', () => {
           routes: [{ path: 'group/:groupId/attendance' }], params: { groupId: '1' },
         };
         const buildRoute = createBuildRoute(ownProps);
-        expect(buildRoute({ otherParam: '2' })).toMatch('/group/1/attendance');
+        expect(buildRoute({ otherParam: '2' })).toBe('/group/1/attendance');
       });
     });
 
@@ -83,19 +83,19 @@ describe('buildRoute', () => {
       test('keeps the old route parameters if no new value is given', () => {
         const ownProps = { routes: [{ path: 'group(/:groupId)' }], params: { groupId: '1' } };
         const buildRoute = createBuildRoute(ownProps);
-        expect(buildRoute('sessions', { param: 'param1' })).toMatch('/group/1/sessions');
+        expect(buildRoute('sessions', { param: 'param1' })).toBe('/group/1/sessions');
       });
 
       test('updates the old route parameters if a new value is given', () => {
         const ownProps = { routes: [{ path: 'group(/:groupId)' }], params: { groupId: '1' } };
         const buildRoute = createBuildRoute(ownProps);
-        expect(buildRoute('sessions', { groupId: '2' })).toMatch('/group/2/sessions');
+        expect(buildRoute('sessions', { groupId: '2' })).toBe('/group/2/sessions');
       });
 
       test('allows use without a route parameter', () => {
         const ownProps = { routes: [{ path: 'group(/:groupId)' }], params: { groupId: '1' } };
         const buildRoute = createBuildRoute(ownProps);
-        expect(buildRoute({ groupId: '2' })).toMatch('/group/2');
+        expect(buildRoute({ groupId: '2' })).toBe('/group/2');
       });
 
       test('does not explode when there is an optional param omitted', () => {
@@ -103,7 +103,7 @@ describe('buildRoute', () => {
           routes: [{ path: 'group(/:groupId)/session(/:sessionId)' }], params: { groupId: '1' },
         };
         const buildRoute = createBuildRoute(ownProps);
-        expect(buildRoute({ groupId: '2' })).toMatch('/group/2/session');
+        expect(buildRoute({ groupId: '2' })).toBe('/group/2/session');
       });
     });
 
@@ -113,7 +113,7 @@ describe('buildRoute', () => {
           routes: [{ path: 'group/:view(/:mode)' }], params: { view: 'day', mode: 'marking' },
         };
         const buildRoute = createBuildRoute(ownProps);
-        expect(buildRoute({ mode: '' })).toMatch('/group/day');
+        expect(buildRoute({ mode: '' })).toBe('/group/day');
       });
 
       test('allows use with a relative path passed as a string', () => {
@@ -123,7 +123,7 @@ describe('buildRoute', () => {
           location: { pathname: '/group/day' },
         };
         const buildRoute = createBuildRoute(ownProps);
-        expect(buildRoute('./marking')).toMatch('/group/day/marking');
+        expect(buildRoute('./marking')).toBe('/group/day/marking');
       });
 
       test('explodes when there is a mandatory param omitted', () => {
@@ -141,32 +141,32 @@ describe('buildRoute', () => {
 
 describe('normalizeUrl()', () => {
   test('supports file:// protocol', () => {
-    expect(normalizeUrl('file:///test/path')).toMatch('file:///test/path');
+    expect(normalizeUrl('file:///test/path')).toBe('file:///test/path');
   });
 
   test('removes duplicated slashes', () => {
-    expect(normalizeUrl('/test//path')).toMatch('/test/path');
-    expect(normalizeUrl('/test///path')).toMatch('/test/path');
-    expect(normalizeUrl('/test_///path')).toMatch('/test_/path');
-    expect(normalizeUrl('/test9///path')).toMatch('/test9/path');
-    expect(normalizeUrl('/test-///path')).toMatch('/test-/path');
+    expect(normalizeUrl('/test//path')).toBe('/test/path');
+    expect(normalizeUrl('/test///path')).toBe('/test/path');
+    expect(normalizeUrl('/test_///path')).toBe('/test_/path');
+    expect(normalizeUrl('/test9///path')).toBe('/test9/path');
+    expect(normalizeUrl('/test-///path')).toBe('/test-/path');
   });
 
   test('resolves two dots going up one level', () => {
-    expect(normalizeUrl('/test/../path')).toMatch('/path');
-    expect(normalizeUrl('/test/path/..')).toMatch('/test');
-    expect(normalizeUrl('/test/path9/..')).toMatch('/test');
-    expect(normalizeUrl('/test/path_/..')).toMatch('/test');
-    expect(normalizeUrl('/test/path-/..')).toMatch('/test');
-    expect(normalizeUrl('/test/sublevel/../path')).toMatch('/test/path');
-    expect(normalizeUrl('/../test/path')).toMatch('/test/path');
+    expect(normalizeUrl('/test/../path')).toBe('/path');
+    expect(normalizeUrl('/test/path/..')).toBe('/test');
+    expect(normalizeUrl('/test/path9/..')).toBe('/test');
+    expect(normalizeUrl('/test/path_/..')).toBe('/test');
+    expect(normalizeUrl('/test/path-/..')).toBe('/test');
+    expect(normalizeUrl('/test/sublevel/../path')).toBe('/test/path');
+    expect(normalizeUrl('/../test/path')).toBe('/test/path');
   });
 
   test('gets rid of "." items', () => {
-    expect(normalizeUrl('/test/./path')).toMatch('/test/path');
-    expect(normalizeUrl('/test/path/.')).toMatch('/test/path');
-    expect(normalizeUrl('/./test/path')).toMatch('/test/path');
-    expect(normalizeUrl('./test/path')).toMatch('test/path');
-    expect(normalizeUrl('./test/././path')).toMatch('test/path');
+    expect(normalizeUrl('/test/./path')).toBe('/test/path');
+    expect(normalizeUrl('/test/path/.')).toBe('/test/path');
+    expect(normalizeUrl('/./test/path')).toBe('/test/path');
+    expect(normalizeUrl('./test/path')).toBe('test/path');
+    expect(normalizeUrl('./test/././path')).toBe('test/path');
   });
 });
