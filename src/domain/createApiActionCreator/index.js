33ced1d26f42155a7187b551fe5cf99a8b7be30c
Luke Plaster <me@lukep.org>
2017-06-28 14:59:59 +0800

    Simplified createApiActionCreator

--- src/domain/createApiActionCreator/index.js
+++ src/domain/createApiActionCreator/index.js
@@ -5,44 +5,46 @@ import ApiCall from 'containers/ApiCalls';
  * Supports currying so that args can be recycled more conveniently.
  * A contrived example:
  * ```
- * const createCreatorForX =
- *    createApiActionCreator('X_REQUEST', 'X_SUCCESS', 'X_FAILURE', 'http://...');
+ * const createCreatorForX = createApiActionCreator('NOTIFICATION', 'http://...');
  * const getXActionCreator = createCreatorForX('GET');
  * const postXActionCreator = createCreatorForX('POST');
  * ```
  */
 
-const createApiActionCreator = (
-    requestActionType, successActionType, failureActionType, url, method = 'GET') =>
+const createApiActionCreator = (actionObjectName, url, method = 'GET') =>
   (dispatch) => {
+    const prefix = (() => {
+      switch (method.toUpperCase()) {
+        case 'DELETE': return 'DELETE_';
+        case 'POST': case 'PUT': return 'SUBMIT_';
+        case 'GET': default: return 'FETCH_';
+      }
+    })();
+
+    const upperActionObjectName = actionObjectName.toUpperCase();
+
     return dispatch(createFetchRequestAction()).payload
       .then((response) => dispatch(createFetchSuccessAction(response)))
       .catch((error) => dispatch(createFetchFailureAction(error)));
 
     function createFetchRequestAction() {
+      const type = `${prefix}${upperActionObjectName}_REQUEST`;
       return ApiCall.createAction({
-        type: requestActionType,
-        payload: fetch(url),
-        url,
-        method,
+        type, url, method, payload: fetch(url),
       });
     }
 
     function createFetchSuccessAction(response) {
+      const type = `${prefix}${upperActionObjectName}_SUCCESS`;
       return ApiCall.createAction({
-        type: successActionType,
-        payload: response,
-        url,
-        method,
+        type, url, method, payload: response,
       });
     }
 
     function createFetchFailureAction(error) {
+      const type = `${prefix}${upperActionObjectName}_FAILURE`;
       return ApiCall.createAction({
-        type: failureActionType,
-        error,
-        url,
-        method,
+        type, url, method, error,
       });
     }
   };
