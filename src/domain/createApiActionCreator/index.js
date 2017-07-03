b4b3a560ddcde7afdc439a1a332facd54d4a5432
Luke Plaster <me@lukep.org>
2017-07-03 16:32:44 +0800

    createApiActionCreator: Added support for an object-based options syntax and action extras

--- src/domain/createApiActionCreator/index.js
+++ src/domain/createApiActionCreator/index.js
@@ -1,3 +1,4 @@
+import is from 'is_js';
 import { fetch } from 'domain/Api';
 import ApiCall from 'containers/ApiCalls';
 
@@ -11,7 +12,12 @@ import ApiCall from 'containers/ApiCalls';
  * ```
  */
 
-const createApiActionCreator = (actionObjectName, url, method = 'GET') =>
+const REQUIRED_ARGS_COUNT = 3;
+
+const createApiActionCreator = (actionObjectName,
+    url, method = 'GET',
+    requestExtras = {}, successExtras = {}, failureExtras = {}) =>  // these are optional!
+
   (dispatch) => {
     const prefix = (() => {
       switch (method.toUpperCase()) {
@@ -30,30 +36,41 @@ const createApiActionCreator = (actionObjectName, url, method = 'GET') =>
     function createFetchRequestAction() {
       const type = `${prefix}${upperActionObjectName}_REQUEST`;
       return ApiCall.createAction({
-        type, url, method, payload: fetch(url),
+        type, url, method, payload: fetch(url), ...requestExtras,
       });
     }
 
     function createFetchSuccessAction(response) {
       const type = `${prefix}${upperActionObjectName}_SUCCESS`;
       return ApiCall.createAction({
-        type, url, method, payload: response,
+        type, url, method, payload: response, ...successExtras,
       });
     }
 
     function createFetchFailureAction(error) {
       const type = `${prefix}${upperActionObjectName}_FAILURE`;
       return ApiCall.createAction({
-        type, url, method, error,
+        type, url, method, error, ...failureExtras,
       });
     }
   };
 
 const curried = (...args) => {
-  if (args.length >= createApiActionCreator.length) {
+  if (args.length >= REQUIRED_ARGS_COUNT) {
     return createApiActionCreator.apply(this, args);
   }
-  return (...rest) => curried.apply(this, args.concat(rest));
+  if (args.length === 1 && is.object(args[0])) {  // allow first arg to be an options hash
+    const options = args[0];
+    return createApiActionCreator.call(this,
+        options.actionObjectName,
+        options.url,
+        options.method,
+        options.requestExtras,
+        options.successExtras,
+        options.failureExtras);
+  }
+  return (...rest) =>
+    curried.apply(this, args.concat(rest));
 };
 
 export default curried;
