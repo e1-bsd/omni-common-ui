b7a1d818f0d208b79d7cb6bd6f39c1dbe3c1ed8e
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-07-03 16:02:57 +0800

    Updated favicon config on Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const REG_EXP_INLINE_SVGS = new RegExp(`(\\.inline\\.svg$)|(components\\${path.sep}Icon\\${path.sep}.+\\.svg$)`);
-const REG_EXP_FAVICONS = new RegExp(`assets\\${path.sep}favicons\\${path.sep}.+$`);
+const REG_EXP_FAVICONS = new RegExp(`assets\\${path.sep}favicons\\${path.sep}.+(svg|png)$`);
 const BABEL_CACHE_ENABLED = true;
 
 module.exports = {
@@ -70,10 +70,6 @@ module.exports = {
         ],
       },
       {
-        test: REG_EXP_FAVICONS,
-        use: 'file-loader?hash=sha512&digest=hex&name=[name].[hash].[ext]',
-      },
-      {
         test: REG_EXP_INLINE_SVGS,
         use: {
           loader: 'svg-inline-loader',
@@ -112,6 +108,30 @@ module.exports = {
               },
             },
           },
+        ],
+      },
+      {
+        test: REG_EXP_FAVICONS,
+        use: 'file-loader?hash=sha512&digest=hex&name=[name].[hash].[ext]',
+      },
+      {
+        test: /favicon\.ico$/,
+        use: 'file-loader?name=[name].[ext]',
+      },
+      {
+        test: /browserconfig\.xml$/,
+        use: [
+          'file-loader?name=[name].[ext]',
+          'extract-loader',
+          'browserconfig-loader',
+        ],
+      },
+      {
+        test: /manifest\.json$/,
+        use: [
+          'file-loader?hash=sha512&digest=hex&name=[name].[hash].[ext]',
+          'extricate-loader',
+          'interpolate-loader',
         ],
       },
     ],
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,6 @@
-const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const CommonConfig = require('./webpack.config.js');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const definePlugin = require('./webpack.define-plugin.js');
 const htmlPlugin = require('./webpack.html-plugin.js');
 
@@ -22,12 +20,5 @@ module.exports = merge.smart(CommonConfig, {
       PRODUCTION: true,
     }),
     htmlPlugin({ PRODUCTION: true }),
-    new CopyWebpackPlugin([
-      { from: path.join(__dirname, 'lib/assets/favicons/browserconfig.xml'), to: path.resolve('dist') },
-      { from: path.join(__dirname, 'lib/assets/favicons/android-chrome-192x192.png'), to: path.resolve('dist') },
-      { from: path.join(__dirname, 'lib/assets/favicons/android-chrome-512x512.png'), to: path.resolve('dist') },
-      { from: path.join(__dirname, 'lib/assets/favicons/mstile-150x150.png'), to: path.resolve('dist') },
-      { from: path.join(__dirname, 'lib/assets/favicons/favicon.ico'), to: path.resolve('dist') },
-    ]),
   ],
 });
