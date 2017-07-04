77c6a6585212edfd8d180f31146a450fb05c111b
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-07-04 14:41:04 +0800

    Removed a bunch of dependencies and files that are not needed anymore

--- scripts/execOrExit.js
+++
@@ -1,7 +0,0 @@
-const { exec } = require('shelljs');
-
-module.exports = function execOrExit(command) {
-  const result = exec(command, { stdio: 'inherit' });
-  result.code === 0 || process.exit(1);
-  return result;
-};
