54417d68cdfd2d01ecafd974a08b989896bb8b64
Luke Plaster <me@lukep.org>
2017-07-04 16:54:42 +0800

    StudentCard: Moved inner profile bits into a new StudentCard.Person for reuse

--- src/components/StudentCard/Profile/spec.jsx
+++ src/components/StudentCard/Profile/spec.jsx
@@ -1,34 +1,23 @@
 import styles from './style.postcss';
 
 import React from 'react';
-import classnames from 'classnames';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
 import StudentPicture from 'components/StudentPicture';
 import Profile from './';
 
-test('renders a StudentPicture with the given attributes', () => {
-  const wrapper = shallow(<Profile avatarUrl="url" gender={1} />);
-  const containsPicture = wrapper.contains(<StudentPicture src="url"
-      gender={1}
-      className={styles.StudentCard_profile_image} />);
-  expect(containsPicture).toBe(true);
-});
-
-test('renders a bigger StudentPicture with the withBiggerAvatar prop', () => {
-  const wrapper = shallow(<Profile withBiggerAvatar avatarUrl="url" gender={1} />);
-  const containsPicture = wrapper.contains(<StudentPicture src="url"
-      gender={1}
-      className={classnames(styles.StudentCard_profile_image, styles.__bigger)} />);
-  expect(containsPicture).toBe(true);
+test('renders a StudentPicture', () => {
+  const wrapper = mount(<Profile avatarUrl="url" gender={1} />);
+  const containsPicture = wrapper.find(StudentPicture);
+  expect(containsPicture).toHaveLength(1);
 });
 
 test('renders profile info with the given name', () => {
-  const wrapper = shallow(<Profile name="John" surname="Doe" localName="王呆呆" />);
+  const wrapper = mount(<Profile name="John" surname="Doe" localName="王呆呆" />);
   expect(wrapper.find(`.${styles.StudentCard_profile_name}`).text()).toBe('John Doe');
 });
 
 test('renders profile info with the given surname', () => {
-  const wrapper = shallow(<Profile name="John" surname="Doe" localName="王呆呆" />);
+  const wrapper = mount(<Profile name="John" surname="Doe" localName="王呆呆" />);
   expect(wrapper.find(`.${styles.StudentCard_profile_localName}`).text()).toBe('王呆呆');
 });
--- src/components/StudentCard/index.js
+++ src/components/StudentCard/index.js
@@ -1,12 +1,15 @@
 import _StudentCard from './StudentCard';
 import _Content from './Content';
 import _Profile from './Profile';
+import _Person from './Person';
 
 export const StudentCard = _StudentCard;
 export const Content = _Content;
 export const Profile = _Profile;
+export const Person = _Person;
 
 StudentCard.Content = Content;
 StudentCard.Profile = Profile;
+StudentCard.Person = Person;
 
 export default StudentCard;
