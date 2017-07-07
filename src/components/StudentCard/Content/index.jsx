242e4af6d7f84076725fb5e0e704f8ee85e9ed6b
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-07-07 10:36:34 +0800

    The property “withSeparatorLine” is now <StudentCard.Content />’s responsibility

--- src/components/StudentCard/Content/index.jsx
+++ src/components/StudentCard/Content/index.jsx
@@ -1,10 +1,16 @@
+import styles from './style.postcss';
 import React from 'react';
 import pure from 'recompose/pure';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
-const Content = (props) => <div>{props.children}</div>;
+const Content = ({ withSeparatorLine, children }) => {
+  const classes = classnames(styles.Content, { [styles.__withSeparatorLine]: withSeparatorLine });
+  return <div className={classes}>{children}</div>;
+};
 
 Content.propTypes = {
+  withSeparatorLine: PropTypes.bool,
   children: PropTypes.node,
 };
 
