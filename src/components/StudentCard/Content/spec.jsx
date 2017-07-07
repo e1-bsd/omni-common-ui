72b3eb4250d35b96b811bda68471179cff6e7021
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-07-07 11:06:30 +0800

    Fixed tests

--- src/components/StudentCard/Content/spec.jsx
+++ src/components/StudentCard/Content/spec.jsx
@@ -1,3 +1,4 @@
+import styles from './style.postcss';
 import React from 'react';
 import { shallow } from 'enzyme';
 import Content from './';
@@ -5,4 +6,9 @@ import Content from './';
 test('renders its children', () => {
   const wrapper = shallow(<Content><div id="innerContent" /></Content>);
   expect(wrapper.contains(<div id="innerContent" />)).toBe(true);
+});
+
+test('renders a StudentCard.Content with separator class (withSeparatorLine provided)', () => {
+  const wrapper = shallow(<Content withSeparatorLine />);
+  expect(wrapper.html()).toContain(styles.__withSeparatorLine);
 });
