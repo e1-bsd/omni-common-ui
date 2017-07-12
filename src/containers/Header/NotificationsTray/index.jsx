2a6246104c2aa2054084e51d859e9f2f4f382646
orangeflame <magicpince@gmail.com>
2017-07-12 10:32:24 +0800

    Mark notifications as read (#32)
    * [OMO-2125] add link mark as read
    
    * [OMO-2125] enhance the css a little bit
    
    * [OMO-2125] footer
    
    * [OMO-2125] sync click status into state
    
    * [OMO-2125] implement check all and uncheck all function
    
    * [OMO-2125] enhance the ui a little bit
    
    * [OMO-2125] keep enhancing ui
    
    * [OMO-2125] keep enhancing ui
    
    * [OMO-2125] rename the parameters
    
    * [OMO-2125]  make more code review actions
    
    * [OMO-2125]  make on click in moding mode check the item
    
    * [OMO-2125]  make real api call
    
    * [OMO-2125]  prevent checkbox event trigger twice
    
    * [OMO-2125]  make even more actions for the code review

--- src/containers/Header/NotificationsTray/index.jsx
+++ src/containers/Header/NotificationsTray/index.jsx
@@ -1,28 +1,54 @@
 import styles from './style.postcss';
 
 import React, { PureComponent } from 'react';
-import classnames from 'classnames';
+import PropTypes from 'prop-types';
+import { Set } from 'immutable';
 import Cursor from 'immutable-cursor';
 import { CSSTransitionGroup } from 'react-transition-group';
+import classnames from 'classnames';
+import { fetch, buildUrl } from 'domain/Api';
 import Config from 'domain/Config';
+import createApiActionCreator from 'domain/createApiActionCreator';
 import connect from 'domain/connect';
 import Icon from 'components/Icon';
 import Callout from 'components/Callout';
+import Button from 'components/Button';
+import Checkbox from 'components/Checkbox';
 
 class NotificationsTray extends PureComponent {
   constructor() {
     super();
-    this.state = { viewingNotification: null };
+    this.state = {
+      viewingNotification: null,
+      isMarkingMode: false,
+      notificationIdsToMarkRead: new Set(),
+    };
     this._onNotificationClicked = this._onNotificationClicked.bind(this);
     this._onClickBackToNotifications = this._onClickBackToNotifications.bind(this);
     this._onCalloutOpenStateChanged = this._onCalloutOpenStateChanged.bind(this);
     this._renderCalloutPopupContent = this._renderCalloutPopupContent.bind(this);
+    this._onMarkAsReadClick = this._onMarkAsReadClick.bind(this);
+    this._onCancelClick = this._onCancelClick.bind(this);
+    this._onMarkClick = this._onMarkClick.bind(this);
   }
 
   _onNotificationClicked(ev) {
+    ev.preventDefault();
+    ev.stopPropagation();
     const element = ev.currentTarget;
     const { notificationId } = element.dataset;
-    this.setState({ viewingNotification: this.props.notifications.get(notificationId) });
+
+    if (this.state.isMarkingMode) {
+      if (! this.state.notificationIdsToMarkRead.has(notificationId)) {
+        return this.setState({
+          notificationIdsToMarkRead: this.state.notificationIdsToMarkRead.add(notificationId),
+        });
+      }
+      return this.setState({
+        notificationIdsToMarkRead: this.state.notificationIdsToMarkRead.delete(notificationId),
+      });
+    }
+    return this.setState({ viewingNotification: this.props.notifications.get(notificationId) });
   }
 
   _onClickBackToNotifications() {
@@ -34,9 +60,118 @@ class NotificationsTray extends PureComponent {
     this.setState({ viewingNotification: null });
   }
 
+  _onMarkAsReadClick() {
+    if (! this.state.isMarkingMode) {
+      this.setState({
+        isMarkingMode: true,
+      });
+    }
+  }
+
+  _onCancelClick() {
+    this.setState({
+      isMarkingMode: false,
+    });
+  }
+
+  _onMarkClick() {
+    if (this.state.notificationIdsToMarkRead.size !== 0) {
+      this.props.markNotificationAsRead(this.state.notificationIdsToMarkRead);
+    }
+    this.setState({
+      notificationIdsToMarkRead: new Set(),
+      isMarkingMode: false,
+    });
+  }
+
+  _renderNotificationCheckbox(notificationId) {
+    if (this.state.isMarkingMode) {
+      return <Checkbox name={notificationId}
+          checked={this.state.notificationIdsToMarkRead.includes(notificationId)}
+          className={styles.NotificationsTray_notification_checkbox}
+          id={notificationId} />;
+    }
+    return null;
+  }
+
+  _renderNotification(notification, notificationId) {
+    return <li>
+      <div className={styles.NotificationsTray_notification}
+          onClick={this._onNotificationClicked}
+          data-notification-id={notificationId}
+          role="button"
+          tabIndex="0">
+        {this._renderNotificationCheckbox(notificationId)}
+        <span className={styles.NotificationsTray_notification_blurb}>
+          {notification.blurb}
+        </span>
+        <span className={styles.NotificationsTray_notification_time}>
+          {notification.moment.fromNow()}
+        </span>
+        <Icon className={styles.NotificationsTray_notification_chevron}
+            id="chevron-small-right" />
+      </div>
+    </li>;
+  }
+
+  _renderNotifications(notifications) {
+    if (notifications && notifications.size) {
+      return <ul className={styles.NotificationsTray_popup_list}>
+        {notifications.map((notification, notificationId) =>
+          this._renderNotification(notification, notificationId)
+        )}
+
+      </ul>;
+    }
+    return null;
+  }
+
+  _onCheckAllNotifications(checked, notifications) {
+    if (checked) {
+      this.setState({
+        notificationIdsToMarkRead: [...notifications.keys()],
+      });
+    } else {
+      this.setState({
+        notificationIdsToMarkRead: [],
+      });
+    }
+  }
+
+  _renderNotificationFooter(notifications) {
+    if (this.state.isMarkingMode) {
+      return <div className={styles.NotificationsTray_notification_footer}>
+        <div>
+          <Checkbox name="check-all"
+              id="check-all"
+              checked={this.state.notificationIdsToMarkRead.length === notifications.size}
+              onChange={(checked) => { this._onCheckAllNotifications(checked, notifications); }}
+              className={styles.NotificationsTray_notification_footer_checkAll} />
+          <span>All</span>
+        </div>
+        <div className={styles.NotificationsTray_notification_footer_btns}>
+          <Button type={Button.Type.default}
+              className={styles.NotificationsTray_notification_footer_cancel}
+              onClick={this._onCancelClick}>
+            Cancel
+          </Button>
+          <Button type={Button.Type.primary}
+              className={styles.NotificationsTray_notification_footer_mark}
+              onClick={this._onMarkClick}>
+            Mark
+          </Button>
+        </div>
+      </div>;
+    }
+    return null;
+  }
+
   _renderCalloutPopupContent() {
     const { notifications } = this.props;
     const { viewingNotification } = this.state;
+    const headerBtnClassName = classnames(styles.NotificationsTray_popup_heading_btn, {
+      [styles.__inActive]: this.state.isMarkingMode,
+    });
     return <div>
       <div className={classnames(styles.NotificationsTray_popup_slide, {
         [styles.__active]: ! this.state.viewingNotification,
@@ -44,6 +179,8 @@ class NotificationsTray extends PureComponent {
       })}>
         <div className={styles.NotificationsTray_popup_heading}>
           <h2>Notifications</h2>
+          <a className={headerBtnClassName}
+              onClick={this._onMarkAsReadClick}>Mark as read</a>
         </div>
         {! notifications || ! notifications.size ?
           <div className={styles.NotificationsTray_popup_empty}>
@@ -52,25 +189,8 @@ class NotificationsTray extends PureComponent {
             <aside>New notifications will appear here when <br />
               teachers add internal notes for your students.</aside>
           </div> : null}
-        {notifications && notifications.size ?
-          <ul className={styles.NotificationsTray_popup_list}>
-            {notifications.map((notification, notificationId) => <li>
-              <div className={styles.NotificationsTray_notification}
-                  onClick={this._onNotificationClicked}
-                  data-notification-id={notificationId}
-                  role="button"
-                  tabIndex="0">
-                <span className={styles.NotificationsTray_notification_blurb}>
-                  {notification.blurb}
-                </span>
-                <span className={styles.NotificationsTray_notification_time}>
-                  {notification.moment.fromNow()}
-                </span>
-                <Icon className={styles.NotificationsTray_notification_chevron}
-                    id="chevron-small-right" />
-              </div>
-            </li>)}
-          </ul> : null}
+        {this._renderNotifications(notifications)}
+        {this._renderNotificationFooter(notifications)}
       </div>
       <div className={classnames(styles.NotificationsTray_popup_slide, {
         [styles.__active]: !! this.state.viewingNotification,
@@ -79,9 +199,11 @@ class NotificationsTray extends PureComponent {
             onClick={this._onClickBackToNotifications}
             role="button"
             tabIndex="-1">
-          <Icon className={styles.NotificationsTray_popup_heading_chevron}
-              id="chevron-small-left" />
-          <h2>Back to notifications</h2>
+          <div className={styles.NotificationsTray_popup_heading_secondary}>
+            <Icon className={styles.NotificationsTray_popup_heading_chevron}
+                id="chevron-small-left" />
+            <h2>Back to notifications</h2>
+          </div>
         </div>
         {viewingNotification && viewingNotification.getNotificationViewNode &&
             viewingNotification.getNotificationViewNode(this)}
@@ -117,10 +239,12 @@ class NotificationsTray extends PureComponent {
 }
 
 NotificationsTray.propTypes = {
-  notifications: React.PropTypes.shape({
-    map: React.PropTypes.func.isRequired,
-    get: React.PropTypes.func.isRequired,
+  notifications: PropTypes.shape({
+    map: PropTypes.func.isRequired,
+    get: PropTypes.func.isRequired,
   }),
+  accessToken: PropTypes.string,
+  markNotificationAsRead: PropTypes.func,
 };
 
 function mapStateToProps(state) {
@@ -129,4 +253,34 @@ function mapStateToProps(state) {
   return { notifications };
 }
 
-export default connect(mapStateToProps)(NotificationsTray);
+function mapDispatchToProps(dispatch) {
+  const markAsReadDispatchConfig = Config.get('notificationsTray').markAsRead.dispatch;
+  const apiUrl = buildUrl(markAsReadDispatchConfig.apiUrl);
+  const method = markAsReadDispatchConfig.method;
+  return {
+    markNotificationAsRead: (notificationIds) => {
+      const data = JSON.stringify(notificationIds.map((id) => {
+        const model = { id };
+        return model;
+      }));
+      const actionCreator = createApiActionCreator({
+        actionObjectName: 'MARK_NOTIFICATIONS_READ',  // SUBMIT_MARK_NOTIFICATIONS_READ_REQUEST, SUBMIT_..._SUCCESS
+        url: apiUrl,
+        method,
+        requestExtras: {
+          disableDefault: true,
+          payload: fetch(apiUrl, {
+            method,
+            body: data,
+          }),
+          notificationIds,
+        },
+        successExtras: { disableDefault: true, notificationIds, },
+        failureExtras: { disableDefault: true, notificationIds, },
+      });
+      actionCreator(dispatch);
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NotificationsTray);
