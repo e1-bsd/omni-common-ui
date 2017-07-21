1b000ec17798640baea650a3a9829803c3c462b5
Luke Plaster <me@lukep.org>
2017-07-21 08:14:09 +0800

    SSO: Signing out other tabs on user sign out

--- src/containers/SingleSignOn/createSignInRedirectMiddleware.js
+++ src/containers/SingleSignOn/createSignInRedirectMiddleware.js
@@ -1,10 +1,11 @@
 import log from 'domain/log';
 import {
+  LOAD_USER_ERROR,
   USER_EXPIRED,
   USER_NOT_FOUND,
-  LOAD_USER_ERROR,
   USER_SIGNED_OUT,
   SESSION_TERMINATED,
+  TRIGGER_USER_CLEAR,
   TRIGGER_SIGNIN_REDIRECT,
   TRIGGER_SIGNOUT_REDIRECT,
 } from 'data/SingleSignOn/actions';
@@ -13,9 +14,9 @@ import { createUserManager } from 'data/SingleSignOn';
 export function createSignInRedirectMiddleware() {
   return (store) => (next) => (action) => {
     switch (action.type) {
+      case LOAD_USER_ERROR:
       case USER_EXPIRED:
       case USER_NOT_FOUND:
-      case LOAD_USER_ERROR:
       case SESSION_TERMINATED:
         if (location.pathname === '/callback') break;  // still processing the callback
         log.info('createSignInRedirectMiddleware: Caught an invalid user/session action. Redirecting.');
@@ -24,10 +25,12 @@ export function createSignInRedirectMiddleware() {
         sessionStorage.lastUrlPath =
             action.returnUrl || location.pathname + location.search;
         createUserManager().signInRedirectWithValidation();
+        // for impersonate mode, prevents an infinite loop
         if (action.type === TRIGGER_SIGNIN_REDIRECT) {
-          store.dispatch({ type: USER_SIGNED_OUT });
+          store.dispatch({ type: TRIGGER_USER_CLEAR });
         }
         break;
+      case USER_SIGNED_OUT:
       case TRIGGER_SIGNOUT_REDIRECT:
         sessionStorage.lastUrlPath =
             action.returnUrl || location.pathname + location.search;
