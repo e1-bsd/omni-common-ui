84fa71eb9dff3e914d28927b4543376c72141c2f
Luke Plaster <me@lukep.org>
2017-07-21 15:48:34 +0800

    SSO: Bouncing 401s in middleware rather than in ErrorPageHandler

--- src/containers/SingleSignOn/createSignInRedirectMiddleware.js
+++ src/containers/SingleSignOn/createSignInRedirectMiddleware.js
@@ -1,4 +1,5 @@
 import log from 'domain/log';
+import ApiCall from 'containers/ApiCalls';
 import {
   LOAD_USER_ERROR,
   USER_EXPIRED,
@@ -11,33 +12,51 @@ import {
 } from 'data/SingleSignOn/actions';
 import { createUserManager } from 'data/SingleSignOn';
 
+const triggerSignInRedirectFlow = (returnUrl) => {
+  sessionStorage.lastUrlPath =
+      returnUrl || location.pathname + location.search;
+  createUserManager().signInRedirectWithValidation();
+};
+
+const triggerSignOutRedirectFlow = (returnUrl) => {
+  sessionStorage.lastUrlPath =
+      returnUrl || location.pathname + location.search;
+  createUserManager().forceSignOutRedirect();
+};
+
 export function createSignInRedirectMiddleware() {
   return (store) => (next) => (action) => {
-    switch (action.type) {
-      case LOAD_USER_ERROR:
-      case USER_EXPIRED:
-      case USER_NOT_FOUND:
-      case SESSION_TERMINATED:
-        if (location.pathname === '/callback') break;  // still processing the callback
-        log.info('createSignInRedirectMiddleware: Caught an invalid user/session action. Redirecting.');
-        // intentionally fall through to the next case!
-      case TRIGGER_SIGNIN_REDIRECT:  // eslint-disable-line
-        sessionStorage.lastUrlPath =
-            action.returnUrl || location.pathname + location.search;
-        createUserManager().signInRedirectWithValidation();
-        // for impersonate mode, prevents an infinite loop
-        if (action.type === TRIGGER_SIGNIN_REDIRECT) {
-          store.dispatch({ type: TRIGGER_USER_CLEAR });
-        }
-        break;
-      case USER_SIGNED_OUT:
-      case TRIGGER_SIGNOUT_REDIRECT:
-        sessionStorage.lastUrlPath =
-            action.returnUrl || location.pathname + location.search;
-        createUserManager().forceSignOutRedirect();
-        break;
-      default: break;
+    // bounce any 401s to the login page
+    if (ApiCall.Action.isApiAction(action) &&
+        ApiCall.Action.isFailure(action) &&
+        action.error.status === 401) {
+      triggerSignInRedirectFlow(action.returnUrl);
+
+    // handle trigger and state change actions
+    } else {
+      switch (action.type) {
+        case LOAD_USER_ERROR:
+        case USER_EXPIRED:
+        case USER_NOT_FOUND:
+        case SESSION_TERMINATED:
+          if (location.pathname === '/callback') break;  // still processing the callback
+          log.info('createSignInRedirectMiddleware: Caught an invalid user/session action. Redirecting.');
+          // intentionally fall through to the next case!
+        case TRIGGER_SIGNIN_REDIRECT:  // eslint-disable-line
+          triggerSignInRedirectFlow(action.returnUrl);
+          // in impersonate mode this prevents an infinite loop
+          if (action.type === TRIGGER_SIGNIN_REDIRECT) {
+            store.dispatch({ type: TRIGGER_USER_CLEAR });
+          }
+          break;
+        case USER_SIGNED_OUT:
+        case TRIGGER_SIGNOUT_REDIRECT:
+          triggerSignOutRedirectFlow(action.returnUrl);
+          break;
+        default: break;
+      }
     }
+
     return next(action);
   };
 }
