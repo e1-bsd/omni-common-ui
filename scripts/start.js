c709899d0767d17b334a03e79b6db66d661b9483
Luke Plaster <me@lukep.org>
2017-07-24 14:46:32 +0800

    scripts/start: passing through --port

--- scripts/start.js
+++ scripts/start.js
@@ -7,20 +7,31 @@ const spawn = require('./spawn');
 const options = require('command-line-args')([
   { name: 'config', alias: 'c', type: String },
   { name: 'host', type: String },
+  { name: 'port', type: String },
 ]);
+
+const DEFAULT_PORT = '8080';
 
 process.env.CONFIG = options.config || '';
 
-const command = ['node_modules/webpack-dev-server/bin/webpack-dev-server.js', '--profile', '--progress', '--hot', '--inline', '--port', '8080', '--config', path.resolve(__dirname, '../webpack.dev.js')];
+const command = ['node_modules/webpack-dev-server/bin/webpack-dev-server.js', '--profile', '--progress', '--hot', '--inline', '--config', path.resolve(__dirname, '../webpack.dev.js')];
+
 if (options.host) {
   command.push('--host');
   command.push(`${options.host}`);
 }
 
+command.push('--port');
+if (options.port) {
+  command.push(`${options.port}`);
+} else {
+  command.push(DEFAULT_PORT);
+}
+
 spawn('node', command, { stdio: 'inherit' })
-    .on('close', (code) => {
-      if (code) {
-        console.error(colors.red('😓  webpack-dev-server was unexpectedly closed.'));
-        process.exit(code);
-      }
-    });
+  .on('close', (code) => {
+    if (code) {
+      console.error(colors.red('😓  webpack-dev-server was unexpectedly closed.'));
+      process.exit(code);
+    }
+  });
