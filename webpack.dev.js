085bbf5a07edf2b851c2ced5c5dc8ceda1c8a325
Luke Plaster <me@lukep.org>
2017-07-25 16:41:07 +0800

    Added happypack, made it work

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const HappyPack = require('happypack');
 const CommonConfig = require('./webpack.config.js');
 const definePlugin = require('./webpack.define-plugin.js');
 const htmlPlugin = require('./webpack.html-plugin.js');
@@ -8,6 +9,8 @@ const htmlPlugin = require('./webpack.html-plugin.js');
 const packageInfo = require(path.resolve('package.json')); // eslint-disable-line import/no-dynamic-require
 const isCommon = packageInfo.name === 'omni-common-ui';
 const port = isCommon ? '3000' : '8080';
+
+const happyThreadPool = new HappyPack.ThreadPool({ size: 5 });
 
 module.exports = merge.smart(CommonConfig, {
   devtool: 'cheap-module-source-map',
@@ -20,15 +23,17 @@ module.exports = merge.smart(CommonConfig, {
   module: {
     rules: [
       {
+        test: /\.jsx?$/,
+        exclude: /node_modules(\/|\\)((?!(omni-common-ui)).)/,
+        use: {
+          loader: 'happypack/loader?id=js',
+        },
+      },
+      {
         test: /\.postcss$/,
-        use: [
-          {
-            loader: 'css-loader',
-            options: {
-              localIdentName: '[local]___[hash:base64:5]',
-            },
-          },
-        ],
+        use: {
+          loader: 'happypack/loader?id=css',
+        },
       },
     ],
   },
@@ -39,6 +44,31 @@ module.exports = merge.smart(CommonConfig, {
     historyApiFallback: true,
   },
   plugins: [
+    new HappyPack({
+      id: 'js',
+      threadPool: happyThreadPool,
+      loaders: [{
+        path: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-2'],
+          cacheDirectory: false,
+        },
+      }],
+    }),
+    new HappyPack({
+      id: 'css',
+      threadPool: happyThreadPool,
+      loaders: [
+        { path: 'style-loader' },
+        {
+          path: 'css-loader',
+          query: {
+            root: '.',
+            localIdentName: '[local]___[hash:base64:5]',
+          },
+        },
+      ],
+    }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.optimize.CommonsChunkPlugin({ name: 'vendor' }),
     new webpack.ProvidePlugin({
