f5fceba62fc94ec3b8a3dfb0dd84603389063d19
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-07-26 11:36:56 +0800

    Added more tests to domain/log

--- src/domain/log/spec.js
+++ src/domain/log/spec.js
@@ -1,26 +1,81 @@
-import log from './';
+let log;
+let Config;
+let buildLogToRaven;
 
-test('is a class', () => {
-  expect(log.prototype).toBeDefined();
+jest.mock('./buildLogToRaven', () => {
+  const mock = jest.fn();
+  return () => mock;
 });
 
-testMethod('error');
-testMethod('info');
-testMethod('log');
-testMethod('warn');
-testMethod('debug');
-
-test('does not crash if there is no "console"', () => {
-  const originalConsole = window.console;
-  delete window.console;
-
-  expect(() => log.debug('some text')).not.toThrowError();
-
-  window.console = originalConsole;
+beforeEach(() => {
+  jest.resetModules();
+  global.console.error = jest.fn();
 });
 
-function testMethod(method) {
-  test(`has a "${method}" method`, () => {
-    expect(typeof log[method]).toBe('function');
+describe('when the config contains a sentryDsn', () => {
+  beforeEach(() => {
+    buildLogToRaven = require('./buildLogToRaven');
+    Config = require('domain/Config');
+    Config.merge({ sentryDsn: 'this is a valid dsn' });
+    log = require('./').default;
   });
+
+  basicTests();
+
+  test('uses the logger returned by buildLogToRaven', () => {
+    log.error('an error');
+    expect(buildLogToRaven()).toHaveBeenCalledWith('an error');
+  });
+
+  test('does not use the console', () => {
+    log.error();
+    expect(global.console.error).not.toHaveBeenCalled();
+  });
+});
+
+describe('when the config does not contain a sentryDsn', () => {
+  beforeEach(() => {
+    buildLogToRaven = require('./buildLogToRaven');
+    Config = require('domain/Config');
+    log = require('./').default;
+  });
+
+  basicTests();
+
+  test('does not the logger returned by buildLogToRaven', () => {
+    log.error('an error');
+    expect(buildLogToRaven()).not.toHaveBeenCalled();
+  });
+
+  test('uses the console', () => {
+    log.error();
+    expect(global.console.error).toHaveBeenCalled();
+  });
+});
+
+function basicTests() {
+  test('is a class', () => {
+    expect(log.prototype).toBeDefined();
+  });
+
+  testMethod('error');
+  testMethod('info');
+  testMethod('log');
+  testMethod('warn');
+  testMethod('debug');
+
+  test('does not crash if there is no "console"', () => {
+    const originalConsole = window.console;
+    delete window.console;
+
+    expect(() => log.debug('some text')).not.toThrowError();
+
+    window.console = originalConsole;
+  });
+
+  function testMethod(method) {
+    test(`has a "${method}" method`, () => {
+      expect(typeof log[method]).toBe('function');
+    });
+  }
 }
