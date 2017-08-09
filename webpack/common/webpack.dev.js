920c91d9a544854ddb25a3bf5818a211f0dad884
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-08-09 15:26:07 +0800

    fix(Build): Brought readable class names for the development build of components within the Common library

--- webpack/common/webpack.dev.js
+++ webpack/common/webpack.dev.js
@@ -3,4 +3,19 @@ const baseConfigForCommon = require('./webpack.config.js');
 
 module.exports = merge.smart(baseConfigForCommon, {
   devtool: 'cheap-module-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.postcss$/,
+        use: [
+          {
+            loader: 'css-loader',
+            options: {
+              localIdentName: '[local]___[hash:base64:5]',
+            },
+          },
+        ],
+      },
+    ],
+  },
 });
