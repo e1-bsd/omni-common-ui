7a6dbf59b814224a3653fb33099032e4bdda5bbc
Rubén Illodo Brea <ruben.illodobrea@ef.com>
2017-08-17 10:09:13 +0800

    fix(Build): Greatly simplified the code for our custom implementation of getLocalIdent() for CSS class name hashing by actually reusing the default implementation from within css-loader

--- webpack/utils/css-class-hash.js
+++ webpack/utils/css-class-hash.js
@@ -1,23 +1,4 @@
-/* eslint-disable no-param-reassign */
+const getLocalIdent = require('css-loader/lib/getLocalIdent');
 
-const path = require('path');
-const is = require('is_js');
-const loaderUtils = require('loader-utils');
-
-module.exports = ({ prefix = '' }) => (loaderContext, localIdentName, localName, options) => {
-  if (! options.context) {
-    options.context = loaderContext.options &&
-        is.string(loaderContext.options.context) ?
-            loaderContext.options.context :
-            loaderContext.context;
-  }
-
-  localName = `${prefix}${localName}`;
-  const request = path.relative(options.context, loaderContext.resourcePath);
-  options.content = `${options.hashPrefix}${request}+${localName}`;
-
-  localIdentName = localIdentName.replace(/\[local\]/gi, localName);
-
-  const hash = loaderUtils.interpolateName(loaderContext, localIdentName, options);
-  return `_${hash}`;
-};
+module.exports = ({ prefix = '' }) => (loaderContext, localIdentName, localName, options) =>
+    getLocalIdent(loaderContext, localIdentName, `${prefix}${localName}`, options);
