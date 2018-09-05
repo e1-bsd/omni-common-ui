0142892588d649f039e68a909f25ebf5c3d24b55
Vic Li (External Consultant) <ext.vic.li@EF.com>
2018-09-05 17:48:06 +0800

    feat(PageCard): Add titleClassName for page card title.

--- src/components/PageCard/Heading/index.jsx
+++ src/components/PageCard/Heading/index.jsx
@@ -7,13 +7,13 @@ import is from 'is_js';
 import PropTypes from 'prop-types';
 
 const PageCardHeading = (props) => {
-  const { className, text } = props;
+  const { className, titleClassName, text } = props;
 
   return <header className={classnames(styles.PageCardHeading, className, {
     [styles.__stackHorizontal]: props.stackMode === 'horizontal',
   })}>
     {is.string(text) && text.length ?
-      <h1 className={styles.PageCardHeading_h1}>
+      <h1 className={classnames(styles.PageCardHeading_h1, titleClassName)}>
         {text}
       </h1> : null}
     {props.children}
@@ -22,6 +22,7 @@ const PageCardHeading = (props) => {
 
 PageCardHeading.propTypes = {
   className: PropTypes.string,
+  titleClassName: PropTypes.string,
   text: PropTypes.string,
   stackMode: PropTypes.oneOf(['vertical', 'horizontal']),  // default: vertical
   children: PropTypes.node,
