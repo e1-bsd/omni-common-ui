fa8b5a92cc05a5241be5b6dfede852aa3413fa73
Vic Li <563203132@qq.com>
2019-01-03 13:34:43 +0800

    Recover semantic-release.

--- ci.deploy-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git add --force lib && \
-git commit -m "no message" && \
-TAG="$(git describe --abbrev=0 --tags)" && \
-npm version "$TAG-dev.$TRAVIS_BUILD_NUMBER" && \
-git add package.json && \
-git commit --amend -m "Merged $TRAVIS_BRANCH ($TRAVIS_COMMIT) into master" && \
-git push origin master:master
