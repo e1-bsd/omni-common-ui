47b99003894afd5a7556748a5c5ec884811138aa
jacob.huang <jacob.huang@ef.com>
2019-01-08 10:28:51 +0800

    remove helpcenter link

--- src/components/Footer/component.jsx
+++ src/components/Footer/component.jsx
@@ -4,13 +4,7 @@ import Icon from 'components/Icon';
 
 const Footer = () => <div className={styles.Footer}>
   <Icon id="ef-logo" className={styles.Footer_logo} />
-  <div className={styles.Footer_content}>
-    <div className={styles.Footer_content_row}>
-      <a className={styles.Footer_content_link}
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://helpcenter.ef.com">Help Center</a>
-    </div>
+  <div className={styles.Footer_content}>    
     <div className={styles.Footer_content_row}>
       <div className={styles.Footer_copyright}>
         <span className={styles.Footer_copyright_firstLine}>
